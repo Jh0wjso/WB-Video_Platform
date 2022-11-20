59ba0201d80ff838b3da144b34fe570c8029ff90
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-11-20 03:06:38 -0300

    First Carousel template

--- src/screens/Inicio/index.tsx
+++ src/screens/Inicio/index.tsx
@@ -5,13 +5,17 @@ import "swiper/css";
 import "swiper/css/navigation";
 import { Navigation } from "swiper";
 
-import One from "../../images/605799.jpg";
-import Two from "../../images/dragon-ball-z-wallpaper-background-1.jpg";
+import Carousel from 'better-react-carousel';
 
 export default function Inicio() {
   return (
-    <section className="flex flex-col mt-7">
-      <Swiper navigation={true} modules={[Navigation]} loop className="mySwiper container">
+    <section className="flex flex-col mt-7 items-center">
+      <Swiper
+        navigation={true}
+        modules={[Navigation]}
+        loop
+        className="mySwiper container"
+      >
         <SwiperSlide>
           <img
             className="object-fill w-full h-96 rounded"
@@ -34,6 +38,39 @@ export default function Inicio() {
           />
         </SwiperSlide>
       </Swiper>
+      <div className="container flex flex-col items-center mt-9">
+        <div className="border-b-2 rounded w-full">
+          <h1 className="text-4xl ml-3 md:ml-9">Dragon Ball Heroes</h1>
+        </div>
+        <div className="mt-3">
+          <Carousel cols={4} rows={1} gap={10} loop>
+            <Carousel.Item>
+              <img width="100%" className="rounded" src="https://picsum.photos/800/600?random=1" />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img width="100%" className="rounded" src="https://picsum.photos/800/600?random=2" />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img width="100%" className="rounded" src="https://picsum.photos/800/600?random=3" />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img width="100%" className="rounded" src="https://picsum.photos/800/600?random=4" />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img width="100%" className="rounded" src="https://picsum.photos/800/600?random=5" />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img width="100%" className="rounded" src="https://picsum.photos/800/600?random=6" />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img width="100%" className="rounded" src="https://picsum.photos/800/600?random=7" />
+            </Carousel.Item>
+            <Carousel.Item>
+                <img width="100%" className="rounded" src="https://picsum.photos/800/600?random=8" />
+            </Carousel.Item>
+          </Carousel>
+        </div>
+      </div>
     </section>
   );
 }
