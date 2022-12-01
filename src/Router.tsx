79d6574bfefb6ad3d29cf2c8fb1ae12059a73d56
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-11-30 21:51:13 -0300

    Animes screen created

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Inicio from "./screens/Inicio";
+import Animes from "./screens/Animes";
 
 export function Router(){
     return(
         <Routes>
             <Route path="/" element={<Inicio />} />
+            <Route path="/animes" element={<Animes />} />
         </Routes>
     );
 }
