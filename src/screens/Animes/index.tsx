496108359fee7378361eebc067c678a211f90f97
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-12-01 21:51:06 -0300

    Updates on Cards

--- src/screens/Animes/index.tsx
+++ src/screens/Animes/index.tsx
@@ -6,21 +6,21 @@ import "./styles.css";
 
 export default function Animes() {
   return (
-    <div className="min-h-scree text-gray-600 flex flex-col items-center justify-center">
+    <div className="min-h-scree text-gray-600 text-xl md:text-base flex flex-col items-center justify-center">
       <div className="md:my-14 cardsContainer items-center justify-between flex md:flex-row flex-col">
         <Link
           to="/"
           className="cardAnime hover:opacity-50 hover:text-gray-500 hover:underline text-center flex flex-col items-center h-full p-4 m-5 bg-white rounded"
         >
           <img src={Db} width={250} className="mb-5 rounded-lg " />
-          <p className="font-bold uppercase text-2xl">Dragon Ball</p>
+          <p className="font-bold uppercase">Dragon Ball</p>
         </Link>
         <Link
           to="/"
           className="cardAnime hover:opacity-50 hover:text-gray-500 hover:underline text-center flex flex-col items-center h-full p-4 m-5 bg-white rounded"
         >
           <img src={Db} width={250} className="mb-5 rounded-lg " />
-          <p className="font-bold uppercase text-2xl">Dragon Ball Z</p>
+          <p className="font-bold uppercase">Dragon Ball Z</p>
         </Link>
 
         <Link
@@ -28,7 +28,14 @@ export default function Animes() {
           className="cardAnime hover:opacity-50 hover:text-gray-500 hover:underline text-center flex flex-col items-center h-full p-4 m-5 bg-white rounded"
         >
           <img src={Db} width={250} className="mb-5 rounded-lg " />
-          <p className="font-bold uppercase text-2xl">Dragon Ball Z Kai</p>
+          <p className="font-bold uppercase">Dragon Ball Z Kai</p>
+        </Link>
+        <Link
+          to="/"
+          className="cardAnime hover:opacity-50 hover:text-gray-500 hover:underline text-center flex flex-col items-center h-full p-4 m-5 bg-white rounded"
+        >
+          <img src={Db} width={250} className="mb-5 rounded-lg " />
+          <p className="font-bold uppercase">Dragon Ball Super</p>
         </Link>
       </div>
       <div className="md:mb-14 cardsContainer items-center justify-between flex md:flex-row flex-col">
@@ -41,7 +48,7 @@ export default function Animes() {
             width={250}
             className="mb-5 rounded-lg "
           />
-          <p className="font-bold uppercase text-2xl">Dragon Ball Super</p>
+          <p className="font-bold uppercase">Dragon Ball GT</p>
         </Link>
         <Link
           to="/"
@@ -52,7 +59,7 @@ export default function Animes() {
             width={250}
             className="mb-5 rounded-lg "
           />
-          <p className="font-bold uppercase text-2xl">Dragon Ball GT</p>
+          <p className="font-bold uppercase">Dragon Ball Heroes</p>
         </Link>
 
         <Link
@@ -64,7 +71,14 @@ export default function Animes() {
             width={250}
             className="mb-5 rounded-lg "
           />
-          <p className="font-bold uppercase text-2xl">Dragon Ball Heroes</p>
+          <p className="font-bold uppercase">Dragon Ball OVAs</p>
+        </Link>
+        <Link
+          to="/"
+          className="cardAnime hover:opacity-50 hover:text-gray-500 hover:underline text-center flex flex-col items-center h-full p-4 m-5 bg-white rounded"
+        >
+          <img src={Db} width={250} className="mb-5 rounded-lg " />
+          <p className="font-bold uppercase">Especiais</p>
         </Link>
       </div>
     </div>
