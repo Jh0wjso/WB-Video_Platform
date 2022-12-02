8cf8eaf8d74792f429ae23c1390001cbce900747
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-12-02 10:04:04 -0300

    Updates on Card

--- src/screens/Animes/index.tsx
+++ src/screens/Animes/index.tsx
@@ -1,85 +1,161 @@
 import { Link } from "react-router-dom";
 
-import Db from "../../images/Db.jpg";
+import Db from "../../images/DB_case.jpg";
 
 import "./styles.css";
 
 export default function Animes() {
   return (
-    <div className="min-h-scree text-gray-600 text-xl md:text-base flex flex-col items-center justify-center">
+    <div className="min-h-scree text-white text-xl md:text-base flex flex-col items-center justify-center">
       <div className="md:my-14 cardsContainer items-center justify-between flex md:flex-row flex-col">
-        <Link
-          to="/"
-          className="cardAnime hover:opacity-50 hover:text-gray-500 hover:underline text-center flex flex-col items-center h-full p-4 m-5 bg-white rounded"
-        >
-          <img src={Db} width={250} className="mb-5 rounded-lg " />
-          <p className="font-bold uppercase">Dragon Ball</p>
-        </Link>
-        <Link
-          to="/"
-          className="cardAnime hover:opacity-50 hover:text-gray-500 hover:underline text-center flex flex-col items-center h-full p-4 m-5 bg-white rounded"
-        >
-          <img src={Db} width={250} className="mb-5 rounded-lg " />
-          <p className="font-bold uppercase">Dragon Ball Z</p>
-        </Link>
-
-        <Link
-          to="/"
-          className="cardAnime hover:opacity-50 hover:text-gray-500 hover:underline text-center flex flex-col items-center h-full p-4 m-5 bg-white rounded"
-        >
+        <div className="cardAnime text-center flex flex-col items-center h-full p-4 m-5 bg-[transparent] border rounded">
           <img src={Db} width={250} className="mb-5 rounded-lg " />
           <p className="font-bold uppercase">Dragon Ball Z Kai</p>
-        </Link>
-        <Link
-          to="/"
-          className="cardAnime hover:opacity-50 hover:text-gray-500 hover:underline text-center flex flex-col items-center h-full p-4 m-5 bg-white rounded"
-        >
+          <div className="btnContent w-[80%] text-xl md:text-sm my-1 items-center">
+            <Link
+              to="/"
+              className="text-center bg-orange-400 text-xl md:text-xs m-2 w-[75%] md:w-auto md:m-2 hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            >
+              Dublado
+            </Link>
+            <Link
+              to="/"
+              className="text-center bg-orange-400 text-xl md:text-xs w-[75%] md:w-auto hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            >
+              Legendado
+            </Link>
+          </div>
+        </div>
+        <div className="cardAnime text-center flex flex-col items-center h-full p-4 m-5 bg-[transparent] border rounded">
           <img src={Db} width={250} className="mb-5 rounded-lg " />
-          <p className="font-bold uppercase">Dragon Ball Super</p>
-        </Link>
+          <p className="font-bold uppercase">Dragon Ball Z Kai</p>
+          <div className="btnContent w-[80%] text-xl md:text-sm my-1 items-center">
+            <Link
+              to="/"
+              className="text-center bg-orange-400 text-xl md:text-xs m-2 w-[75%] md:w-auto md:m-2 hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            >
+              Dublado
+            </Link>
+            <Link
+              to="/"
+              className="text-center bg-orange-400 text-xl md:text-xs w-[75%] md:w-auto hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            >
+              Legendado
+            </Link>
+          </div>
+        </div>
+
+        <div className="cardAnime text-center flex flex-col items-center h-full p-4 m-5 bg-[transparent] border rounded">
+          <img src={Db} width={250} className="mb-5 rounded-lg " />
+          <p className="font-bold uppercase">Dragon Ball Z Kai</p>
+          <div className="btnContent w-[80%] text-xl md:text-sm my-1 items-center">
+            <Link
+              to="/"
+              className="text-center bg-orange-400 text-xl md:text-xs m-2 w-[75%] md:w-auto md:m-2 hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            >
+              Dublado
+            </Link>
+            <Link
+              to="/"
+              className="text-center bg-orange-400 text-xl md:text-xs w-[75%] md:w-auto hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            >
+              Legendado
+            </Link>
+          </div>
+        </div>
+        <div className="cardAnime text-center flex flex-col items-center h-full p-4 m-5 bg-[transparent] border rounded">
+          <img src={Db} width={250} className="mb-5 rounded-lg " />
+          <p className="font-bold uppercase">Dragon Ball Z Kai</p>
+          <div className="btnContent w-[80%] text-xl md:text-sm my-1 items-center">
+            <Link
+              to="/"
+              className="text-center bg-orange-400 text-xl md:text-xs m-2 w-[75%] md:w-auto md:m-2 hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            >
+              Dublado
+            </Link>
+            <Link
+              to="/"
+              className="text-center bg-orange-400 text-xl md:text-xs w-[75%] md:w-auto hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            >
+              Legendado
+            </Link>
+          </div>
+        </div>
       </div>
       <div className="md:mb-14 cardsContainer items-center justify-between flex md:flex-row flex-col">
-      <Link
-          to="/"
-          className="cardAnime hover:opacity-50 hover:text-gray-500 hover:underline text-center flex flex-col items-center h-full p-4 m-5 bg-white rounded"
-        >
-          <img
-            src={Db}
-            width={250}
-            className="mb-5 rounded-lg "
-          />
-          <p className="font-bold uppercase">Dragon Ball GT</p>
-        </Link>
-        <Link
-          to="/"
-          className="cardAnime hover:opacity-50 hover:text-gray-500 hover:underline text-center flex flex-col items-center h-full p-4 m-5 bg-white rounded"
-        >
-          <img
-            src={Db}
-            width={250}
-            className="mb-5 rounded-lg "
-          />
-          <p className="font-bold uppercase">Dragon Ball Heroes</p>
-        </Link>
-
-        <Link
-          to="/"
-          className="cardAnime hover:opacity-50 hover:text-gray-500 hover:underline text-center flex flex-col items-center h-full p-4 m-5 bg-white rounded"
-        >
-          <img
-            src={Db}
-            width={250}
-            className="mb-5 rounded-lg "
-          />
-          <p className="font-bold uppercase">Dragon Ball OVAs</p>
-        </Link>
-        <Link
-          to="/"
-          className="cardAnime hover:opacity-50 hover:text-gray-500 hover:underline text-center flex flex-col items-center h-full p-4 m-5 bg-white rounded"
-        >
+        <div className="cardAnime text-center flex flex-col items-center h-full p-4 m-5 bg-[transparent] border rounded">
           <img src={Db} width={250} className="mb-5 rounded-lg " />
-          <p className="font-bold uppercase">Especiais</p>
-        </Link>
+          <p className="font-bold uppercase">Dragon Ball Z Kai</p>
+          <div className="btnContent w-[80%] text-xl md:text-sm my-1 items-center">
+            <Link
+              to="/"
+              className="text-center bg-orange-400 text-xl md:text-xs m-2 w-[75%] md:w-auto md:m-2 hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            >
+              Dublado
+            </Link>
+            <Link
+              to="/"
+              className="text-center bg-orange-400 text-xl md:text-xs w-[75%] md:w-auto hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            >
+              Legendado
+            </Link>
+          </div>
+        </div>
+        <div className="cardAnime text-center flex flex-col items-center h-full p-4 m-5 bg-[transparent] border rounded">
+          <img src={Db} width={250} className="mb-5 rounded-lg " />
+          <p className="font-bold uppercase">Dragon Ball Z Kai</p>
+          <div className="btnContent w-[80%] text-xl md:text-sm my-1 items-center">
+            <Link
+              to="/"
+              className="text-center bg-orange-400 text-xl md:text-xs m-2 w-[75%] md:w-auto md:m-2 hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            >
+              Dublado
+            </Link>
+            <Link
+              to="/"
+              className="text-center bg-orange-400 text-xl md:text-xs w-[75%] md:w-auto hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            >
+              Legendado
+            </Link>
+          </div>
+        </div>
+
+        <div className="cardAnime text-center flex flex-col items-center h-full p-4 m-5 bg-[transparent] border rounded">
+          <img src={Db} width={250} className="mb-5 rounded-lg " />
+          <p className="font-bold uppercase">Dragon Ball Z Kai</p>
+          <div className="btnContent w-[80%] text-xl md:text-sm my-1 items-center">
+            <Link
+              to="/"
+              className="text-center bg-orange-400 text-xl md:text-xs m-2 w-[75%] md:w-auto md:m-2 hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            >
+              Dublado
+            </Link>
+            <Link
+              to="/"
+              className="text-center bg-orange-400 text-xl md:text-xs w-[75%] md:w-auto hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            >
+              Legendado
+            </Link>
+          </div>
+        </div>
+        <div className="cardAnime text-center flex flex-col items-center h-full p-4 m-5 bg-[transparent] border rounded">
+          <img src={Db} width={250} className="mb-5 rounded-lg " />
+          <p className="font-bold uppercase">Dragon Ball Z Kai</p>
+          <div className="btnContent w-[80%] text-xl md:text-sm my-1 items-center">
+            <Link
+              to="/"
+              className="text-center bg-orange-400 text-xl md:text-xs m-2 w-[75%] md:w-auto md:m-2 hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            >
+              Dublado
+            </Link>
+            <Link
+              to="/"
+              className="text-center bg-orange-400 text-xl md:text-xs w-[75%] md:w-auto hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            >
+              Legendado
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
