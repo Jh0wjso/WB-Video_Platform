1569a7ca1ad6f8ad7e12acbfd49c987815eff249
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-12-01 21:41:05 -0300

    Updates cards

--- src/screens/Animes/index.tsx
+++ src/screens/Animes/index.tsx
@@ -1,78 +1,71 @@
-import React from "react";
 import { Link } from "react-router-dom";
 
 import Db from "../../images/Db.jpg";
 
-import "./styles.css"
+import "./styles.css";
 
 export default function Animes() {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center">
+    <div className="min-h-scree text-gray-600 flex flex-col items-center justify-center">
       <div className="md:my-14 cardsContainer items-center justify-between flex md:flex-row flex-col">
-        <div className="cardAnime text-center flex flex-col items-center h-full p-4 mx-5">
-          <Link to="/">
-            <img
-              src={Db}
-              width={250}
-              className="mb-5 rounded-lg hover:opacity-50"
-            />
-          </Link>
-          <p className="text-white font-bold uppercase text-2xl">Dragon Ball</p>
-        </div>
-        <div className="cardAnime text-center flex flex-col items-center h-full p-4 mx-5">
-          <Link to="/">
-            <img
-              src={Db}
-              width={250}
-              className="mb-5 rounded-lg hover:opacity-50"
-            />
-          </Link>
-          <p className="text-white font-bold uppercase text-2xl">Dragon Ball Z</p>
-        </div>
+        <Link
+          to="/"
+          className="cardAnime hover:opacity-50 hover:text-gray-500 hover:underline text-center flex flex-col items-center h-full p-4 m-5 bg-white rounded"
+        >
+          <img src={Db} width={250} className="mb-5 rounded-lg " />
+          <p className="font-bold uppercase text-2xl">Dragon Ball</p>
+        </Link>
+        <Link
+          to="/"
+          className="cardAnime hover:opacity-50 hover:text-gray-500 hover:underline text-center flex flex-col items-center h-full p-4 m-5 bg-white rounded"
+        >
+          <img src={Db} width={250} className="mb-5 rounded-lg " />
+          <p className="font-bold uppercase text-2xl">Dragon Ball Z</p>
+        </Link>
 
-        <div className="cardAnime text-center flex flex-col items-center h-full p-4 mx-5">
-          <Link to="/">
-            <img
-              src={Db}
-              width={250}
-              className="mb-5 rounded-lg hover:opacity-50"
-            />
-          </Link>
-          <p className="text-white font-bold uppercase text-2xl">Dragon Ball Z KAI</p>
-        </div>
+        <Link
+          to="/"
+          className="cardAnime hover:opacity-50 hover:text-gray-500 hover:underline text-center flex flex-col items-center h-full p-4 m-5 bg-white rounded"
+        >
+          <img src={Db} width={250} className="mb-5 rounded-lg " />
+          <p className="font-bold uppercase text-2xl">Dragon Ball Z Kai</p>
+        </Link>
       </div>
       <div className="md:mb-14 cardsContainer items-center justify-between flex md:flex-row flex-col">
-        <div className="cardAnime text-center flex flex-col items-center h-full p-4 mx-5">
-          <Link to="/">
-            <img
-              src={Db}
-              width={250}
-              className="mb-5 rounded-lg hover:opacity-50"
-            />
-          </Link>
-          <p className="text-white font-bold uppercase text-2xl">Dragon Ball Super</p>
-        </div>
-        <div className="cardAnime text-center flex flex-col items-center h-full p-4 mx-5">
-          <Link to="/">
-            <img
-              src={Db}
-              width={250}
-              className="mb-5 rounded-lg hover:opacity-50"
-            />
-          </Link>
-          <p className="text-white font-bold uppercase text-2xl">Dragon Ball GT</p>
-        </div>
+      <Link
+          to="/"
+          className="cardAnime hover:opacity-50 hover:text-gray-500 hover:underline text-center flex flex-col items-center h-full p-4 m-5 bg-white rounded"
+        >
+          <img
+            src={Db}
+            width={250}
+            className="mb-5 rounded-lg "
+          />
+          <p className="font-bold uppercase text-2xl">Dragon Ball Super</p>
+        </Link>
+        <Link
+          to="/"
+          className="cardAnime hover:opacity-50 hover:text-gray-500 hover:underline text-center flex flex-col items-center h-full p-4 m-5 bg-white rounded"
+        >
+          <img
+            src={Db}
+            width={250}
+            className="mb-5 rounded-lg "
+          />
+          <p className="font-bold uppercase text-2xl">Dragon Ball GT</p>
+        </Link>
 
-        <div className="cardAnime text-center flex flex-col items-center h-full p-4 mx-5">
-          <Link to="/">
-            <img
-              src={Db}
-              width={250}
-              className="mb-5 rounded-lg hover:opacity-50"
-            />
-          </Link>
-          <p className="text-white font-bold uppercase text-2xl">Dragon Ball Heroes</p>
-        </div>
+        <Link
+          to="/"
+          className="cardAnime hover:opacity-50 hover:text-gray-500 hover:underline text-center flex flex-col items-center h-full p-4 m-5 bg-white rounded"
+        >
+          <img
+            src={Db}
+            width={250}
+            className="mb-5 rounded-lg "
+          />
+          <p className="font-bold uppercase text-2xl">Dragon Ball Heroes</p>
+        </Link>
       </div>
     </div>
   );
