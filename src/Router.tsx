ab90d8eb4c0086ba080bdba3880ba2259ee1940e
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-12-03 14:54:49 -0300

    Movie Screen created

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,15 @@
 import { Routes, Route } from "react-router-dom";
+
 import Inicio from "./screens/Inicio";
 import Animes from "./screens/Animes";
+import Movies from "./screens/Movies";
 
 export function Router(){
     return(
         <Routes>
             <Route path="/" element={<Inicio />} />
             <Route path="/animes" element={<Animes />} />
+            <Route path="/filmes" element={<Movies />} />
         </Routes>
     );
 }
