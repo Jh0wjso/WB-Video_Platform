b8962153bda454ea2299b9579d0ad1e9805f29f7
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-12-05 09:27:54 -0300

    Creating tittle

--- src/screens/Animes/index.tsx
+++ src/screens/Animes/index.tsx
@@ -7,6 +7,9 @@ import "./styles.css";
 export default function Animes() {
   return (
     <div className="min-h-screen text-white text-xl md:text-base flex flex-col items-center justify-center">
+      <h1 className="text-3xl md:text-5xl font-bold text-center my-5">
+        Assista a todos os episódios
+      </h1>
       <div className="md:my-14 cardsContainer items-center justify-between flex md:flex-row flex-col">
         <div className="cardAnime text-center flex flex-col items-center h-full p-4 m-5 bg-[transparent] border rounded">
           <img src={Db} width={250} className="mb-5 rounded-lg " />
