56d4271c70dcafad94e455110a49188a9f3cac21
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-12-19 11:14:50 -0300

    Db classsic season three content

---
+++ src/components/DragonBall/SeasonThree/index.tsx
@@ -0,0 +1,330 @@
+import { Link } from "react-router-dom";
+import "../styles.css";
+
+export default function SeasonThree() {
+  return (
+    <div className="flex flex-col border rounded mb-5 p-5">
+      <h1 className="text-center text-4xl font-bold">Saga da Red Ribbon</h1>
+      <section className="mt-8 flex flex-col items-center">
+        <section className="episodeButtons">
+          <Link
+            to="/video/O rio que pose se mudar/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 29
+          </Link>
+          <Link
+            to="/video/O Mariciano e a Patrulha Misteriosa/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 30
+          </Link>
+          <Link
+            to="/video/Goku Pensou ter encontrado a esfera/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 31
+          </Link>
+          <Link
+            to="/video/A Fortaleza Voadora Desaparece/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 32
+          </Link>
+          <Link
+            to="/video/A Lenda do Dragão/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 33
+          </Link>
+          <Link
+            to="/video/A Força Red Ribbon e o seu Cruel Comandante/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 34
+          </Link>
+        </section> 
+        <section className="episodeButtons">
+          <Link
+            to="/video/Suno, a meninda da aldeia do norte/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 35
+          </Link>
+          <Link
+            to="/video/A batalha com o sargente metalíco/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 36
+          </Link>
+          <Link
+            to="/video/Chegou o Ninja Purpura/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 37
+          </Link>
+          <Link
+            to="/video/Cinco Ninjas contra Goku/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 38
+          </Link>
+          <Link
+            to="/video/O encontro com o Misterioso Android Nº8/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 39
+          </Link>
+          <Link
+            to="/video/A luta de Goku contra p terrível Buyon/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 40
+          </Link>
+        </section>
+        <section className="episodeButtons">
+          <Link
+            to="/video/O último dia na Torre da Força/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 41
+          </Link>
+          <Link
+            to="/video/O segredo inseperado do Dr. Frapê/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 42
+          </Link>
+          <Link
+            to="/video/A casa de Bulma a Oeste de Metrópole/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 43
+          </Link>
+          <Link
+            to="/video/Goku e seus amigos estão em apuros/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 44
+          </Link>
+          <Link
+            to="/video/Cuidado, é uma armdilha/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 45
+          </Link>
+          <Link
+            to="/video/O completo freacasso de Bulma/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 46
+          </Link>
+        </section>
+        <section className="episodeButtons">
+          <Link
+            to="/video/Encontrando a casa do Mestre Kame/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 47
+          </Link>
+          <Link
+            to="/video/O General Blue inicia o ataque/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 48
+          </Link>
+          <Link
+            to="/video/Cuidado com o Inimigo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 49
+          </Link>
+          <Link
+            to="/video/O segredo dos piratas/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 50
+          </Link>
+          <Link
+            to="/video/O guardião das Profundesas/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 51
+          </Link>
+          <Link
+            to="/video/Genial, encontramos o tesouro/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 52
+          </Link>
+        </section>
+        <section className="episodeButtons">
+          <Link
+            to="/video/A caverna sinistra/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 53
+          </Link>
+          <Link
+            to="/video/A escapda/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 54
+          </Link>
+          <Link
+            to="/video/O caminho para a Vila Pinguim/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 55
+          </Link>
+          <Link
+            to="/video/Arale viaja na Nuvem Voadora/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 56
+          </Link>
+          <Link
+            to="/video/Aralae contra o General Blue/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 57
+          </Link>
+          <Link
+            to="/video/A Terra Sagrada/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 58
+          </Link>
+        </section>
+        <section className="episodeButtons">
+          <Link
+            to="/video/Tao Pai Pai, o Exterminador mais famoso do Mundo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 59
+          </Link>
+          <Link
+            to="/video/A força Kame-Hame-Ha contra o Tap Pai Pai/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 60
+          </Link>
+          <Link
+            to="/video/A Torre do Mestre Karin/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 61
+          </Link>
+          <Link
+            to="/video/Os benefícios da água sagrada/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 62
+          </Link>
+          <Link
+            to="/video/Goku contra-ataca/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 63
+          </Link>
+          <Link
+            to="/video/O último Tao Pai Pai/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 64
+          </Link>
+        </section>
+        <section className="episodeButtons">
+          <Link
+            to="/video/Vamos Goku! Começa aluta!/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 65
+          </Link>
+          <Link
+            to="/video/O último empenho da força Red Ribbon/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 66
+          </Link>
+          <Link
+            to="/video/O comandante da força Red Ribbon morre!/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 67
+          </Link>
+          <Link
+            to="/video/A ultima esfera do dragão/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 68
+          </Link>
+          <Link
+            to="/video/Quem é a Vovó Uranai?/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 69
+          </Link>
+          <Link
+            to="/video/O ataque dos cinco guerreiros/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 70
+          </Link>
+        </section>
+        <section className="episodeButtons">
+          <Link
+            to="/video/Uma batalha sangrenta e mortal/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 71
+          </Link>
+          <Link
+            to="/video/A luta de Goku na Piscina do Diabo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 72
+          </Link>
+          <Link
+            to="/video/O que faz o esplendor mortal do Diabo?/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 73
+          </Link>
+          <Link
+            to="/video/O quinto guerreiro misterioso/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 74
+          </Link>
+          <Link
+            to="/video/Como um adversario se tornou um velho amigo?/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 75
+          </Link>
+          <Link
+            to="/video/A verdadeira identidade do Homem Mascarado/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 76
+          </Link>
+        </section>
+        <section className="episodeButtons">
+          <Link
+            to="/video/O grande plano de Pilaf/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 77
+          </Link>
+          <Link
+            to="/video/O reaparecimento do Deus Dragão/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 78
+          </Link>
+        </section>
+      </section>
+    </div>
+  );
+}
--- src/screens/ClassicScreen/index.tsx
+++ src/screens/ClassicScreen/index.tsx
@@ -1,4 +1,5 @@
 import SeasonOne from "../../components/DragonBall/SeasonOne";
+import SeasonThree from "../../components/DragonBall/SeasonThree";
 import SeasonTwo from "../../components/DragonBall/SeasonTwo";
 import DragonBallAbstract from "../../components/DragonBallAbstract";
 import "./styles.css";
@@ -11,6 +12,7 @@ export default function ClassicSceen() {
       <section className="flex flex-col w-[80%]">
         <SeasonOne />
         <SeasonTwo />
+        <SeasonThree />
       </section>
     </div>
   );
