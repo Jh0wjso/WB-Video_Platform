c746b04c0aac54651c0ee25afd6d3ce78ab9fb55
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-12-21 11:43:34 -0300

    Dragon Ball finished

--- src/components/DragonBall/SeasonFive/index.tsx
+++ src/components/DragonBall/SeasonFive/index.tsx
@@ -8,37 +8,37 @@ export default function SeasonFive() {
       <section className="mt-8 flex flex-col items-center">
         <section className="episodeButtons">
           <Link
-            to="/video/O segredo das Esferas do Dragão/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/A morte de Kuririn foi um complô/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 102
           </Link>
           <Link
-            to="/video/A busca do imperador/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/O temido Piccolo Daimaoh/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 103
           </Link>
           <Link
-            to="/video/A nuvem voadora do Kame/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/A volta de Goku/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 104
           </Link>
           <Link
-            to="/video/Oolong, o sequestrador de garotas/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/O grnade Yajirobe/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 105
           </Link>
           <Link
-            to="/video/O forte e malvado Yamcha vive no deserto/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/O malvado tamborin se aproxima/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 106
           </Link>
           <Link
-            to="/video/Vigie as Esferas do Dragão/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Goku fica muto bravo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 107
@@ -46,37 +46,37 @@ export default function SeasonFive() {
         </section>
         <section className="episodeButtons">
           <Link
-            to="/video/O rei Cutelo na montanha do fogo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/A aparição de Piccolo Daimaoh/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 108
           </Link>
           <Link
-            to="/video/O Kame-Hame-Ha de Kame/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Goku enfrenta Piccolo Daimaoh/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 109
           </Link>
           <Link
-            to="/video/O truque especial do chefe Coelho/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Vai que é tua Goku/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 110
           </Link>
           <Link
-            to="/video/As Esferas do Dragão são roubadas/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/O último Mafuba do Mestre Kame/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 111
           </Link>
           <Link
-            to="/video/O Dragão é chamado/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Será que Piccolo Daimaoh vai recuperar sua juventude/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 112
           </Link>
           <Link
-            to="/video/Um pedido feito ao Deus Dragão/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Ataque ao castelo real/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 113
@@ -84,37 +84,37 @@ export default function SeasonFive() {
         </section>
         <section className="episodeButtons">
           <Link
-            to="/video/O rei Cutelo na montanha do fogo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/A esperança de Goku/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 114
           </Link>
           <Link
-            to="/video/O Kame-Hame-Ha de Kame/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Em busca da misteriosa Água dos Deuses/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 115
           </Link>
           <Link
-            to="/video/O truque especial do chefe Coelho/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Mestre Kame está vivo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 116
           </Link>
           <Link
-            to="/video/As Esferas do Dragão são roubadas/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Goku volta a si/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 117
           </Link>
           <Link
-            to="/video/O Dragão é chamado/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/A decisão de Tenshinhan/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 118
           </Link>
           <Link
-            to="/video/Um pedido feito ao Deus Dragão/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/O famoso golpe Mafuba/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 119
@@ -122,37 +122,37 @@ export default function SeasonFive() {
         </section>
         <section className="episodeButtons">
           <Link
-            to="/video/O rei Cutelo na montanha do fogo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Goku fica irado/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 120
           </Link>
           <Link
-            to="/video/O Kame-Hame-Ha de Kame/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/O momento crucial de Goku/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 121
           </Link>
           <Link
-            to="/video/O truque especial do chefe Coelho/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/O último esforço/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 122
           </Link>
           <Link
-            to="/video/As Esferas do Dragão são roubadas/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/O segredo do Bastão Mágico/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 123
           </Link>
           <Link
-            to="/video/O Dragão é chamado/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Goku chega à Plataforma Celeste/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 124
           </Link>
           <Link
-            to="/video/Um pedido feito ao Deus Dragão/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/A aparição de Kami-Sama/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 125
@@ -160,37 +160,37 @@ export default function SeasonFive() {
         </section>
         <section className="episodeButtons">
           <Link
-            to="/video/O rei Cutelo na montanha do fogo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Shen Long volta a viver/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 126
           </Link>
           <Link
-            to="/video/O Kame-Hame-Ha de Kame/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Mais rápido que um relâmpago/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 127
           </Link>
           <Link
-            to="/video/O truque especial do chefe Coelho/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Tranquilo como o céu/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 128
           </Link>
           <Link
-            to="/video/As Esferas do Dragão são roubadas/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Goku faz uma viagem no tempo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 129
           </Link>
           <Link
-            to="/video/O Dragão é chamado/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/O adversário de Goku é Goku/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 130
           </Link>
           <Link
-            to="/video/Um pedido feito ao Deus Dragão/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Cada um segue um caminho diferente/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 131
@@ -198,37 +198,37 @@ export default function SeasonFive() {
         </section>
         <section className="episodeButtons">
           <Link
-            to="/video/O rei Cutelo na montanha do fogo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Mais quente que a lava/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 132
           </Link>
           <Link
-            to="/video/O Kame-Hame-Ha de Kame/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Um reencontro depois da tormenta/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 133
           </Link>
           <Link
-            to="/video/O truque especial do chefe Coelho/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/O Torneio de Artes Marciais já começou/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 134
           </Link>
           <Link
-            to="/video/As Esferas do Dragão são roubadas/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Os oito lutadores/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 135
           </Link>
           <Link
-            to="/video/O Dragão é chamado/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/O contra-ataque do assassino Tao Pai Pai/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 136
           </Link>
           <Link
-            to="/video/Um pedido feito ao Deus Dragão/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/A promessa de Goku/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 137
@@ -236,37 +236,37 @@ export default function SeasonFive() {
         </section>
         <section className="episodeButtons">
           <Link
-            to="/video/O rei Cutelo na montanha do fogo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/O misterioso homem chamado Sheng/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 138
           </Link>
           <Link
-            to="/video/O Kame-Hame-Ha de Kame/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Mais uma luta violenta: Goku contra Tenshinhan/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 139
           </Link>
           <Link
-            to="/video/O truque especial do chefe Coelho/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/O verdadeiro poder/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 140
           </Link>
           <Link
-            to="/video/As Esferas do Dragão são roubadas/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Quatro Tenshinhans/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 141
           </Link>
           <Link
-            to="/video/O Dragão é chamado/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Quem será o mais forte? Kami-Sama enfrenta Piccolo Daimaoh/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 142
           </Link>
           <Link
-            to="/video/Um pedido feito ao Deus Dragão/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/O destino do mundo corre perigo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 143
@@ -274,37 +274,37 @@ export default function SeasonFive() {
         </section>
         <section className="episodeButtons">
           <Link
-            to="/video/O rei Cutelo na montanha do fogo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/O super Kame-Hame-Ha/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 144
           </Link>
           <Link
-            to="/video/O Kame-Hame-Ha de Kame/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/O gigante Piccolo Daimaoh/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 145
           </Link>
           <Link
-            to="/video/O truque especial do chefe Coelho/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/O truque de Goku/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 146
           </Link>
           <Link
-            to="/video/As Esferas do Dragão são roubadas/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Tudo está perdido/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 147
           </Link>
           <Link
-            to="/video/O Dragão é chamado/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/O homem mais forte do mundo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 148
           </Link>
           <Link
-            to="/video/Um pedido feito ao Deus Dragão/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/O vestido de noiva cercado de chamas/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 149
@@ -312,25 +312,25 @@ export default function SeasonFive() {
         </section>
         <section className="episodeButtons">
           <Link
-            to="/video/O rei Cutelo na montanha do fogo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/A ave engole-fogo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 150
           </Link>
           <Link
-            to="/video/O Kame-Hame-Ha de Kame/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Chi-Chi treina muito para ser uma boa esposa/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 151
           </Link>
           <Link
-            to="/video/O rei Cutelo na montanha do fogo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/Depressa Goku! O mistério da Montanha dos Cinco Elementos/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 152
           </Link>
           <Link
-            to="/video/O rei Cutelo na montanha do fogo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            to="/video/A montanha Fry-Pan está em chamas!.../1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
             className="linkToEpisode"
           >
             Episodio 152
