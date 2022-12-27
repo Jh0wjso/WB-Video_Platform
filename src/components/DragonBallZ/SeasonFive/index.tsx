37c699d4c173675d296ed810042ed609e1a3c300
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-12-27 14:32:52 -0300

    New Dragon Ball Z Movie

---
+++ src/components/DragonBallZ/SeasonFive/index.tsx
@@ -0,0 +1,152 @@
+import { Link } from "react-router-dom";
+import "../styles.css";
+
+export default function SeasonFiveBbz() {
+  return (
+    <div className="flex flex-col border mt-5 rounded p-5">
+      <h1 className="text-center text-4xl font-bold">Saga dos Androides</h1>
+      <section className="mt-8 flex flex-col items-center">
+        <section className="episodeButtons">
+          <Link
+            to="/video/Este é o planeta Terra papai/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 118
+          </Link>
+          <Link
+            to="/video/Eu me encarregarei de vencer Freeza/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 119
+          </Link>
+          <Link
+            to="/video/O fim de Freeza/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 120
+          </Link>
+          <Link
+            to="/video/Goku voltou/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 121
+          </Link>
+          <Link
+            to="/video/A identidade do jovem misterioso/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 122
+          </Link>
+          <Link
+            to="/video/A técnica especial de Goku/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 123
+          </Link>
+        </section>
+        <section className="episodeButtons">
+          <Link
+            to="/video/Vou superar Goku/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 124
+          </Link>
+          <Link
+            to="/video/Uma prova muito difícil para Goku/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 125
+          </Link>
+          <Link
+            to="/video/Quem são os androides/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 126
+          </Link>
+          <Link
+            to="/video/Os terríveis androides Nº 19 e 20/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 127
+          </Link>
+          <Link
+            to="/video/Goku tem dois problemas/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 128
+          </Link>
+          <Link
+            to="/video/Vegeta se transforma num super Saiyajin/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 129
+          </Link>
+        </section>
+        <section className="episodeButtons">
+          <Link
+            to="/video/O segredo do Dr. Maki/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 130
+          </Link>
+          <Link
+            to="/video/A realidade é muito pior do que o futuro/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 131
+          </Link>
+          <Link
+            to="/video/É preciso encontrar o laboratório do Dr. Maki/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 132
+          </Link>
+          <Link
+            to="/video/Os androides nº 17 e 18 despertam/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 133
+          </Link>
+          <Link
+            to="/video/O exército encarregado de matar Goku/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 134
+          </Link>
+          <Link
+            to="/video/As habilidade do androide nº 18/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 135
+          </Link>
+        </section>
+        <section className="episodeButtons">
+          <Link
+            to="/video/Será este o fim dos guerreiros Z/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 136
+          </Link>
+          <Link
+            to="/video/A decisão de Piccolo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 137
+          </Link>
+          <Link
+            to="/video/Os androides destruidores/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 138
+          </Link>
+          <Link
+            to="/video/Bulma descobre um grande mistério/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+            className="linkToEpisode"
+          >
+            Episodio 139
+          </Link>
+        </section>
+      </section>
+    </div>
+  );
+}
--- src/screens/DBZScreen/index.tsx
+++ src/screens/DBZScreen/index.tsx
@@ -5,12 +5,13 @@ import MovieContentZ from "../../components/DragonBallZ/MovieContentZ";
 import SeasonTwoBbz from "../../components/DragonBallZ/SeasonTwo";
 import SeasonThreeBbz from "../../components/DragonBallZ/SeasonThree";
 import SeasonFourBbz from "../../components/DragonBallZ/SeasonFour";
+import SeasonFiveBbz from "../../components/DragonBallZ/SeasonFive";
 
 export default function DBZScreen() {
   return (
     <div className="min-h-screen flex flex-col items-center">
       <h1 className="text-center text-5xl py-8 font-bold">
-        Assista a Todos os Episodios Aqui:
+        Assista a Todos os Episódios Aqui:
       </h1>
       <DragonBallZAbstract />
       <section className="flex flex-col w-[80%]">
@@ -18,6 +19,7 @@ export default function DBZScreen() {
         <SeasonTwoBbz />
         <SeasonThreeBbz />
         <SeasonFourBbz />
+        <SeasonFiveBbz />
       </section>
       <MovieContentZ />
     </div>
