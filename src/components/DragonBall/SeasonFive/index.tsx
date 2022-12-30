431dac7904e1a66124e5df48900e01be23bc452b
Jh0wjso <jhonatansilverio1258@gmail.com>
2022-12-30 14:44:02 -0300

    Restructuring Dragon Ball season five code

--- src/components/DragonBall/SeasonFive/index.tsx
+++ src/components/DragonBall/SeasonFive/index.tsx
@@ -1,340 +1,90 @@
 import { Link } from "react-router-dom";
 import "../styles.css";
+import SectionLinks from "../../SectionLinks";
+import SectionButtons from "../../SectionButtons";
 
 export default function SeasonFive() {
   return (
     <div className="flex flex-col border rounded mb-5 p-5">
-      <h1 className="text-center text-4xl font-bold">Saga de Piccolo Daimao e Piccolo Jr.</h1>
+      <h1 className="text-center text-4xl font-bold">Saga de Piccolo Daimaoh e Piccolo Jr.</h1>
       <section className="mt-8 flex flex-col items-center">
+        <SectionButtons 
+          num1="102" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="A morte de Kuririn foi um complô"
+          num2="103" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="O temido Piccolo Daimaoh"
+          num3="104" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="A volta de Goku"
+          num4="105" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="O grande Yajirobe"
+          num5="106" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="O malvado Tamborin se aproxima"
+          num6="107" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Goku fica muto bravo"
+        />
+
+        <SectionButtons 
+          num1="108" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="A aparição de Piccolo Daimaoh"
+          num2="109" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="Goku enfrenta Piccolo Daimaoh"
+          num3="110" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Vai que é tua Goku"
+          num4="111" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="O último Mafuba do Mestre K"
+          num5="112" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Será que Piccolo Daimaoh vai recuperar sua juventude"
+          num6="113" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Ataque ao castelo real"
+        />
+
+        <SectionButtons 
+          num1="114" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="A esperança de Goku"
+          num2="115" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="Em busca da misteriosa Água dos Deuses"
+          num3="116" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Mestre Kame está vivo"
+          num4="117" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Goku volta a si"
+          num5="118" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="A decisão de Tenshinhan"
+          num6="119" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="O famoso golpe Mafuba"
+        />
+
+        <SectionButtons 
+          num1="120" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Goku fica irado"
+          num2="121" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="O momento crucial de Goku"
+          num3="122" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="O último esforço"
+          num4="123" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="O segredo do Bastão Mágico"
+          num5="124" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Goku chega à Plataforma Celeste"
+          num6="125" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="A aparição de Kami-Sama"
+        />
+
+        <SectionButtons 
+          num1="126" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="ShenLong volta a viver"
+          num2="127" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="Mais rápido que um relâmpago"
+          num3="128" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Tranquilo como o céu"
+          num4="129" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Goku faz uma viagem no tempo"
+          num5="130" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="O adversário de Goku é Goku"
+          num6="131" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Cada um segue um caminho diferente"
+        />
+
+        <SectionButtons 
+          num1="132" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Mais quente que a lava"
+          num2="133" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="Um reencontro depois da tormenta"
+          num3="134" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="O Torneio de Artes Marciais já começou"
+          num4="135" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Os oito lutadores"
+          num5="136" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="O contra-ataque do assassino Tao Pai Pai"
+          num6="137" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="A promessa de Goku"
+        />
+
+        <SectionButtons 
+          num1="138" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="O misterioso homem chamado Sheng"
+          num2="139" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="Mais uma luta violenta: Goku contra Tenshinhan"
+          num3="140" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="O verdadeiro poder"
+          num4="141" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Quatro Tenshinhans"
+          num5="142" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Quem será o mais forte? Kami-Sama enfrenta Piccolo Daimaoh"
+          num6="143" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="O destino do mundo corre perigo"
+        />
+
+        <SectionButtons 
+          num1="144" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="O super Kame-Hame-Ha"
+          num2="145" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="O gigante Piccolo Daimaoh"
+          num3="146" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="O truque de Goku"
+          num4="147" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Tudo está perdido"
+          num5="148" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="O homem mais forte do mundo"
+          num6="149" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="O vestido de noiva cercado de chamas"
+        />
+
         <section className="episodeButtons">
-          <Link
-            to="/video/A morte de Kuririn foi um complô/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 102
-          </Link>
-          <Link
-            to="/video/O temido Piccolo Daimaoh/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 103
-          </Link>
-          <Link
-            to="/video/A volta de Goku/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 104
-          </Link>
-          <Link
-            to="/video/O grnade Yajirobe/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 105
-          </Link>
-          <Link
-            to="/video/O malvado tamborin se aproxima/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 106
-          </Link>
-          <Link
-            to="/video/Goku fica muto bravo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 107
-          </Link>
-        </section>
-        <section className="episodeButtons">
-          <Link
-            to="/video/A aparição de Piccolo Daimaoh/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 108
-          </Link>
-          <Link
-            to="/video/Goku enfrenta Piccolo Daimaoh/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 109
-          </Link>
-          <Link
-            to="/video/Vai que é tua Goku/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 110
-          </Link>
-          <Link
-            to="/video/O último Mafuba do Mestre Kame/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 111
-          </Link>
-          <Link
-            to="/video/Será que Piccolo Daimaoh vai recuperar sua juventude/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 112
-          </Link>
-          <Link
-            to="/video/Ataque ao castelo real/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 113
-          </Link>
-        </section>
-        <section className="episodeButtons">
-          <Link
-            to="/video/A esperança de Goku/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 114
-          </Link>
-          <Link
-            to="/video/Em busca da misteriosa Água dos Deuses/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 115
-          </Link>
-          <Link
-            to="/video/Mestre Kame está vivo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 116
-          </Link>
-          <Link
-            to="/video/Goku volta a si/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 117
-          </Link>
-          <Link
-            to="/video/A decisão de Tenshinhan/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 118
-          </Link>
-          <Link
-            to="/video/O famoso golpe Mafuba/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 119
-          </Link>
-        </section>
-        <section className="episodeButtons">
-          <Link
-            to="/video/Goku fica irado/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 120
-          </Link>
-          <Link
-            to="/video/O momento crucial de Goku/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 121
-          </Link>
-          <Link
-            to="/video/O último esforço/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 122
-          </Link>
-          <Link
-            to="/video/O segredo do Bastão Mágico/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 123
-          </Link>
-          <Link
-            to="/video/Goku chega à Plataforma Celeste/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 124
-          </Link>
-          <Link
-            to="/video/A aparição de Kami-Sama/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 125
-          </Link>
-        </section>
-        <section className="episodeButtons">
-          <Link
-            to="/video/Shen Long volta a viver/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 126
-          </Link>
-          <Link
-            to="/video/Mais rápido que um relâmpago/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 127
-          </Link>
-          <Link
-            to="/video/Tranquilo como o céu/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 128
-          </Link>
-          <Link
-            to="/video/Goku faz uma viagem no tempo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 129
-          </Link>
-          <Link
-            to="/video/O adversário de Goku é Goku/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 130
-          </Link>
-          <Link
-            to="/video/Cada um segue um caminho diferente/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 131
-          </Link>
-        </section>
-        <section className="episodeButtons">
-          <Link
-            to="/video/Mais quente que a lava/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 132
-          </Link>
-          <Link
-            to="/video/Um reencontro depois da tormenta/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 133
-          </Link>
-          <Link
-            to="/video/O Torneio de Artes Marciais já começou/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 134
-          </Link>
-          <Link
-            to="/video/Os oito lutadores/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 135
-          </Link>
-          <Link
-            to="/video/O contra-ataque do assassino Tao Pai Pai/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 136
-          </Link>
-          <Link
-            to="/video/A promessa de Goku/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 137
-          </Link>
-        </section>
-        <section className="episodeButtons">
-          <Link
-            to="/video/O misterioso homem chamado Sheng/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 138
-          </Link>
-          <Link
-            to="/video/Mais uma luta violenta: Goku contra Tenshinhan/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 139
-          </Link>
-          <Link
-            to="/video/O verdadeiro poder/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 140
-          </Link>
-          <Link
-            to="/video/Quatro Tenshinhans/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 141
-          </Link>
-          <Link
-            to="/video/Quem será o mais forte? Kami-Sama enfrenta Piccolo Daimaoh/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 142
-          </Link>
-          <Link
-            to="/video/O destino do mundo corre perigo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 143
-          </Link>
-        </section>
-        <section className="episodeButtons">
-          <Link
-            to="/video/O super Kame-Hame-Ha/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 144
-          </Link>
-          <Link
-            to="/video/O gigante Piccolo Daimaoh/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 145
-          </Link>
-          <Link
-            to="/video/O truque de Goku/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 146
-          </Link>
-          <Link
-            to="/video/Tudo está perdido/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 147
-          </Link>
-          <Link
-            to="/video/O homem mais forte do mundo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 148
-          </Link>
-          <Link
-            to="/video/O vestido de noiva cercado de chamas/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 149
-          </Link>
-        </section>
-        <section className="episodeButtons">
-          <Link
-            to="/video/A ave engole-fogo/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 150
-          </Link>
-          <Link
-            to="/video/Chi-Chi treina muito para ser uma boa esposa/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 151
-          </Link>
-          <Link
-            to="/video/Depressa Goku! O mistério da Montanha dos Cinco Elementos/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 152
-          </Link>
-          <Link
-            to="/video/A montanha Fry-Pan está em chamas!.../1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-            className="linkToEpisode"
-          >
-            Episodio 152
-          </Link>
+          <SectionLinks num="150" url="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name="A ave engole-fogo" />
+          <SectionLinks num="151" url="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name="Chi-Chi treina muito para ser uma boa esposa" />
+          <SectionLinks num="152" url="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name="Depressa Goku! O mistério da Montanha dos Cinco Elem" />
+          <SectionLinks num="153" url="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name="A montanha Fry-Pan está em chamas" />
         </section>
       </section>
     </div>
