a124c1971365a87476f5ed309eb50bccf537451b
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-01-02 20:01:12 -0300

    DbGT season componts createds

---
+++ src/components/DragonBallGT/SeasonFinal/index.tsx
@@ -0,0 +1,12 @@
+import "../styles.css";
+import SectionButtons from "../../SectionButtons";
+import SectionLinks from "../../SectionLinks";
+
+export default function SeasonFinalGT() {
+  return (
+    <div className="flex flex-col border rounded p-5">
+      <h1 className="text-center text-4xl font-bold">Saga dos Dragões Malignos</h1>
+      <section className="mt-8 flex flex-col items-center"></section>
+    </div>
+  );
+}
---
+++ src/components/DragonBallGT/SeasonOne/index.tsx
@@ -0,0 +1,12 @@
+import "../styles.css";
+import SectionButtons from "../../SectionButtons";
+import SectionLinks from "../../SectionLinks";
+
+export default function SeasonOneGT() {
+  return (
+    <div className="flex flex-col border rounded p-5">
+      <h1 className="text-center text-4xl font-bold">Saga Viagem Pelo Universo</h1>
+      <section className="mt-8 flex flex-col items-center"></section>
+    </div>
+  );
+}
