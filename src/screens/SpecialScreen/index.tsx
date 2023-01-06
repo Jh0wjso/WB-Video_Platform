7a3ab6d32c25d6c0e5e5f519c4c0e73e8e66403d
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-01-06 15:19:02 -0300

    OVAs finisheds

--- src/screens/SpecialScreen/index.tsx
+++ src/screens/SpecialScreen/index.tsx
@@ -16,7 +16,7 @@ export default function SpecialScreen() {
           <p className="font-bold">A Batalha de Freeza Contra o Pai de Goku</p>
           <div className="btnContent w-[80%] text-xl md:text-sm my-1 items-center justify-center">
             <Link
-              to="/assistir/A Batalha de Freeza Contra o Pai de Goku/"
+              to="/assistir/A Batalha de Freeza Contra o Pai de Goku/wWkPs1R8z3wKP_ogXP2lRJRpd"
               className="text-center bg-orange-400 text-xl md:text-xs m-2 w-[75%] md:w-auto md:m-2 hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
             >
               Assistir
@@ -28,19 +28,19 @@ export default function SpecialScreen() {
           <p className="font-bold">Gohan e Trunks, os Guerreiros do Futuro</p>
           <div className="btnContent w-[80%] text-xl md:text-sm my-1 items-center justify-center">
             <Link
-              to="/assistir/Gohan e Trunks, os Guerreiros do Futuro/"
+              to="/assistir/Gohan e Trunks, os Guerreiros do Futuro/1qMGg4cfSfAuhmAVFeQ2umS-kQtUE87Va"
               className="text-center bg-orange-400 text-xl md:text-xs m-2 w-[75%] md:w-auto md:m-2 hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
             >
               Assistir
             </Link>
           </div>
         </div>
-        <div className="cardAnime text-center flex flex-col items-center h-full p-4 m-5 bg-[transparent] border rounded">
+        <div className="cardAnime text-center flex flex-col items-center h-full p-7 m-5 bg-[transparent] border rounded">
           <img src={Tres} width={250} className="mb-5 rounded-lg " />
-          <p className="font-bold">Toriko X One piece & Dragon ball z</p>
+          <p className="font-bold">Toriko X One piece X DBZ</p>
           <div className="btnContent w-[80%] text-xl md:text-sm my-1 items-center justify-center">
             <Link
-              to="/assistir/Uma Aventura Mistica/"
+              to="/assistir/Toriko X One piece X DBZ/1qLRSpvcSUmT7FMAi6oQCRrW6F6m_dGPe"
               className="text-center bg-orange-400 text-xl md:text-xs m-2 w-[75%] md:w-auto md:m-2 hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
             >
               Assistir
