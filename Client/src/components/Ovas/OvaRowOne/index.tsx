e632c6983d78f618e6babd8b2f61c394c832f2b4
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-01-12 10:56:04 -0300

    Ova styles finished

--- Client/src/components/Ovas/OvaRowOne/index.tsx
+++ Client/src/components/Ovas/OvaRowOne/index.tsx
@@ -7,50 +7,50 @@ import Quatro from "../../../images/Ovas/4.jpeg";
 
 export default function OvaRowOne() {
   return (
-    <div className="cardsContainer items-center justify-between flex md:flex-row flex-col">
-      <div className="cardAnime text-center flex flex-col items-center h-full p-4 m-5 bg-[transparent] border rounded">
-        <img src={Um} width={250} className="mb-5 rounded-lg " />
+    <div className="cardsContainer">
+      <div className="cardAnime">
+        <img src={Um} width={250} className="imageCard" />
         <p className="font-bold">Segurança no trânsito</p>
-        <div className="btnContent w-[80%] text-xl md:text-sm my-1 items-center justify-center">
+        <div className="btnContent">
           <Link
             to="/assistir/Segurança no trânsito/1WKQ4j_18wcNOJjrqU8L7l9eSTb9NGeJo"
-            className="text-center bg-orange-400 text-xl md:text-xs m-2 w-[75%] md:w-auto md:m-2 hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            className="btnWatch"
           >
             Assistir
           </Link>
         </div>
       </div>
-      <div className="cardAnime text-center flex flex-col items-center h-full p-4 m-5 bg-[transparent] border rounded">
-        <img src={Dois} width={250} className="mb-5 rounded-lg " />
+      <div className="cardAnime">
+        <img src={Dois} width={250} className="imageCard" />
         <p className="font-bold">O Corpo de Bombeiros de Goku</p>
-        <div className="btnContent w-[80%] text-xl md:text-sm my-1 items-center justify-center">
+        <div className="btnContent">
           <Link
             to="/assistir/O Corpo de Bombeiros de Goku/1cUGipvHu9vGjorpenSdIDO0XcnzB3anB"
-            className="text-center bg-orange-400 text-xl md:text-xs m-2 w-[75%] md:w-auto md:m-2 hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            className="btnWatch"
           >
             Assistir
           </Link>
         </div>
       </div>
-      <div className="cardAnime text-center flex flex-col items-center h-full p-4 m-5 bg-[transparent] border rounded">
-        <img src={Tres} width={250} className="mb-5 rounded-lg " />
+      <div className="cardAnime">
+        <img src={Tres} width={250} className="imageCard" />
         <p className="font-bold">Junte-se a nós! O Mundo de Goku</p>
-        <div className="btnContent w-[80%] text-xl md:text-sm my-1 items-center justify-center">
+        <div className="btnContent">
           <Link
             to="/assistir/Junte-se a nós! O Mundo de Goku/1IxGJwpk8bmEiloVsXSkReqtkpBYZwFri"
-            className="text-center bg-orange-400 text-xl md:text-xs m-2 w-[75%] md:w-auto md:m-2 hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            className="btnWatch"
           >
             Assistir
           </Link>
         </div>
       </div>
-      <div className="cardAnime text-center flex flex-col items-center h-full p-4 m-5 bg-[transparent] border rounded">
-        <img src={Quatro} width={250} className="mb-5 rounded-lg " />
+      <div className="cardAnime">
+        <img src={Quatro} width={250} className="imageCard" />
         <p className="font-bold">O Plano Para Erradicar os Saiyajins</p>
-        <div className="btnContent w-[80%] text-xl md:text-sm my-1 items-center justify-center">
+        <div className="btnContent">
           <Link
             to="/assistir/O Plano Para Erradicar os Saiyajins/1BCWfFWHzdTJlVZKj9nyOhOw0W_y62Mwx"
-            className="text-center bg-orange-400 text-xl md:text-xs m-2 w-[75%] md:w-auto md:m-2 hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            className="btnWatch"
           >
             Assistir
           </Link>
--- Client/src/components/Ovas/OvaRowTwo/index.tsx
+++ Client/src/components/Ovas/OvaRowTwo/index.tsx
@@ -7,50 +7,50 @@ import Oito from "../../../images/Ovas/8.jpeg";
 
 export default function OvaRowTwo() {
   return (
-    <div className="cardsContainer items-center justify-between flex md:flex-row flex-col">
-      <div className="cardAnime text-center flex flex-col items-center h-full p-4 m-5 bg-[transparent] border rounded">
-        <img src={Cinco} width={250} className="mb-5 rounded-lg " />
+    <div className="cardsContainer">
+      <div className="cardAnime">
+        <img src={Cinco} width={250} className="imageCard" />
         <p className="font-bold">Especial de 2007<br></br>(2007)</p>
-        <div className="btnContent w-[80%] text-xl md:text-sm my-1 items-center justify-center">
+        <div className="btnContent">
           <Link
             to="/assistir/Especial de 2007/1QE4zDFW7eQbuOHenuhbTOE6Qu-r_Hfme"
-            className="text-center bg-orange-400 text-xl md:text-xs m-2 w-[75%] md:w-auto md:m-2 hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            className="btnWatch"
           >
             Assistir
           </Link>
         </div>
       </div>
-      <div className="cardAnime text-center flex flex-col items-center h-full p-4 m-5 bg-[transparent] border rounded">
-        <img src={Seis} width={250} className="mb-5 rounded-lg " />
+      <div className="cardAnime">
+        <img src={Seis} width={250} className="imageCard" />
         <p className="font-bold">Ossu! O retorno de Son Goku <br></br>e seus Companheiros</p>
-        <div className="btnContent w-[80%] text-xl md:text-sm my-1 items-center justify-center">
+        <div className="btnContent">
           <Link
             to="/assistir/Ossu! O retorno de Son Goku e seus Companheiros/1HN37TqZ_yLwO5_cZLSkj_VFV1TN3rmoC"
-            className="text-center bg-orange-400 text-xl md:text-xs m-2 w-[75%] md:w-auto md:m-2 hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            className="btnWatch"
           >
             Assistir
           </Link>
         </div>
       </div>
-      <div className="cardAnime text-center flex flex-col items-center h-full p-4 m-5 bg-[transparent] border rounded">
-        <img src={Sete} width={250} className="mb-5 rounded-lg " />
+      <div className="cardAnime">
+        <img src={Sete} width={250} className="imageCard" />
         <p className="font-bold">Remake - O Plano para Erradicar <br></br> os Super Saiyajins</p>
-        <div className="btnContent w-[80%] text-xl md:text-sm my-1 items-center justify-center">
+        <div className="btnContent">
           <Link
             to="/assistir/Remake - O Plano para Erradicar os Super Saiyajins/1juZpwSDqmDQWNyPur7adNBqNIbkLk9Ri/"
-            className="text-center bg-orange-400 text-xl md:text-xs m-2 w-[75%] md:w-auto md:m-2 hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            className="btnWatch"
           >
             Assistir
           </Link>
         </div>
       </div>
-      <div className="cardAnime text-center flex flex-col items-center h-full p-4 m-5 bg-[transparent] border rounded">
-        <img src={Oito} width={250} className="mb-5 rounded-lg " />
+      <div className="cardAnime">
+        <img src={Oito} width={250} className="imageCard" />
         <p className="font-bold">Episódio de Bardock<br></br> (2011) </p>
-        <div className="btnContent w-[80%] text-xl md:text-sm my-1 items-center justify-center">
+        <div className="btnContent">
           <Link
             to="/assistir/Episódio de Bardock/1vpkfMuAUvKwBSARQB4Todl_ZYYyMsD3H"
-            className="text-center bg-orange-400 text-xl md:text-xs m-2 w-[75%] md:w-auto md:m-2 hover:bg-orange-600 hover:transition-colors text-white p-2 rounded font-bold"
+            className="btnWatch"
           >
             Assistir
           </Link>
