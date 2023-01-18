df3b27c8616a369d133ad8d1c0b06699fb524d4c
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-01-18 13:31:48 -0300

    Dragon ball hereos season six content finished

--- Client/src/components/DragonBallHeroes/SeasonSix/index.tsx
+++ Client/src/components/DragonBallHeroes/SeasonSix/index.tsx
@@ -10,24 +10,12 @@ export default function SeasonSixHeroes() {
       </h1>
       <section className="mt-8 flex flex-col items-center">
         <SectionButtons
-          num1="33"
-          url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-          name1={`Nova Guerra Espaço-Tempo! A Batalha Feroz Final Começa!`}
-          num2="34"
-          url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-          name2={`O Guerreiro de Preto Aparece! Uma Nova Aventura Começa!`}
-          num3="35"
-          url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-          name3={`O Orgulho Da Raça Guerreira! Vegeta, Despertando!!`}
-          num4="36"
-          url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-          name4={`Super Saiyajin Rosé Vs. Ultra Instinct! Um Grande Duelo Que Abala O Planeta!`}
-          num5="37"
-          url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-          name5={`Guerreiro De Preto Vs. Goku Preto! O Enredo Sombrio Se Torna Claro!`}
-          num6="38"
-          url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
-          name6={`A Batalha Final No Universo Falso! Confronto De Azul E Escarlate`}
+          num1="41" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1={`Plano Em Ação! Os Guerreiros Mais Poderosos de Todo o Espaço-Tempo se Reúnem!`}      
+          num2="42" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2={`O Ataque dos Guerreiros de Preto! A Batalha Feroz no Super Torneio Espaço-Tempo!`}
+          num3="43" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3={`Uma Batalha Feroz que Transcende o Tempo! A Ameaça dos Guerreiros de Preto!`}
+          num4="44" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4={`Goku vs O Guerreiro de Preto! Lutando pelos seus Próprios Destinos!`}
+          num5="45" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5={`A Batalha Decisiva no Reino dos Deuses! O Poder do Tempo se Aproxima!`}
+          num6="46" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6={`O Terrível Rei das Trevas Ataca! A terrível Batalha Começa!`}
         /> 
       </section>
     </div>
