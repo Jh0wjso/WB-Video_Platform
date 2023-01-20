217a871abc14d8baaee5c465899f38f9e684da37
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-01-20 10:04:12 -0300

    Update on names for dragon ball season two

--- Client/src/components/DragonBall/SeasonTwo/index.tsx
+++ Client/src/components/DragonBall/SeasonTwo/index.tsx
@@ -8,26 +8,26 @@ export default function SeasonTwo() {
       <h1 className="text-center text-4xl font-bold">Saga do Tenkaichi Budokai</h1>
       <section className="mt-8 flex flex-col items-center">
         <SectionButtons 
-          num1="14" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="O rival de Goku"
-          num2="15" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="A misteriosa garota Lanch"
-          num3="16" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Caça às pedras"
-          num4="17" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Os entregadores de leite"
-          num5="18" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Treinamento ao Estilo de Mestre Kame"
-          num6="19" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="O Grande torneio de artes marciais"
+          num1="14" url1={`1DXOes0Uc2sCOTEQtbKl5e0GRmDG1ZysH`} name1={`O rival de Goku`}
+          num2="15" url2={`1DXOes0Uc2sCOTEQtbKl5e0GRmDG1ZysH`} name2={`A misteriosa garota Lanch`}
+          num3="16" url3={`1DXOes0Uc2sCOTEQtbKl5e0GRmDG1ZysH`} name3={`Caça às pedras`}
+          num4="17" url4={`1DXOes0Uc2sCOTEQtbKl5e0GRmDG1ZysH`} name4={`Os entregadores de leite`}
+          num5="18" url5={`1DXOes0Uc2sCOTEQtbKl5e0GRmDG1ZysH`} name5={`Treinamento ao Estilo de Mestre Kame`}
+          num6="19" url6={`1DXOes0Uc2sCOTEQtbKl5e0GRmDG1ZysH`} name6={`O Grande torneio de artes marciais`}
         />
         <SectionButtons 
-          num1="20" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="O rival de Goku"
-          num2="21" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="A misteriosa garota Lanch"
-          num3="22" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Caça às pedras"
-          num4="23" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Os entregadores de leite"
-          num5="24" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Treinamento ao Estilo de Mestre Kame"
-          num6="25" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="O Grande torneio de artes marciais"
+          num1="20" url1={`1DXOes0Uc2sCOTEQtbKl5e0GRmDG1ZysH`} name1={`O Árduo Treinamento Poderá Ser Posto em Prática`}
+          num2="21" url2={`1DXOes0Uc2sCOTEQtbKl5e0GRmDG1ZysH`} name2={`Cuidado, Kuririn`}
+          num3="22" url3={`1DXOes0Uc2sCOTEQtbKl5e0GRmDG1ZysH`} name3={`Yamcha contra Jackie Chun`}
+          num4="23" url4={`1DXOes0Uc2sCOTEQtbKl5e0GRmDG1ZysH`} name4={`O Terrível Giran`}
+          num5="24" url5={`1DXOes0Uc2sCOTEQtbKl5e0GRmDG1ZysH`} name5={`O Ataque Furioso de Kuririn`}
+          num6="25" url6={`1DXOes0Uc2sCOTEQtbKl5e0GRmDG1ZysH`} name6={`O Terrível Ataque Aéreo de Nam`}
         />
 
         <section className="episodeButtons">
-          <SectionLinks num="26" url="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name="A força do Kame-Hame-Ha" />
-          <SectionLinks num="27" url="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name="Goku em sérios apuros" />
-          <SectionLinks num="28" url="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name="O desaparecimento de Goku" />
+          <SectionLinks num="26" url={`1DXOes0Uc2sCOTEQtbKl5e0GRmDG1ZysH`} name={`A força do Kame-Hame-Ha`} />
+          <SectionLinks num="27" url={`1DXOes0Uc2sCOTEQtbKl5e0GRmDG1ZysH`} name={`Goku em sérios apuros`} />
+          <SectionLinks num="28" url={`1DXOes0Uc2sCOTEQtbKl5e0GRmDG1ZysH`} name={`O desaparecimento de Goku`} />
         </section>
       </section>
     </div>
