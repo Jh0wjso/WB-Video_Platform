58a11fe470c8caf79ceeeef94b2ec509db650007
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-01-23 10:17:15 -0300

    Dragon ball season three part one and two

--- Client/src/components/DragonBall/SeasonThree/index.tsx
+++ Client/src/components/DragonBall/SeasonThree/index.tsx
@@ -8,81 +8,81 @@ export default function SeasonThree() {
       <h1 className="text-center text-4xl font-bold">Saga da Red Ribbon</h1>
       <section className="mt-8 flex flex-col items-center">
         <SectionButtons 
-          num1="29" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="O rio que pose se mudar"
-          num2="30" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="O Maraciano e a Patrulha Misteriosa"
-          num3="31" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Goku Pensou ter encontrado a esfera"
-          num4="32" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="A Fortaleza Voadora Desaparece"
-          num5="33" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="A Lenda do Dragão"
-          num6="34" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="A Força Red Ribbon e o seu Cruel Comandante"
+          num1="29" url1={`1N2mAIgAhavM6WqfAHn2qyeQ8u18Jn9cs`} name1={`O rio que pose se mudar`}
+          num2="30" url2={`1N-SZkP73_qbfxxdboaTX50Vt2XbiL-gu`} name2={`O Maraciano e a Patrulha Misteriosa`}
+          num3="31" url3={`1NENlLDAw6BHR57oYSSiHpOgEbmz_lIxx`} name3={`Goku Pensou ter encontrado a esfera`}
+          num4="32" url4={`1NB3hmqUYUyqoUuZHPd041jIso8R1d3tZ`} name4={`A Fortaleza Voadora Desaparece`}
+          num5="33" url5={`1Mi500WISF4IWU_avy-OX_8Yacz23ZYBs`} name5={`A Lenda do Dragão`}
+          num6="34" url6={`1N5PH2mmtGn7BCov2tu6OU5ZA8gEkD1vG`} name6={`A Força Red Ribbon e o seu Cruel Comandante`}
         />
 
         <SectionButtons 
-          num1="35" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Suno, a menina da aldeia do norte"
-          num2="36" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="A batalha com o sargente metálico"
-          num3="37" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Chegou o Ninja Purpura"
-          num4="38" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Cinco Ninjas contra Goku"
-          num5="39" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="O encontro com o Misterioso Android Nº8"
-          num6="40" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="A luta de Goku contra p terrível Buyon"
+          num1="35" url1={`1MXZcur20r204YwlIdNYTGztdEq-zBl34`} name1={`Suno, a menina da aldeia do norte`}
+          num2="36" url2={`1MpMxnoaJnaG5fFKuDkaWNY2HM_dI57Mq`} name2={`A batalha com o sargente metálico`}
+          num3="37" url3={`1MkGdD2gpI6ygp06c1-IciXWxNYu7m6xQ`} name3={`Chegou o Ninja Purpura`}
+          num4="38" url4={`1NLkgprSbAwILUWU6Es4orXAu1nUnKIbg`} name4={`Cinco Ninjas contra Goku`}
+          num5="39" url5={`1NSwOA0dqZYp4VnF99IvHF7Qe4HZs7fMd`} name5={`O encontro com o Misterioso Android Nº8`}
+          num6="40" url6={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name6={`A luta de Goku contra p terrível Buyon`}
         />
 
         <SectionButtons 
-          num1="41" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="O último dia na Torre da Força"
-          num2="42" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="O segredo inesperado do Dr. Frapê"
-          num3="43" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="A casa de Bulma a Oeste de Metrópole"
-          num4="44" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Goku e seus amigos estão em apuros"
-          num5="45" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Cuidado, é uma armadilha"
-          num6="46" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="O completo fracasso de Bulma"
+          num1="41" url1={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name1={`O último dia na Torre da Força`}
+          num2="42" url2={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name2={`O segredo inesperado do Dr. Frapê`}
+          num3="43" url3={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name3={`A casa de Bulma a Oeste de Metrópole`}
+          num4="44" url4={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name4={`Goku e seus amigos estão em apuros`}
+          num5="45" url5={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name5={`Cuidado, é uma armadilha`}
+          num6="46" url6={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name6={`O completo fracasso de Bulma`}
         />
 
         <SectionButtons 
-          num1="47" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Encontrando a casa do Mestre Kame"
-          num2="48" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="O General Blue inicia o ataque"
-          num3="49" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Cuidado com o Inimigo"
-          num4="50" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="O segredo dos piratas"
-          num5="51" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="O guardião das Profundezas"
-          num6="52" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Genial, encontramos o tesouro"
+          num1="47" url1={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name1={`Encontrando a casa do Mestre Kame`}
+          num2="48" url2={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name2={`O General Blue inicia o ataque`}
+          num3="49" url3={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name3={`Cuidado com o Inimigo`}
+          num4="50" url4={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name4={`O segredo dos piratas`}
+          num5="51" url5={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name5={`O guardião das Profundezas`}
+          num6="52" url6={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name6={`Genial, encontramos o tesouro`}
         />
 
         <SectionButtons 
-          num1="53" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="A caverna sinistra"
-          num2="54" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="A escapada"
-          num3="55" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="O caminho para a Vila Pinguim"
-          num4="56" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Arale viaja na Nuvem Voadora"
-          num5="57" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Arale contra o General Blue"
-          num6="58" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="A Terra Sagrada"
+          num1="53" url1={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name1={`A caverna sinistra`}
+          num2="54" url2={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name2={`A escapada`}
+          num3="55" url3={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name3={`O caminho para a Vila Pinguim`}
+          num4="56" url4={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name4={`Arale viaja na Nuvem Voadora`}
+          num5="57" url5={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name5={`Arale contra o General Blue`}
+          num6="58" url6={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name6={`A Terra Sagrada`}
         />
 
         <SectionButtons 
-          num1="59" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Tao Pai Pai, o Exterminador mais famoso do Mundo"
-          num2="60" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="A força Kame-Hame-Ha contra o Tap Pai Pai"
-          num3="61" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="A Torre do Mestre Karin"
-          num4="62" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Os benefícios da água sagrada"
-          num5="63" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Goku contra-ataca"
-          num6="64" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="O último Tao Pai Pai"
+          num1="59" url1={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name1={`Tao Pai Pai, o Exterminador mais famoso do Mundo`}
+          num2="60" url2={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name2={`A força Kame-Hame-Ha contra o Tap Pai Pai`}
+          num3="61" url3={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name3={`A Torre do Mestre Karin`}
+          num4="62" url4={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name4={`Os benefícios da água sagrada`}
+          num5="63" url5={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name5={`Goku contra-ataca`}
+          num6="64" url6={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name6={`O último Tao Pai Pai`}
         />
 
         <SectionButtons 
-          num1="65" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Vamos Goku! Começa aluta"
-          num2="66" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="O último empenho da força Red Ribbon"
-          num3="67" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="O comandante da força Red Ribbon morre"
-          num4="68" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="A ultima esfera do dragão"
-          num5="69" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Quem é a Vovó Uranai"
-          num6="70" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="O ataque dos cinco guerreiros"
+          num1="65" url1={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name1={`Vamos Goku! Começa aluta`}
+          num2="66" url2={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name2={`O último empenho da força Red Ribbon`}
+          num3="67" url3={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name3={`O comandante da força Red Ribbon morre`}
+          num4="68" url4={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name4={`A ultima esfera do dragão`}
+          num5="69" url5={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name5={`Quem é a Vovó Uranai`}
+          num6="70" url6={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name6={`O ataque dos cinco guerreiros`}
         />
 
 
         <SectionButtons 
-          num1="71" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Uma batalha sangrenta e mortal"
-          num2="82" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="A luta de Goku na Piscina do Diabo"
-          num3="73" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="O que faz o esplendor mortal do Diabo"
-          num4="74" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="O quinto guerreiro misterioso"
-          num5="75" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Como um adversário se tornou um velho amigo"
-          num6="76" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="A verdadeira identidade do Homem Mascarado"
+          num1="71" url1={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name1={`Uma batalha sangrenta e mortal`}
+          num2="82" url2={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name2={`A luta de Goku na Piscina do Diabo`}
+          num3="73" url3={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name3={`O que faz o esplendor mortal do Diabo`}
+          num4="74" url4={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name4={`O quinto guerreiro misterioso`}
+          num5="75" url5={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name5={`Como um adversário se tornou um velho amigo`}
+          num6="76" url6={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name6={`A verdadeira identidade do Homem Mascarado`}
         />
 
         <section className="episodeButtons">
-          <SectionLinks num="77" url="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name="O grande plano de Pilaf" />
-          <SectionLinks num="78" url="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name="O reaparecimento do Deus Dragão" />
+          <SectionLinks num="77" url={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name={`O grande plano de Pilaf`} />
+          <SectionLinks num="78" url={`1NN99MxVHQlPuHL7t8xOBV1x2aYjdGIOu`} name={`O reaparecimento do Deus Dragão`} />
         </section>
       </section>
     </div>
