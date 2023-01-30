3eab7d5db3cf88563d7aa8ae41f1377817b76b4a
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-01-30 15:42:57 -0300

    Sidebar first version

--- Client/src/components/SideBar/index.tsx
+++ Client/src/components/SideBar/index.tsx
@@ -1,15 +1,52 @@
 import "./styles.css";
 
-export default function Sidebar(){
-    return(
-        <aside className="sidebar w-[15%] h-[75vh] bg-zinc-900 p-6 border-l border-gray-600 my-[15vh]">
-            <span className="font-bold text-center text-2xl pb-6 mb-6 border-b border-gray-500 block">
-                Novidades
-            </span>
+import Img from "../../images/dbReduced.jpg";
+import { Link } from "react-router-dom";
 
-            <div className="flex flex-col gap-8">
-                <p>Exemplo</p>
-            </div>
-        </aside>
-    );
+export default function Sidebar() {
+  return (
+    <aside className="sidebar w-[15%] h-[75vh] bg-zinc-900 p-6 border-l border-gray-600 my-[15vh]">
+      <span className="font-bold text-center text-2xl pb-6 mb-6 border-b border-gray-500 block">
+        Novidades Db Heroes
+      </span>
+
+      <div className="flex flex-col gap-8 items-center">
+        <Link
+          to="video/O%20Terrível%20Rei%20das%20Trevas%20Ataca!%20A%20terrível%20Batalha%20Começa!/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+          className="cardNew"
+        >
+          <img src={Img} width={50} className="imgCardNew" />
+          <p className="font-bold text-white">Episódio 46</p>
+        </Link>
+        <Link
+          to="video/O%20Terrível%20Rei%20das%20Trevas%20Ataca!%20A%20terrível%20Batalha%20Começa!/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+          className="cardNew"
+        >
+          <img src={Img} width={50} className="imgCardNew" />
+          <p className="font-bold text-white">Episódio 45</p>
+        </Link>
+        <Link
+          to="video/O%20Terrível%20Rei%20das%20Trevas%20Ataca!%20A%20terrível%20Batalha%20Começa!/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+          className="cardNew"
+        >
+          <img src={Img} width={50} className="imgCardNew" />
+          <p className="font-bold text-white">Episódio 44</p>
+        </Link>
+        <Link
+          to="video/O%20Terrível%20Rei%20das%20Trevas%20Ataca!%20A%20terrível%20Batalha%20Começa!/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+          className="cardNew"
+        >
+          <img src={Img} width={50} className="imgCardNew" />
+          <p className="font-bold text-white">Episódio 43</p>
+        </Link>
+        <Link
+          to="video/O%20Terrível%20Rei%20das%20Trevas%20Ataca!%20A%20terrível%20Batalha%20Começa!/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+          className="cardNew"
+        >
+          <img src={Img} width={50} className="imgCardNew" />
+          <p className="font-bold text-white">Episódio 42</p>
+        </Link>
+      </div>
+    </aside>
+  );
 }
