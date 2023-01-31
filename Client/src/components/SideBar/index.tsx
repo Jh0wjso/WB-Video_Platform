55a9790943b407796889177aa1dc5efa0400f249
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-01-31 15:28:29 -0300

    Update on sidebar

--- Client/src/components/SideBar/index.tsx
+++ Client/src/components/SideBar/index.tsx
@@ -5,46 +5,47 @@ import { Link } from "react-router-dom";
 
 export default function Sidebar() {
   return (
-    <aside className="sidebar w-[15%] h-[75vh] bg-zinc-900 p-6 border-l border-gray-600 my-[15vh]">
-      <span className="font-bold text-center text-2xl pb-6 mb-6 border-b border-gray-500 block">
-        Novidades Db Heroes
+    <aside className="sidebar">
+      <span className="nameNews">
+        Novidades
       </span>
 
       <div className="flex flex-col gap-8 items-center">
         <Link
-          to="video/O%20Terrível%20Rei%20das%20Trevas%20Ataca!%20A%20terrível%20Batalha%20Começa!/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+          to={`video/O Terrível Rei das Trevas Ataca! A terrível Batalha Começa!/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm`}
           className="cardNew"
         >
-          <img src={Img} width={50} className="imgCardNew" />
-          <p className="font-bold text-white">Episódio 46</p>
+          <p className="newsTittle">Dragon Ball Heroes <br/>Episódio 46</p>
         </Link>
         <Link
-          to="video/O%20Terrível%20Rei%20das%20Trevas%20Ataca!%20A%20terrível%20Batalha%20Começa!/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+          to={`video/A Batalha Decisiva no Reino dos Deuses! O Poder do Tempo se Aproxima!/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm`}
           className="cardNew"
         >
-          <img src={Img} width={50} className="imgCardNew" />
-          <p className="font-bold text-white">Episódio 45</p>
+          <p className="newsTittle">Dragon Ball Heroes <br/>Episódio 45</p>
         </Link>
         <Link
-          to="video/O%20Terrível%20Rei%20das%20Trevas%20Ataca!%20A%20terrível%20Batalha%20Começa!/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+          to={`video/Goku vs O Guerreiro de Preto! Lutando pelos seus Próprios Destinos!/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm`}
           className="cardNew"
         >
-          <img src={Img} width={50} className="imgCardNew" />
-          <p className="font-bold text-white">Episódio 44</p>
+          <p className="newsTittle">Dragon Ball Heroes <br/>Episódio 44</p>
         </Link>
         <Link
-          to="video/O%20Terrível%20Rei%20das%20Trevas%20Ataca!%20A%20terrível%20Batalha%20Começa!/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+          to={`video/Uma Batalha Feroz que Transcende o Tempo! A Ameaça dos Guerreiros de Preto!/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm`}
           className="cardNew"
         >
-          <img src={Img} width={50} className="imgCardNew" />
-          <p className="font-bold text-white">Episódio 43</p>
+          <p className="newsTittle">Dragon Ball Heroes <br/>Episódio 43</p>
         </Link>
         <Link
-          to="video/O%20Terrível%20Rei%20das%20Trevas%20Ataca!%20A%20terrível%20Batalha%20Começa!/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm"
+          to={`video/Goku vs O Guerreiro de Preto! Lutando pelos seus Próprios Destinos!/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm`}
           className="cardNew"
         >
-          <img src={Img} width={50} className="imgCardNew" />
-          <p className="font-bold text-white">Episódio 42</p>
+          <p className="newsTittle">Dragon Ball Heroes <br/>Episódio 42</p>
+        </Link>
+        <Link
+          to={`video/A Batalha Decisiva no Reino dos Deuses! O Poder do Tempo se Aproxima!/1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm`}
+          className="cardNew"
+        >
+          <p className="newsTittle">Dragon Ball Heroes <br/>Episódio 41</p>
         </Link>
       </div>
     </aside>
