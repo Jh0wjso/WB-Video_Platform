5a122c3ad680371b55e75de83f01e5f48734642d
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-02-02 09:44:37 -0300

    Dragon ball heroes season one

--- Client/src/components/DragonBallHeroes/SeasonOne/index.tsx
+++ Client/src/components/DragonBallHeroes/SeasonOne/index.tsx
@@ -7,12 +7,12 @@ export default function SeasonOneHeroes() {
       <h1 className="text-center text-4xl font-bold">Saga Prisão Planetária</h1>
       <section className="mt-8 flex flex-col items-center">
         <SectionButtons 
-          num1="1" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1={`Goku Vs. Goku! Uma Batalha Transcendente Começa No Planeta Da Prisão`}
-          num2="2" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2={`Goku Fica Furioso! A Fúria do Mal Saião!`}
-          num3="3" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3={`O Esplendor Mais Poderoso! Vegetto Blue Explode Kaio-Ken!`}
-          num4="4" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4={`Raiva! O Super Fu Aparece!`}
-          num5="5" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5={`O Guerreiro Mais Poderoso! Vegetto Super Saiyajin 4!!`}
-          num6="6" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6={`Eu Vou Resolver Isso! Ativar! Instinto Superior!`}
+          num1="1" url1="1fz0GqbpP4fInBjHV_Yy0Z6fadMoRdPqv" name1={`Goku Vs. Goku! Uma Batalha Transcendente Começa No Planeta Da Prisão`}
+          num2="2" url2="1fg4xCXI8PHx9R3xvR-GRVZdSowYE7oy8" name2={`Goku Fica Furioso! A Fúria do Mal Saião!`}
+          num3="3" url3="1fvAIZqfwbGJQ57qyW4Cpqre6TPgPw6iP" name3={`O Esplendor Mais Poderoso! Vegetto Blue Explode Kaio-Ken!`}
+          num4="4" url4="1flXZsiMZzfgrwlqRmo0S8HAsrCozUwHJ" name4={`Raiva! O Super Fu Aparece!`}
+          num5="5" url5="1fqIp8Z9kq1J_QQURhk2HZDuEPRD2LK3z" name5={`O Guerreiro Mais Poderoso! Vegetto Super Saiyajin 4!!`}
+          num6="6" url6="1fsTfVgVlflgjtr6GcHsaEnTS9M2AmX99" name6={`Eu Vou Resolver Isso! Ativar! Instinto Superior!`}
         />
       </section>
     </div>
