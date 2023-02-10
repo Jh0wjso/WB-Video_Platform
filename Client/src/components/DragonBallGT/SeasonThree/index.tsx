12282b4aaac39998a76ca3ee25641c551fc15b3d
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-02-10 09:18:06 -0300

    Dragon Ball Gt Season Three Finished

--- Client/src/components/DragonBallGT/SeasonThree/index.tsx
+++ Client/src/components/DragonBallGT/SeasonThree/index.tsx
@@ -7,12 +7,12 @@ export default function SeasonThreeGT() {
       <h1 className="text-center text-4xl font-bold">Saga do Super 17</h1>
       <section className="flex flex-col items-center">
         <SectionButtons 
-          num1="42" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Morra Goku! Os Inimigos Mais Fortes Escapam do Inferno"
-          num2="43" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="Volta de Cell e Freeza"
-          num3="44" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="O ultimo Android! O Super 17"
-          num4="45" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="O Plano Para Escapar do Inferno"
-          num5="46" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="O Super Saiyajin 4 Enfrenta o N°17"
-          num6="47" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Goku e o N°18 Lutam Juntos"
+          num1="42" url1="15j36jz7d9eW71-68EJo1ZGQwHJpDKUCS" name1="Morra Goku! Os Inimigos Mais Fortes Escapam do Inferno"
+          num2="43" url2="15gYwj1MFvO2oOhh1aR9h1RrxnSN8NzEr" name2="Volta de Cell e Freeza"
+          num3="44" url3="15h-97y_y2u3SfBZoizD1DkGw79fre7kM" name3="O ultimo Android! O Super 17"
+          num4="45" url4="15z9JkK41yGaww_TwFIkFxS8hL6lkERGY" name4="O Plano Para Escapar do Inferno"
+          num5="46" url5="15zoQpPpnXRoHJhqVoGiRM6bfeNc3yrxf" name5="O Super Saiyajin 4 Enfrenta o N°17"
+          num6="47" url6="15leAVFFfPMX_qW6nIzScrWNKjn8V7rvR" name6="Goku e o N°18 Lutam Juntos"
         />
       </section>
     </div>
