c332fd78dcecaf723118d42384a76480e7b9e24d
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-02-13 09:26:30 -0300

    Update on caroussels

--- Client/src/components/DragonBallCarousel/index.tsx
+++ Client/src/components/DragonBallCarousel/index.tsx
@@ -8,16 +8,16 @@ import DbS2 from "../../images/supertwo.jpg";
 export default function DragonBallCarousel() {
   return (
     <div className="container flex flex-col items-center mt-9 w-screen">
-      <div className="container md:bg-gray-700 flex p-2 flex-row justify-between items-center w-screen border-gray-700 md:rounded-t-lg">
+      <div className="container bg-gray-700 flex p-2 flex-row justify-between items-center w-screen border-gray-700 rounded-t-lg">
         <h1 className="text-2xl text-white md:text-4xl ml-4">Super</h1>
         <Link
           className="mr-4 md:mr-10 text-white chrome bg-orange-400 rounded md:py-1 px-1 md:px-2 transition-colors hover:bg-orange-600 font-bold"
-          to="/dbsuper"
+          to="/dbheroes"
         >
           Ver Mais
         </Link>
       </div>
-      <div className="carouselDb md:p-5 text-center font-bold mb-5 container md:text-gray-700 md:border-2 w-full md:rounded-b-lg md:bg-gray-300 md:border-gray-700">
+      <div className="carouselDb p-5 text-center font-bold mb-5 container text-gray-700 border-2 w-full rounded-b-lg bg-gray-300 md:border-gray-700">
         <Carousel cols={5} rows={1} gap={10} loop>
           <Carousel.Item>
             <Link
--- Client/src/components/DragonBallHeroes/HeroesCarousel/index.tsx
+++ Client/src/components/DragonBallHeroes/HeroesCarousel/index.tsx
@@ -8,7 +8,7 @@ import "./styles.css";
 export default function HeroesCarousel() {
   return (
     <div className="container flex flex-col items-center mt-9 w-screen">
-      <div className="container md:bg-gray-700 flex p-2 flex-row justify-between items-center w-screen border-gray-700 md:rounded-t-lg">
+      <div className="container bg-gray-700 flex p-2 flex-row justify-between items-center w-screen border-gray-700 rounded-t-lg">
         <h1 className="text-2xl text-white md:text-4xl ml-4">Heroes</h1>
         <Link
           className="mr-4 md:mr-10 text-white chrome bg-orange-400 rounded md:py-1 px-1 md:px-2 transition-colors hover:bg-orange-600 font-bold"
@@ -17,7 +17,7 @@ export default function HeroesCarousel() {
           Ver Mais
         </Link>
       </div>
-      <div className="carouselDb md:p-5 text-center font-bold mb-5 container md:text-gray-700 md:border-2 w-full md:rounded-b-lg md:bg-gray-300 md:border-gray-700">
+      <div className="carouselDb p-5 text-center font-bold mb-5 container text-gray-700 border-2 w-full rounded-b-lg bg-gray-300 md:border-gray-700">
         <Carousel cols={5} rows={1} gap={10} loop>
           <Carousel.Item>
             <Link
