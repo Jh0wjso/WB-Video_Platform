8b64a8b13cc5df29f716ac7967678cfb3b008a88
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-02-15 10:36:18 -0300

    Dragon ball super season four

--- Client/src/components/DragonBallSuper/SeasonFour/index.tsx
+++ Client/src/components/DragonBallSuper/SeasonFour/index.tsx
@@ -7,44 +7,44 @@ export default function SeasonFourSuper() {
       <h1 className="text-center text-4xl font-bold">Trunks do Futuro</h1>
       <section className="mt-8 flex flex-col items-center">
         <SectionButtons 
-          num1="47" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1={``}
-          num2="48" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2={`Uma Nova Esperança! Desperte no Presente, Trunks!`}
-          num3="49" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3={`Uma Mensagem do Futuro. O Ataque de Goku Black!`}
-          num4="50" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4={`Goku vs. Black! O Caminho Para o Futuro Foi Selado!`}
-          num5="51" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5={`Os Sentimentos Que Atravessam o Tempo! Trunks e Mai!`}
-          num6="52" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6={`Mestre e Discípulo! Son Gohan e o Trunks do Futuro Se Reencontram!`}
+          num1="47" url1="1G9CMBcDc-A4zkaHpAcBn4mGUJVyiigL1" name1={`Um SOS do Futuro! Um Novo Inimigo Sombrio Aparece!!`}
+          num2="48" url2="1G3XuEVMGI4XmNOnKU6wH4w2sUfi7AMx2" name2={`Uma Nova Esperança! Desperte no Presente, Trunks!`}
+          num3="49" url3="1G29S9tmb_OS9vGaW-gNCA10iBIsHii99" name3={`Uma Mensagem do Futuro. O Ataque de Goku Black!`}
+          num4="50" url4="1FwcO13QWEK94bpYWk9h8Nq7FOzkl-upW" name4={`Goku vs. Black! O Caminho Para o Futuro Foi Selado!`}
+          num5="51" url5="1GBTNc1TtkKkwfIlUDpPvacioN49BZwPW" name5={`Os Sentimentos Que Atravessam o Tempo! Trunks e Mai!`}
+          num6="52" url6="1GASnSg9nqykPIMA-myZVxrAyEnus2Lvy" name6={`Mestre e Discípulo! Son Gohan e o Trunks do Futuro Se Reencontram!`}
         />
         <SectionButtons 
-          num1="53" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1={`A Identidade de Black é Revelada! Vamos ao Mundo dos Senhores Kaioh!`}
-          num2="54" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2={`Aquele Que Herdou o Sangue Saiyajin! A Decisão de Trunks!`}
-          num3="55" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3={`Eu Quero Ver o Son Goku, Né? O Grande Zen-ô Está Chamando!`}
-          num4="56" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4={`A Revanche Contra o Goku Black! Surge o Super Saiyajin Rosé!`}
-          num5="57" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5={`A Divindade de Corpo Imortal! Zamasu Chega à Terra!`}
-          num6="58" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6={`Zamasu e Black! O Mistério Aumenta!`}
+          num1="53" url1="1GQtj_EIJkOYeKidMEqPqIACRcYvfzvem" name1={`A Identidade de Black é Revelada! Vamos ao Mundo dos Senhores Kaioh!`}
+          num2="54" url2="1GPzatDMunAlMS-fphmNYQl3z02gkYcQt" name2={`Aquele Que Herdou o Sangue Saiyajin! A Decisão de Trunks!`}
+          num3="55" url3="1GPV0Ki1HkCQU9Sy7tYI0Tj1Iyoe4XCIh" name3={`Eu Quero Ver o Son Goku, Né? O Grande Zen-ô Está Chamando!`}
+          num4="56" url4="1GNadJ0dNarulyKKMoOvKKGJYfIA8f8Qq" name4={`A Revanche Contra o Goku Black! Surge o Super Saiyajin Rosé!`}
+          num5="57" url5="1GNZU-pepoRyip4iVXMXcs0aWxqm9LDQM" name5={`A Divindade de Corpo Imortal! Zamasu Chega à Terra!`}
+          num6="58" url6="1GCWrklXjucgoKo9E2dbjNMr8WR-T2-RB" name6={`Zamasu e Black! O Mistério Aumenta!`}
         />
         <SectionButtons 
-          num1="59" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1={`Proteja o Supremo Senhor Kaioh Gowasu! Acabe Com Zamasu!`}
-          num2="60" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2={`De Volta Para o Futuro! A Identidade de Goku Black é Revelada!!`}
-          num3="61" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3={`A Ambição de Zamasu! O Terrível "Plano Zero Humanos" é Revelado!!`}
-          num4="62" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4={`Eu Vou Proteger o Mundo! A Fúria de Trunks Faz Explodir um Superpoder!`}
-          num5="63" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5={`Não Subestime as Células dos Super Saiyajins! Começa a Heroica Batalha de Vegeta!`}
-          num6="64" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6={`Adorem-no! Venerem-no! A Poderosa Fusão de Zamasu!!`}
+          num1="59" url1="1GTDNDKfHcEMhjxWRgxwHpJ5WnjjlBxPs" name1={`Proteja o Supremo Senhor Kaioh Gowasu! Acabe Com Zamasu!`}
+          num2="60" url2="1GT15bQUIZp-f97KamsXJzTa7zJrvXXu_" name2={`De Volta Para o Futuro! A Identidade de Goku Black é Revelada!!`}
+          num3="61" url3="1GR9NJ2pHXi7cSPQBJMrWRdji0huklLAC" name3={`A Ambição de Zamasu! O Terrível "Plano Zero Humanos" é Revelado!!`}
+          num4="62" url4="1GaLoeQhwIkm3c4MKegTD6aYqdXxuVU_R" name4={`Eu Vou Proteger o Mundo! A Fúria de Trunks Faz Explodir um Superpoder!`}
+          num5="63" url5="1GZQC0oT1Cy9KkHp5Pl2uCXRGbqtJbMtO" name5={`Não Subestime as Células dos Super Saiyajins! Começa a Heroica Batalha de Vegeta!`}
+          num6="64" url6="1GVyICAmOZzs6mLhvPPxbcge-v4sCDrMk" name6={`Adorem-no! Venerem-no! A Poderosa Fusão de Zamasu!!`}
         />
         <SectionButtons 
-          num1="65" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1={`É o Juízo Final? O Poder Máximo do Deus Supremo!`}
-          num2="66" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2={`A Batalha Decisiva! O Poder Milagroso dos Guerreiros Que Nunca Desistem!`}
-          num3="67" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3={`Uma Nova Esperança Em Nossos Corações! Adeus, Trunks!`}
-          num4="68" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4={`Apareça, Shen Long! Qual Desejo Será Atendido?!`}
-          num5="69" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5={`Goku vs. Arale! Uma Batalha Idiota Vai Destruir a Terra?!`}
-          num6="70" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6={`O Desafio de Champa! A Luta Agora é no Campo de Beisebol!`}
+          num1="65" url1="1H4cUjl-pgEoEPlJno-6erObhQcPgEXCF" name1={`É o Juízo Final? O Poder Máximo do Deus Supremo!`}
+          num2="66" url2="1GwkshxvHPUN2CU5jLTPX4Al6wZZteVbM" name2={`A Batalha Decisiva! O Poder Milagroso dos Guerreiros Que Nunca Desistem!`}
+          num3="67" url3="1GuL5VqctN4tbBCtGckRENy8mVQvnUqeW" name3={`Uma Nova Esperança Em Nossos Corações! Adeus, Trunks!`}
+          num4="68" url4="1Gt-h1uiEf-qz7UKSGkRpZOPP7j4zn-lN" name4={`Apareça, Shen Long! Qual Desejo Será Atendido?!`}
+          num5="69" url5="1Gm10Jy6cItxmsbGOmkcNUNNIcopa0A0Q" name5={`Goku vs. Arale! Uma Batalha Idiota Vai Destruir a Terra?!`}
+          num6="70" url6="1Gc8WhMe7UuACV767vNRwQdK1kKM0AMSl" name6={`O Desafio de Champa! A Luta Agora é no Campo de Beisebol!`}
         />
         <SectionButtons 
-          num1="71" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1={`A Morte de Goku! A Missão Assassina!`}
-          num2="72" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2={`O Contra-Ataque?! O Golpe Mortal Invisível!`}
-          num3="73" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3={`O Dilema de Gohan! O Filme do Grande Saiyaman?!`}
-          num4="74" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4={`Pelo Bem dos Que Eu Amo! O Invencível Grande Saiyaman!`}
-          num5="75" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5={`Goku e Kuririn! De Volta ao Treinamento!v`}
-          num6="76" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6={`Derrote os Inimigos! A Volta do Espírito de Luta de Kuririn!`}
+          num1="71" url1="1HmN5keyQUidnRtFmou9sTwJzw4KA7rSp" name1={`A Morte de Goku! A Missão Assassina!`}
+          num2="72" url2="1HlzGTy3FPG7Iyvge-5zDLL4cvsUk1QYe" name2={`O Contra-Ataque?! O Golpe Mortal Invisível!`}
+          num3="73" url3="1HhbFXdQ9TsbY-8lgMF95fOVqfa7RiNE2" name3={`O Dilema de Gohan! O Filme do Grande Saiyaman?!`}
+          num4="74" url4="1HcKd95fcEW3yjlVh_44SI4omaABJh0T0" name4={`Pelo Bem dos Que Eu Amo! O Invencível Grande Saiyaman!`}
+          num5="75" url5="1HTUQKCkTne_iEiUGBJLVRKsVgl6W2Oq_" name5={`Goku e Kuririn! De Volta ao Treinamento!v`}
+          num6="76" url6="1HNJGsxu6AlB7WmuQN0Ol1jDznbws5Tg3" name6={`Derrote os Inimigos! A Volta do Espírito de Luta de Kuririn!`}
         />
       </section>
     </div>
