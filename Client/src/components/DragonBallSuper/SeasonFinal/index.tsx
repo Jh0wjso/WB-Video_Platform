f175f11b29a14607413746d23142ab465bb185ce
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-02-16 11:04:38 -0300

    New links for dragon ball super final season

--- Client/src/components/DragonBallSuper/SeasonFinal/index.tsx
+++ Client/src/components/DragonBallSuper/SeasonFinal/index.tsx
@@ -8,79 +8,79 @@ export default function SeasonFinalSuper() {
       <h1 className="text-center text-4xl font-bold">Sobrevivência do Universo</h1>
       <section className="mt-8 flex flex-col items-center">
         <SectionButtons 
-          num1="77" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1={`Vamos Lá, Senhor Zen-ô! O Torneio de Artes Marciais Entre Todos os Universos!`}
-          num2="78" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2={`Todos os Deuses do Universo em Choque?! Quem Perder Será Apagado no Torneio do Poder`}
-          num3="79" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3={`Basil, o Chutador do Universo Nove vs. Majin Boo, do Universo Sete!`}
-          num4="80" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4={`Desperte o Seu Espírito de Luta! A Batalha de Son Gohan!`}
-          num5="81" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5={`Bergamo, o Esmagador Contra Goku! Quem Possui o Poder Ilimitado?!`}
-          num6="82" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6={`Nunca o Perdoarei, Son Goku! A Intervenção de Toppo, o Guerreiro da Justiça!`}
+          num1="77" url1="1IlDK6F7Jspk08KWjc7dT-ra8qxj48w0l" name1={`Vamos Lá, Senhor Zen-ô! O Torneio de Artes Marciais Entre Todos os Universos!`}
+          num2="78" url2="1IxRTG_2iBI3C8q0ib0gh8gdU5bzrP7F_" name2={`Todos os Deuses do Universo em Choque?! Quem Perder Será Apagado no Torneio do Poder`}
+          num3="79" url3="1Izhg_sN7x0zR6-ybQSzr1wD3Xe24lonr" name3={`Basil, o Chutador do Universo Nove vs. Majin Boo, do Universo Sete!`}
+          num4="80" url4="1J-mct1isPJDoIzJy1gY6FIJR6lBLBlIA" name4={`Desperte o Seu Espírito de Luta! A Batalha de Son Gohan!`}
+          num5="81" url5="1J36pMcwxYDjxzIn4nNEaoPt0BQTQHvPP" name5={`Bergamo, o Esmagador Contra Goku! Quem Possui o Poder Ilimitado?!`}
+          num6="82" url6="1J4FZlw0xQ92-j_ZJ0DUH8DNEY9maLJmp" name6={`Nunca o Perdoarei, Son Goku! A Intervenção de Toppo, o Guerreiro da Justiça!`}
         />
         <SectionButtons 
-          num1="83" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1={`Qual Será o Time do Universo Sete! Quem São os Dez Mais Fortes?`}
-          num2="84" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2={`Son Goku Recruta Kuririn e a Androide No. 18!`}
-          num3="85" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3={`Os Universos Começam a Agir! A Motivação de Cada Um!`}
-          num4="86" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4={`Trocando Golpes Pela Primeira Vez! Androide No. 17 vs. Son Goku!`}
-          num5="87" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5={`Atrás dos Caçadores! Goku e Número Dezessete Se Unem!`}
-          num6="88" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6={`Gohan e Piccolo! Treino Além dos Limites!`}
+          num1="83" url1="1J6tmAcGMuWezlXPENUXOvt_zg-XV2Xjs" name1={`Qual Será o Time do Universo Sete! Quem São os Dez Mais Fortes?`}
+          num2="84" url2="1JAbQaIV6nl5iDIjPE8RbqzmaHCXZagm2" name2={`Son Goku Recruta Kuririn e a Androide No. 18!`}
+          num3="85" url3="1JDrQy_fwVQ589QhunEqRRLxkA5Xe7YK2" name3={`Os Universos Começam a Agir! A Motivação de Cada Um!`}
+          num4="86" url4="1JKXnicM7TL_pbjlrBjqBQ_NuJznfaVJN" name4={`Trocando Golpes Pela Primeira Vez! Androide No. 17 vs. Son Goku!`}
+          num5="87" url5="1JQJMKrdHM2VZZYw2Cg7JylTA285zYYgl" name5={`Atrás dos Caçadores! Goku e Número Dezessete Se Unem!`}
+          num6="88" url6="1JSUZT87mKLGx9qwUxLETt1hxCUH4iaIu" name6={`Gohan e Piccolo! Treino Além dos Limites!`}
         />
         <SectionButtons 
-          num1="89" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1={`Uma Beldade Misteriosa! O Mistério do Dojo do Estilo Tenshin?!`}
-          num2="90" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2={`A Barreira a Ser Superada! Goku vs. Gohan!`}
-          num3="91" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3={`Qual Universo Irá Sobreviver?! Os Guerreiros Mais Fortes Se Reúnem!`}
-          num4="92" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4={`O Plano de Emergência! O Time de Dez Membros Está Incompleto!`}
-          num5="93" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5={`O Décimo Guerreiro! Goku Vai Ao Encontro de Freeza!`}
-          num6="94" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6={`O Retorno do Imperador do Mal! Uma Recepção Para os Assassinos Misteriosos?!`}
+          num1="89" url1="1JjRQjLOwBm4cvB6-E8VxuoScvageu4G2" name1={`Uma Beldade Misteriosa! O Mistério do Dojo do Estilo Tenshin?!`}
+          num2="90" url2="1JmdEAc4PSAgzo0691W1UhvfL9bFOrNx_" name2={`A Barreira a Ser Superada! Goku vs. Gohan!`}
+          num3="91" url3="1JXHhGD1Z3Id0N938jxRbbxQNCrcsR437" name3={`Qual Universo Irá Sobreviver?! Os Guerreiros Mais Fortes Se Reúnem!`}
+          num4="92" url4="1JY-N905HwYt6_wgazx3ISkgTa0hanUMs" name4={`O Plano de Emergência! O Time de Dez Membros Está Incompleto!`}
+          num5="93" url5="1JeYcOXoWpgTSHE4kmRMywtemOddEPqab" name5={`O Décimo Guerreiro! Goku Vai Ao Encontro de Freeza!`}
+          num6="94" url6="1JiYQuYWAY4EFyl97LpppfFXDwy2f53sV" name6={`O Retorno do Imperador do Mal! Uma Recepção Para os Assassinos Misteriosos?!`}
         />
         <SectionButtons 
-          num1="95" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1={`O Mais Maligno de Todos! A Explosão Furiosa de Freeza!`}
-          num2="96" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2={`Chegou a Hora! Vamos ao Mundo do Vácuo Para Decidir o Destino do Universo!`}
-          num3="97" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3={`Sobrevivam! Enfim Começa o Torneio do Poder!`}
-          num4="98" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4={`Incerteza Total! O Desespero de um Universo!!`}
-          num5="99" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5={`Mostre Pra Eles! O Verdadeiro Poder de Kuririn!`}
-          num6="100" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6={`Fora de Controle! Um Guerreiro Selvagem Desperta!`}
+          num1="95" url1="1Jr6QPujncfuETBLdOJ_8XUv6G-or5LC9" name1={`O Mais Maligno de Todos! A Explosão Furiosa de Freeza!`}
+          num2="96" url2="1K-rSESWwTTIdsIO8Ta0cu5SHdKY0i38a" name2={`Chegou a Hora! Vamos ao Mundo do Vácuo Para Decidir o Destino do Universo!`}
+          num3="97" url3="1K0ti4OOxRWHRyUjnSlW17FScMuPFVpS8" name3={`Sobrevivam! Enfim Começa o Torneio do Poder!`}
+          num4="98" url4="1K6f4FByKQcUlYBzkZcfd0_v-sbpQk5CA" name4={`Incerteza Total! O Desespero de um Universo!!`}
+          num5="99" url5="1K6onk6RrqZWZpPpotRYjVZGEN1MU_rpV" name5={`Mostre Pra Eles! O Verdadeiro Poder de Kuririn!`}
+          num6="100" url6="1K8oNjmTduQQXIT5_aoLVGiOLz-37US95" name6={`Fora de Controle! Um Guerreiro Selvagem Desperta!`}
         />
         <SectionButtons 
-          num1="101" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1={`Os Guerreiros da Justiça! Eis a Tropa do Orgulho!`}
-          num2="102" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2={`O Poder do Amor Explode?! As Guerreiras Mágicas do Universo Dois!`}
-          num3="103" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3={`Gohan, Não Tenha Piedade! A Batalha Contra o Universo Dez!`}
-          num4="104" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4={`Batalha Em Alta Velocidade! Goku e Hit Unem Forças!`}
-          num5="105" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5={`Batalha Desesperada! Mestre Kame Sacrifica Sua Vida!`}
-          num6="106" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6={`Encontre Ele! Luta Mortal Contra Um Adversário Invisível!`}
+          num1="101" url1="" name1={`Os Guerreiros da Justiça! Eis a Tropa do Orgulho!`}
+          num2="102" url2="" name2={`O Poder do Amor Explode?! As Guerreiras Mágicas do Universo Dois!`}
+          num3="103" url3="" name3={`Gohan, Não Tenha Piedade! A Batalha Contra o Universo Dez!`}
+          num4="104" url4="" name4={`Batalha Em Alta Velocidade! Goku e Hit Unem Forças!`}
+          num5="105" url5="" name5={`Batalha Desesperada! Mestre Kame Sacrifica Sua Vida!`}
+          num6="106" url6="" name6={`Encontre Ele! Luta Mortal Contra Um Adversário Invisível!`}
         />
         <SectionButtons 
-          num1="107" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1={`A Vingança de F! Uma Armadilha Engenhosa?!`}
-          num2="108" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2={`Freeza e Frost: Os Vilões Se Uniram?!`}
-          num3="109" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3={`O Inimigo Mais Forte! Goku, Libere a Genkidama!`}
-          num4="110" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4={`O Despertar de Son Goku! Uma Nova Transformação!`}
-          num5="111" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5={`A Batalha Surreal! Hit vs. Jiren!`}
-          num6="112" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6={`O Juramento de um Saiyajin! A Determinação de Vegeta!`}
+          num1="107" url1="" name1={`A Vingança de F! Uma Armadilha Engenhosa?!`}
+          num2="108" url2="" name2={`Freeza e Frost: Os Vilões Se Uniram?!`}
+          num3="109" url3="" name3={`O Inimigo Mais Forte! Goku, Libere a Genkidama!`}
+          num4="110" url4="" name4={`O Despertar de Son Goku! Uma Nova Transformação!`}
+          num5="111" url5="" name5={`A Batalha Surreal! Hit vs. Jiren!`}
+          num6="112" url6="" name6={`O Juramento de um Saiyajin! A Determinação de Vegeta!`}
         />
         <SectionButtons 
-          num1="113" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1={`Loucos por Batalha! Os Saiyajins se Enfrentam de Novo!`}
-          num2="114" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2={`Sedenta por Sangue! Nasce uma Nova Super guerreira!`}
-          num3="115" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3={`Goku vs Kefla! Super Saiyajin Blue Derrotado?!`}
-          num4="116" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4={`Contra-Ataque! A Explosão do Instinto Superior!`}
-          num5="117" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5={`Batalha do Amor! Os Androides Contra o Universo 2!`}
-          num6="118" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6={`Tragédia Anunciada! O Universo Eliminado é...`}
+          num1="113" url1="" name1={`Loucos por Batalha! Os Saiyajins se Enfrentam de Novo!`}
+          num2="114" url2="" name2={`Sedenta por Sangue! Nasce uma Nova Super guerreira!`}
+          num3="115" url3="" name3={`Goku vs Kefla! Super Saiyajin Blue Derrotado?!`}
+          num4="116" url4="" name4={`Contra-Ataque! A Explosão do Instinto Superior!`}
+          num5="117" url5="" name5={`Batalha do Amor! Os Androides Contra o Universo 2!`}
+          num6="118" url6="" name6={`Tragédia Anunciada! O Universo Eliminado é...`}
         />
         <SectionButtons 
-          num1="119" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1={`É Impossível se Defender?! O Intenso Ataque Furtivo!`}
-          num2="120" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2={`A Estratégia Perfeita! O Assassino do Universo Três!`}
-          num3="121" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3={`Tudo ou Nada! Os Quatro Guerreiros Contra o Universo Sete!`}
-          num4="122" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4={`Em Nome do Orgulho! O Desafio de Vegeta Para Ser o Mais Forte!`}
-          num5="123" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5={`Liberem Todo o Seu Poder! Goku e Vegeta!`}
-          num6="124" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6={`Ataque Implacável! A Investida Final de Gohan!`}
+          num1="119" url1="" name1={`É Impossível se Defender?! O Intenso Ataque Furtivo!`}
+          num2="120" url2="" name2={`A Estratégia Perfeita! O Assassino do Universo Três!`}
+          num3="121" url3="" name3={`Tudo ou Nada! Os Quatro Guerreiros Contra o Universo Sete!`}
+          num4="122" url4="" name4={`Em Nome do Orgulho! O Desafio de Vegeta Para Ser o Mais Forte!`}
+          num5="123" url5="" name5={`Liberem Todo o Seu Poder! Goku e Vegeta!`}
+          num6="124" url6="" name6={`Ataque Implacável! A Investida Final de Gohan!`}
         />
         <SectionButtons 
-          num1="125" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1={`Presença Imponente! Toppo, O Deus da Destruição!`}
-          num2="126" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2={`Superar um Deus! O Golpe Desesperado de Vegeta!`}
-          num3="127" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3={`Obstaculo Final! A Última Barreira de Esperança!`}
-          num4="128" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4={`Orgulho Honrado! A Queda de Vegeta!`}
-          num5="129" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5={`Limites Super-superados! Dominando o Instinto Superior!`}
-          num6="130" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6={`A Maior Luta de Todos os Tempos! A Batalha Definitiva pela Sobrevivência!`}
+          num1="125" url1="" name1={`Presença Imponente! Toppo, O Deus da Destruição!`}
+          num2="126" url2="" name2={`Superar um Deus! O Golpe Desesperado de Vegeta!`}
+          num3="127" url3="" name3={`Obstaculo Final! A Última Barreira de Esperança!`}
+          num4="128" url4="" name4={`Orgulho Honrado! A Queda de Vegeta!`}
+          num5="129" url5="" name5={`Limites Super-superados! Dominando o Instinto Superior!`}
+          num6="130" url6="" name6={`A Maior Luta de Todos os Tempos! A Batalha Definitiva pela Sobrevivência!`}
         />
         <section className="episodeButtons">
-          <SectionLinks num="131" url="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name={`Desfecho Milagroso! Adeus, Goku, Nos Vemos em Breve!`} />
+          <SectionLinks num="131" url="" name={`Desfecho Milagroso! Adeus, Goku, Nos Vemos em Breve!`} />
         </section>
       </section>
     </div>
