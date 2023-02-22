c4d4ac008a9bd9022a608dba8d87089ee0ddb048
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-02-22 09:42:52 -0300

    Dragon ball super season two news eps

--- Client/src/components/DragonBallZ/SeasonTwo/index.tsx
+++ Client/src/components/DragonBallZ/SeasonTwo/index.tsx
@@ -8,100 +8,98 @@ export default function SeasonTwoBbz() {
       <h1 className="text-center text-4xl font-bold">Saga Freeza</h1>
       <section className="mt-8 flex flex-col items-center">
         <SectionButtons 
-          num1="36" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Vamos ao planeta de Piccolo"
-          num2="37" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="Um lugar misterioso chamado Yunzabit"
-          num3="38" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Vamos para Namekusei"
-          num4="39" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Serão amigos ou inimigos"
-          num5="40" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Isso é Namekusei"
-          num6="41" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Extraterrestres muito gentis"
+          num1="36" url1="1yYkgAtJcWas7KRhjxEjWC8eDpjLonfhB" name1="Vamos ao planeta de Piccolo"
+          num2="37" url2="1pICSwv9Hs2WmvnCwAA67ZwMPeFkx1nnw" name2="Um lugar misterioso chamado Yunzabit"
+          num3="38" url3="1Rp3ujjL8seG-cXFw7O8mcQF1_hXvjWpa" name3="Vamos para Namekusei"
+          num4="39" url4="1AhrTEgQt7T6YTbkoyQbaWGkZhkdLCrai" name4="Serão amigos ou inimigos"
+          num5="40" url5="1ttPI3A1Wur37hKuXxtJeflk1q1IOov7x" name5="Isso é Namekusei"
+          num6="41" url6="1nFJxTTzyoQ2DrgZoyVcwalB5XREBC1AN" name6="Extraterrestres muito gentis"
+        />
+        <SectionButtons 
+          num1="42" url1="12TotNBsmx37C--YWr19J5rDSA2FvXvtG" name1="O Planeta Freeza No.79 Vegeta é revivido"
+          num2="43" url2="1by72Hit2w6-8hJnDsWk-VJYJ4fDl_wW2" name2="Reunimos as Esferas do Dragão"
+          num3="44" url3="1kmWH7zrxOKCTd0uA8UxliW5zeBsSyw8D" name3="Um novo inimigo! O Imperador do universo, Freeza"
+          num4="45" url4="15wkNuWGcXn0XI_tl7tlUt03YyXtFn8uT" name4="A ambição de Vegeta! Sou o guerreiro mais forte do Universo"
+          num5="46" url5="1PiG4cZYH0tF0MSB0eRUJ1A-9Xyy-NbAF" name5="A viagem de Goku à Namekusei"
+          num6="47" url6="1a4ObNfbk1RdKU1pVOCxbnvYEqf5dgTzf" name6="Um ataque surpresa! O ancião destrói os rastreadores"
+        />
+        <SectionButtons 
+          num1="48" url1="1-swNQSZ0sNozFD6DdVJq4zxjErAWnRiU" name1="Gohan se arrisca! O perseguidor Dodória"
+          num2="49" url2="1aM2D5fU2L-AmguLMPSbxmTKsdjs-zX2o" name2="A morte de Dodória"
+          num3="50" url3="1Bxm-b27_DX6pyA7lIFXdWOm9HAg_3uLF" name3="Goku usa um Kamehameha para salvar a pele"
+          num4="51" url4="1XdruXC59ZgpEZbGng4GTFYAxUd8Dzru-" name4="Os grandes lutadores se reúnem com o Sr.Kaio"
+          num5="52" url5="14EAWwxpqp7VHU40ASk3-Yy41hMRCsiQI" name5="Preste atenção Goku! Não chegue perto de Freeza"
+          num6="53" url6="1dnISX0vsdLQG7965-qfKcSbLbtcK0CRY" name6="A incrível transformação de Zarbon"
         />
 
         <SectionButtons 
-          num1="42" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="O Planeta Freeza No.79 Vegeta é revivido"
-          num2="43" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="Reunimos as Esferas do Dragão"
-          num3="44" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Um novo inimigo! O Imperador do universo, Freeza"
-          num4="45" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="A ambição de Vegeta! Sou o guerreiro mais forte do Universo"
-          num5="46" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="A viagem de Goku à Namekusei"
-          num6="47" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Um ataque surpresa! O ancião destrói os rastreadores"
+          num1="54" url1="" name1="É preciso proteger o planeta da esperança"
+          num2="55" url2="" name2="Vegeta revive milagrosamente"
+          num3="56" url3="" name3="O complô de Freeza se desfaz"
+          num4="57" url4="" name4="Goku treina numa gravidade 100 vezes maior"
+          num5="58" url5="" name5="O grupo secreto de Freeza"
+          num6="59" url6="" name6="Tome cuidado Bulma"
         />
 
         <SectionButtons 
-          num1="48" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Gohan se arrisca! O perseguidor Dodória"
-          num2="49" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="A morte de Dodória"
-          num3="50" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Goku usa um Kamehameha para salvar a pele"
-          num4="51" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Os grandes lutadores se reúnem com o Sr.Kaio"
-          num5="52" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Preste atenção Goku! Não chegue perto de Freeza"
-          num6="53" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="A incrível transformação de Zarbon"
+          num1="60" url1="" name1="A vontade indestrutível de Goku"
+          num2="61" url2="" name2="A batalha se aproxima! Chegam as forças especiais Ginyuu"
+          num3="62" url3="" name3="Goku se aproxima!! Os temíveis homens de Freeza"
+          num4="63" url4="" name4="Sr.Gurdo se aborrece e utiliza um truque mágico"
+          num5="64" url5="" name5="O terrível Rikum"
+          num6="65" url6="" name6="Não morra Gohan! Goku chega finalmente"
         />
 
         <SectionButtons 
-          num1="54" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="É preciso proteger o planeta da esperança"
-          num2="55" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="Vegeta revive milagrosamente"
-          num3="56" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="O complô de Freeza se desfaz"
-          num4="57" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Goku treina numa gravidade 100 vezes maior"
-          num5="58" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="O grupo secreto de Freeza"
-          num6="59" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Tome cuidado Bulma"
+          num1="66" url1="" name1="Goku!! O lendário Saiyajin"
+          num2="67" url2="" name2="Yeaze e Botter atacam Goku"
+          num3="68" url3="" name3="A aparição do capitão Ginyuu"
+          num4="69" url4="" name4="Ja viram o verdadeiro poder de Goku"
+          num5="70" url5="" name5="Freeza encontra com o Patriarca"
+          num6="71" url6="" name6="Goku é Ginyuu!? e Ginyuu é Goku"
         />
 
         <SectionButtons 
-          num1="60" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="A vontade indestrutível de Goku"
-          num2="61" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="A batalha se aproxima! Chegam as forças especiais Ginyuu"
-          num3="62" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Goku se aproxima!! Os temíveis homens de Freeza"
-          num4="63" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Sr.Gurdo se aborrece e utiliza um truque mágico"
-          num5="64" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="O terrível Rikum"
-          num6="65" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Não morra Gohan! Goku chega finalmente"
+          num1="72" url1="" name1="Sai daí Shenlong!! E realize o meu desejo"
+          num2="73" url2="" name2="Eu não sou ele! Gohan ataque seu pai sem medo"
+          num3="74" url3="" name3="Ginyuu se transforma numa rãn"
+          num4="75" url4="" name4="Diga o código secreto das Esferas do Dragão"
+          num5="76" url5="" name5="Piccolo e Kami-Sama revivem"
+          num6="77" url6="" name6="Será que surge um novo guerreiro"
         />
 
         <SectionButtons 
-          num1="66" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Goku!! O lendário Saiyajin"
-          num2="67" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="Yeaze e Botter atacam Goku"
-          num3="68" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="A aparição do capitão Ginyuu"
-          num4="69" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Ja viram o verdadeiro poder de Goku"
-          num5="70" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Freeza encontra com o Patriarca"
-          num6="71" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Goku é Ginyuu!? e Ginyuu é Goku"
+          num1="78" url1="" name1="Um pesadelo de transformação"
+          num2="79" url2="" name2="Gohan é atacado pelo impiedoso Freeza"
+          num3="80" url3="" name3="A chegada de Piccolo"
+          num4="81" url4="" name4="Piccolo se encarrega de vencer Freeza"
+          num5="82" url5="" name5="A segunda transformação de Freeza"
+          num6="83" url6="" name6="Freeza tem a batalha"
         />
 
         <SectionButtons 
-          num1="72" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Sai daí Shenlong!! E realize o meu desejo"
-          num2="73" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="Eu não sou ele! Gohan ataque seu pai sem medo"
-          num3="74" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Ginyuu se transforma numa rãn"
-          num4="75" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Diga o código secreto das Esferas do Dragão"
-          num5="76" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Piccolo e Kami-Sama revivem"
-          num6="77" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Será que surge um novo guerreiro"
+          num1="84" url1="" name1="A morte do Dendê"
+          num2="85" url2="" name2="Goku se recupera"
+          num3="86" url3="" name3="Morre Vegeta!! Um orgulhoso Saiyajin"
+          num4="87" url4="" name4="Começa a batalha final"
+          num5="88" url5="" name5="Uma luta corpo a corpo"
+          num6="89" url6="" name6="Freeza luta sem usar as mãos"
         />
 
         <SectionButtons 
-          num1="78" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Um pesadelo de transformação"
-          num2="79" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="Gohan é atacado pelo impiedoso Freeza"
-          num3="80" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="A chegada de Piccolo"
-          num4="81" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Piccolo se encarrega de vencer Freeza"
-          num5="82" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="A segunda transformação de Freeza"
-          num6="83" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Freeza tem a batalha"
-        />
-
-        <SectionButtons 
-          num1="84" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="A morte do Dendê"
-          num2="85" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="Goku se recupera"
-          num3="86" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Morre Vegeta!! Um orgulhoso Saiyajin"
-          num4="87" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Começa a batalha final"
-          num5="88" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Uma luta corpo a corpo"
-          num6="89" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Freeza luta sem usar as mãos"
-        />
-
-        <SectionButtons 
-          num1="90" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Goku é muito valente"
-          num2="91" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="Goku faz um Kaioken aumentado 20 vezes"
-          num3="92" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="A gigantesca Genki-Dama! É a última esperança"
-          num4="93" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Não deixem passar esta oportunidade"
-          num5="94" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="O poder destrutivo da Genki-Dama"
-          num6="95" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Goku finalmente se transforma no lendário Super Saiyajin"
+          num1="90" url1="" name1="Goku é muito valente"
+          num2="91" url2="" name2="Goku faz um Kaioken aumentado 20 vezes"
+          num3="92" url3="" name3="A gigantesca Genki-Dama! É a última esperança"
+          num4="93" url4="" name4="Não deixem passar esta oportunidade"
+          num5="94" url5="" name5="O poder destrutivo da Genki-Dama"
+          num6="95" url6="" name6="Goku finalmente se transforma no lendário Super Saiyajin"
         />
 
         <section className="episodeButtons">
-          <SectionLinks num="96" url="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name="Goku vingará todas as mortes" />
-          <SectionLinks num="97" url="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name="Uma grande faísca atravessa Namekusei" />
-          <SectionLinks num="98" url="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name="Freeza utiliza seu poder máximo" />
-          <SectionLinks num="99" url="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name="Namekusei está prestes a explodir" />
+          <SectionLinks num="96" url="" name="Goku vingará todas as mortes" />
+          <SectionLinks num="97" url="" name="Uma grande faísca atravessa Namekusei" />
+          <SectionLinks num="98" url="" name="Freeza utiliza seu poder máximo" />
+          <SectionLinks num="99" url="" name="Namekusei está prestes a explodir" />
         </section>
       </section>
     </div>
