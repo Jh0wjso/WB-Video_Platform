cbd2167bf39eb17f1787f8e23553a79726043e3c
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-02-27 09:28:08 -0300

    Dragon dbz season three finished

--- Client/src/components/DragonBallZ/SeasonThree/index.tsx
+++ Client/src/components/DragonBallZ/SeasonThree/index.tsx
@@ -9,16 +9,16 @@ export default function SeasonThreeBbz() {
       <h1 className="text-center text-4xl font-bold">Eu sou o filho de Goku</h1>
       <section className="mt-8 flex flex-col items-center">
         <SectionButtons 
-          num1="100" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Eu sou o filho de Goku"
-          num2="101" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="O ultimo desejo"
-          num3="102" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Uma batalha extraordinária"
-          num4="103" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="O desespero de Freeza"
-          num5="104" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="A auto-destruição de Freeza"
-          num6="105" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Freeza perde a luta"
+          num1="100" url1="1lcvd64uvvBNWW-CNEzEQ67QYgHO_MSHp" name1="Eu sou o filho de Goku"
+          num2="101" url2="1lG_KIJbRYZc4UONULZIP86T3-0Cq2QcW" name2="O ultimo desejo"
+          num3="102" url3="1HVHCg_ZVE3lC4p-I6sX2sAo7W1_NJh0h" name3="Uma batalha extraordinária"
+          num4="103" url4="14AHbhqR2dSxOlKSWZ9Z7PqP0kybxLMWA" name4="O desespero de Freeza"
+          num5="104" url5="17KHuy5V9HX3p7gPtrdbroIKKXR4ASGTB" name5="A auto-destruição de Freeza"
+          num6="105" url6="1BFsh-xBuG82N5JL9VLEiveuutqvW0ywN" name6="Freeza perde a luta"
         />
         <section className="episodeButtons">
-          <SectionLinks num="114" url="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name="Namekusei Explode" />
-          <SectionLinks num="115" url="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name="Os guerreiros Z ressuscitam" />
+          <SectionLinks num="106" url="16hQCTtBuGTZ-Cf4my-YDje0_zjZMANRP" name="Namekusei Explode" />
+          <SectionLinks num="107" url="19-HQmsfR82EPGGuSgmnzs5gLxSwZ8vVN" name="Os guerreiros Z ressuscitam" />
         </section>
       </section>
     </div>
