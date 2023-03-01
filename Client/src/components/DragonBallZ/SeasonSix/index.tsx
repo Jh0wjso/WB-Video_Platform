7f63cdafc26c9426f42108de8a6c133c7125d308
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-03-01 09:50:19 -0300

    Removing example links for dbz season six

--- Client/src/components/DragonBallZ/SeasonSix/index.tsx
+++ Client/src/components/DragonBallZ/SeasonSix/index.tsx
@@ -6,93 +6,93 @@ export default function SeasonSixDbz(){
         <h1 className="text-center text-4xl font-bold">Saga Cell</h1>
         <section className=" flex flex-col items-center">
             <SectionButtons 
-            num1="140" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Um ovo misterioso encontrado na máquina do tempo"
-            num2="141" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="O nascimento de um novo guerreiro"
-            num3="142" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="O monstro que tem o KI de Goku"
-            num4="143" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="O androide Cell"
-            num5="144" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Cell escapa das mãos de Piccolo"
-            num6="145" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="O esconderijo secreto do laboratório do Dr. Gero"
+            num1="140" url1="" name1="Um ovo misterioso encontrado na máquina do tempo"
+            num2="141" url2="" name2="O nascimento de um novo guerreiro"
+            num3="142" url3="" name3="O monstro que tem o KI de Goku"
+            num4="143" url4="" name4="O androide Cell"
+            num5="144" url5="" name5="Cell escapa das mãos de Piccolo"
+            num6="145" url6="" name6="O esconderijo secreto do laboratório do Dr. Gero"
             />
 
             <SectionButtons 
-            num1="146" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Goku se recupera"
-            num2="147" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="Vamos à sala do templo"
-            num3="148" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Piccolo contra o androide Nº 17"
-            num4="149" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Cell está perto de aperfeiçoar seu corpo"
-            num5="150" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Piccolo luta com todas as androide forças"
-            num6="151" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="A última esperança! O androide Nº 16"
+            num1="146" url1="" name1="Goku se recupera"
+            num2="147" url2="" name2="Vamos à sala do templo"
+            num3="148" url3="" name3="Piccolo contra o androide Nº 17"
+            num4="149" url4="" name4="Cell está perto de aperfeiçoar seu corpo"
+            num5="150" url5="" name5="Piccolo luta com todas as androide forças"
+            num6="151" url6="" name6="A última esperança! O androide Nº 16"
             />
 
             <SectionButtons 
-            num1="152" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Cell absorve o androide Nº 17"
-            num2="153" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="O novo desafio de Goku"
-            num3="154" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Apareça Nº 18"
-            num4="155" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="O super poder de Vegeta"
-            num5="156" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Eu sou o Super vegeta"
-            num6="157" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Um guerreiro orgulhoso"
+            num1="152" url1="" name1="Cell absorve o androide Nº 17"
+            num2="153" url2="" name2="O novo desafio de Goku"
+            num3="154" url3="" name3="Apareça Nº 18"
+            num4="155" url4="" name4="O super poder de Vegeta"
+            num5="156" url5="" name5="Eu sou o Super vegeta"
+            num6="157" url6="" name6="Um guerreiro orgulhoso"
             />
             
             <SectionButtons 
-            num1="158" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="A decisão de Kuririn"
-            num2="159" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="Cell quer aperfeiçoar o seu corpo"
-            num3="160" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Cell o deus da destruição"
-            num4="161" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Super Vegeta corre perigo"
-            num5="162" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Trunks fica furioso"
-            num6="163" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Salvem meu pai"
+            num1="158" url1="" name1="A decisão de Kuririn"
+            num2="159" url2="" name2="Cell quer aperfeiçoar o seu corpo"
+            num3="160" url3="" name3="Cell o deus da destruição"
+            num4="161" url4="" name4="Super Vegeta corre perigo"
+            num5="162" url5="" name5="Trunks fica furioso"
+            num6="163" url6="" name6="Salvem meu pai"
             />
 
             <SectionButtons 
-            num1="164" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="O desespero de viver num futuro infernal"
-            num2="165" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="O ponto fraco de Super Trunks"
-            num3="166" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="O mistério do novo torneio de artes marciais"
-            num4="167" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="O anúncio do torneio de Cell"
-            num5="168" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Os novos poderes de Goku e Gohan"
-            num6="169" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="A tranquilidade de Goku"
+            num1="164" url1="" name1="O desespero de viver num futuro infernal"
+            num2="165" url2="" name2="O ponto fraco de Super Trunks"
+            num3="166" url3="" name3="O mistério do novo torneio de artes marciais"
+            num4="167" url4="" name4="O anúncio do torneio de Cell"
+            num5="168" url5="" name5="Os novos poderes de Goku e Gohan"
+            num6="169" url6="" name6="A tranquilidade de Goku"
             />
 
             <SectionButtons 
-            num1="170" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="O descanso dos guerreiros"
-            num2="171" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="Quando Gohan era bebê"
-            num3="172" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Vamos procurar um novo Kami-Sama"
-            num4="173" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="As Esferas do Dragão voltam a existir"
-            num5="174" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="O sofrimento de Goku"
-            num6="175" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Começa o torneio de Cell"
+            num1="170" url1="" name1="O descanso dos guerreiros"
+            num2="171" url2="" name2="Quando Gohan era bebê"
+            num3="172" url3="" name3="Vamos procurar um novo Kami-Sama"
+            num4="173" url4="" name4="As Esferas do Dragão voltam a existir"
+            num5="174" url5="" name5="O sofrimento de Goku"
+            num6="175" url6="" name6="Começa o torneio de Cell"
             />
 
             <SectionButtons 
-            num1="176" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="O exército do Sr. Satan"
-            num2="177" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="Goku contra Cell"
-            num3="178" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="O super Kamehameha de Cell"
-            num4="179" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="Cell muda as regras do jogo"
-            num5="180" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Goku se rende"
-            num6="181" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Gohan… O jovem mais forte"
+            num1="176" url1="" name1="O exército do Sr. Satan"
+            num2="177" url2="" name2="Goku contra Cell"
+            num3="178" url3="" name3="O super Kamehameha de Cell"
+            num4="179" url4="" name4="Cell muda as regras do jogo"
+            num5="180" url5="" name5="Goku se rende"
+            num6="181" url6="" name6="Gohan… O jovem mais forte"
             />
 
             <SectionButtons 
-            num1="182" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Gohan libera todo o seu poder"
-            num2="183" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="O ataque dos Cell juniores"
-            num3="184" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Gohan fica nervoso"
-            num4="185" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="A destruição dos Cells juniores"
-            num5="186" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Cell é golpeado"
-            num6="187" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="A transformação de Cell"
+            num1="182" url1="" name1="Gohan libera todo o seu poder"
+            num2="183" url2="" name2="O ataque dos Cell juniores"
+            num3="184" url3="" name3="Gohan fica nervoso"
+            num4="185" url4="" name4="A destruição dos Cells juniores"
+            num5="186" url5="" name5="Cell é golpeado"
+            num6="187" url6="" name6="A transformação de Cell"
             />
 
             <SectionButtons 
-            num1="188" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Adeus a todos"
-            num2="189" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="Um terrível pesadelo em pleno dia"
-            num3="190" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="Uma mensagem para Gohan"
-            num4="191" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="A luta terminou obrigado Goku"
-            num5="192" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Goku vai treinar no além"
-            num6="193" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Pai jamais vou me dar por vencido"
+            num1="188" url1="" name1="Adeus a todos"
+            num2="189" url2="" name2="Um terrível pesadelo em pleno dia"
+            num3="190" url3="" name3="Uma mensagem para Gohan"
+            num4="191" url4="" name4="A luta terminou obrigado Goku"
+            num5="192" url5="" name5="Goku vai treinar no além"
+            num6="193" url6="" name6="Pai jamais vou me dar por vencido"
             />
             
             <SectionButtons 
-            num1="194" url1="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name1="Outro final feliz"
-            num2="195" url2="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name2="O lutador mais forte do outro mundo"
-            num3="196" url3="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name3="O encontro dos lutadores mais valentes"
-            num4="197" url4="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name4="O torneio fica emocionante"
-            num5="198" url5="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name5="Quem vai ganhar!? Goku ou Paikuhan"
-            num6="199" url6="1lw0zTaPLke9kRB6u4KEkXgaHMKCkuKZm" name6="Goku faz um Kamehameha com toda rapidez"
+            num1="194" url1="" name1="Outro final feliz"
+            num2="195" url2="" name2="O lutador mais forte do outro mundo"
+            num3="196" url3="" name3="O encontro dos lutadores mais valentes"
+            num4="197" url4="" name4="O torneio fica emocionante"
+            num5="198" url5="" name5="Quem vai ganhar!? Goku ou Paikuhan"
+            num6="199" url6="" name6="Goku faz um Kamehameha com toda rapidez"
             />
         </section>
     </div>
