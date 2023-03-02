1b51ea90db812e15421453ebf8652b1dabf071b6
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-03-02 10:14:58 -0300

    New dbz episodes

--- Client/src/components/DragonBallZ/SeasonSix/index.tsx
+++ Client/src/components/DragonBallZ/SeasonSix/index.tsx
@@ -6,48 +6,48 @@ export default function SeasonSixDbz(){
         <h1 className="text-center text-4xl font-bold">Saga Cell</h1>
         <section className=" flex flex-col items-center">
             <SectionButtons 
-            num1="140" url1="" name1="Um ovo misterioso encontrado na máquina do tempo"
-            num2="141" url2="" name2="O nascimento de um novo guerreiro"
-            num3="142" url3="" name3="O monstro que tem o KI de Goku"
-            num4="143" url4="" name4="O androide Cell"
-            num5="144" url5="" name5="Cell escapa das mãos de Piccolo"
-            num6="145" url6="" name6="O esconderijo secreto do laboratório do Dr. Gero"
+            num1="140" url1="1gf8S0FxoX-TS4s6TqGb8WoYfTpcFQQ-W" name1="Um ovo misterioso encontrado na máquina do tempo"
+            num2="141" url2="1PlOnUiqQkGPnA5F69hcAfYHepFnFzzyR" name2="O nascimento de um novo guerreiro"
+            num3="142" url3="1Q4pDzeOQxcrwaNGtZq3or8pgyD0hobZR" name3="O monstro que tem o KI de Goku"
+            num4="143" url4="1Pwdt9Np-YflAWE5GJTeThldOcZOqGeRN" name4="O androide Cell"
+            num5="144" url5="1NVOP5FlsHPNAjxmOMGqrmVFX7rokSAKZ" name5="Cell escapa das mãos de Piccolo"
+            num6="145" url6="1odxaRLzkrKVunM-XJriicn0uDk1EVVof" name6="O esconderijo secreto do laboratório do Dr. Gero"
             />
 
             <SectionButtons 
-            num1="146" url1="" name1="Goku se recupera"
-            num2="147" url2="" name2="Vamos à sala do templo"
-            num3="148" url3="" name3="Piccolo contra o androide Nº 17"
-            num4="149" url4="" name4="Cell está perto de aperfeiçoar seu corpo"
-            num5="150" url5="" name5="Piccolo luta com todas as androide forças"
-            num6="151" url6="" name6="A última esperança! O androide Nº 16"
+            num1="146" url1="1yUyjnA1ACWdkUH2fV1MNYrMBDiAVPJD6" name1="Goku se recupera"
+            num2="147" url2="13AzZCvKdrRoXCv70nZtb5WIf8MqM5WFO" name2="Vamos à sala do templo"
+            num3="148" url3="1vyP4aJl2IHARQgj9y_yoG161nOvJqyVR" name3="Piccolo contra o androide Nº 17"
+            num4="149" url4="1lOpSMue3UKipz6UM6iOkar1cCsTHIR1J" name4="Cell está perto de aperfeiçoar seu corpo"
+            num5="150" url5="1UUOy51BiOANuopXgd3Cy2kX-h1nJ0PBr" name5="Piccolo luta com todas as androide forças"
+            num6="151" url6="1tcGZz0P5gNmrplcMQJgkXQX6HNZX0n-A" name6="A última esperança! O androide Nº 16"
             />
 
             <SectionButtons 
-            num1="152" url1="" name1="Cell absorve o androide Nº 17"
-            num2="153" url2="" name2="O novo desafio de Goku"
-            num3="154" url3="" name3="Apareça Nº 18"
-            num4="155" url4="" name4="O super poder de Vegeta"
-            num5="156" url5="" name5="Eu sou o Super vegeta"
-            num6="157" url6="" name6="Um guerreiro orgulhoso"
-            />
-            
-            <SectionButtons 
-            num1="158" url1="" name1="A decisão de Kuririn"
-            num2="159" url2="" name2="Cell quer aperfeiçoar o seu corpo"
-            num3="160" url3="" name3="Cell o deus da destruição"
-            num4="161" url4="" name4="Super Vegeta corre perigo"
-            num5="162" url5="" name5="Trunks fica furioso"
-            num6="163" url6="" name6="Salvem meu pai"
+            num1="152" url1="1Jqf7P_7Es8Go8FEioIpet3EyqQ-JCnXA" name1="Cell absorve o androide Nº 17"
+            num2="153" url2="1Nk5av64hcvAzY0AwOFYeRimOH38LQjRC" name2="O novo desafio de Goku"
+            num3="154" url3="1DFC__euvvpLwZzRsLxrygwtTi5y0ig0m" name3="Apareça Nº 18"
+            num4="155" url4="1OYIIlubHpwA78LoZPB-T0pqG8yEzQLpX" name4="O super poder de Vegeta"
+            num5="156" url5="1Kgs3swdfS2UjalmoR045ISzbyv297496" name5="Eu sou o Super vegeta"
+            num6="157" url6="1ty6i9EQQuIyVYjYRss1hYRch7itkNade" name6="Um guerreiro orgulhoso"
             />
 
             <SectionButtons 
-            num1="164" url1="" name1="O desespero de viver num futuro infernal"
-            num2="165" url2="" name2="O ponto fraco de Super Trunks"
-            num3="166" url3="" name3="O mistério do novo torneio de artes marciais"
-            num4="167" url4="" name4="O anúncio do torneio de Cell"
-            num5="168" url5="" name5="Os novos poderes de Goku e Gohan"
-            num6="169" url6="" name6="A tranquilidade de Goku"
+            num1="158" url1="1j8vusRSzwvhyP_g3YWIVV9W51V251NLi" name1="A decisão de Kuririn"
+            num2="159" url2="1ZMDToLIyKIz3bUhJVmF-tHIfVAbDn3fS" name2="Cell quer aperfeiçoar o seu corpo"
+            num3="160" url3="15z_ps4aWNaegf66uaoAbgFyS_BbxZ63m" name3="Cell o deus da destruição"
+            num4="161" url4="1X5SzbwI0kki--A0t2pkWqVGj5wRiNKkZ" name4="Super Vegeta corre perigo"
+            num5="162" url5="1ddwuu6dQZfvWZaHZYYfSlhV9fVNapgse" name5="Trunks fica furioso"
+            num6="163" url6="1ITPi_I-4SaqjiPEYv_QswC7lWc7iCXoS" name6="Salvem meu pai"
+            />
+
+            <SectionButtons 
+            num1="164" url1="1tvma0ZcvdEhzmI00bYh7m4vShofb9gh3" name1="O desespero de viver num futuro infernal"
+            num2="165" url2="1F9dpFY0c1ZzbYwWgTeU4kgiFg1DtqV8f" name2="O ponto fraco de Super Trunks"
+            num3="166" url3="17xoGXLoxfE7-SP34yAgagRP9BlCYbKVi" name3="O mistério do novo torneio de artes marciais"
+            num4="167" url4="1TPe0x0-BeGjSOsrnUpGovIhReD5oysdT" name4="O anúncio do torneio de Cell"
+            num5="168" url5="1sGFfdLQQDJHyF0dqXgbLRjUk3NIFQR_K" name5="Os novos poderes de Goku e Gohan"
+            num6="169" url6="1nNGUyn0pu8D8uuN_b84d45HEiAKNQVdu" name6="A tranquilidade de Goku"
             />
 
             <SectionButtons 
