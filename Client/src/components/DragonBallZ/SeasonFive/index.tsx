66acf4cb8e7966d731d559fdeb7d0730f7295be8
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-03-17 10:51:35 -0300

    :bricks: ci: Solving links problems for Dragon ball z

--- Client/src/components/DragonBallZ/SeasonFive/index.tsx
+++ Client/src/components/DragonBallZ/SeasonFive/index.tsx
@@ -8,34 +8,34 @@ export default function SeasonFiveBbz() {
       <h1 className="text-center text-4xl font-bold">Saga dos Androides</h1>
       <section className="mt-8 flex flex-col items-center">
         <SectionButtons 
-          name1="Este é o planeta Terra papai" num1="118" url1="1UwGi2U8ME8VTMw9gOBV7Wfs_qRlE-isJ"
-          name2="Eu me encarregarei de vencer Freeza" num2="119" url2="1cK3DrzTO4o8yfCpkcTqNqP7XAuEVQcF6"
-          name3="O fim de Freeza" num3="120" url3="1yvvgXkhc0WazH7pZ9_T1e4s9vo1z0gJx"
-          name4="Goku voltou" num4="121" url4="11ud-hawDkzMA_qbQ8ejb23IehTPxLacE"
-          name5="A identidade do jovem misterioso" num5="122" url5="1cyP_cx9d-4vvXwQeOZlD0_Ma4VBZ2DD1"
-          name6="A técnica especial de Goku" num6="123" url6="1nc5qi2iM5BXDDNRiuSBYQqWFkhrnYV9K"
+          num1="118" name1="dragon-ball-z-dublado"
+          num2="119" name2="dragon-ball-z-dublado"
+          num3="120" name3="dragon-ball-z-dublado"
+          num4="121" name4="dragon-ball-z-dublado"
+          num5="122" name5="dragon-ball-z-dublado"
+          num6="123" name6="dragon-ball-z-dublado"
         />
         <SectionButtons 
-          name1="Vou superar Goku" num1="124" url1="1XZSylJ2t5RKEWOyZ620iAp14TqM4OE1i"
-          name2="Uma prova muito difícil para Goku" num2="125" url2="1mUbuMUBSZ951Wmq7plIr12MXexzIY8cB"
-          name3="Quem são os androides" num3="126" url3="1sSqvTJaR2Yfy9szvYZCP7nwpodlYMG71"
-          name4="Os terríveis androides Nº 19 e 20" num4="127" url4="129kvGizslwY13BlqGrzCXnHJFmr3BJxB"
-          name5="Goku tem dois problemas" num5="128" url5="1_qOfqzGrXLcCmBeLS-gwDa8VrxqEawhv"
-          name6="Vegeta se transforma num super Saiyajin" num6="129" url6="1X23HTtK19CzYpdpFiciJTwxyDqM6s43I"
+          num1="124" name1="dragon-ball-z-dublado"
+          num2="125" name2="dragon-ball-z-dublado"
+          num3="126" name3="dragon-ball-z-dublado"
+          num4="127" name4="dragon-ball-z-dublado"
+          num5="128" name5="dragon-ball-z-dublado"
+          num6="129" name6="dragon-ball-z-dublado"
         />
         <SectionButtons 
-          name1="O segredo do Dr. Maki" num1="130" url1="1O5DtWu04rHXhM7TyPV8trNGUh20C8lzf"
-          name2="A realidade é muito pior do que o futuro" num2="131" url2="1we0KnovQBRaq2au0xugTtHxXglTrdOr0"
-          name3="É preciso encontrar o laboratório do Dr. Maki" num3="132" url3="1LPLd_i3o8vR_xmgikMjXUp4pm3bExXW4"
-          name4="Os androides nº 17 e 18 despertam" num4="133" url4="1Qq1FfO7nxDHUgnGfbhNwvDQPKEaj3Jw9"
-          name5="O exército encarregado de matar Goku" num5="134" url5="18zKQoxjDREvYZGobfPEmv6wkYlXk8JBJ"
-          name6="As habilidade do androide nº 18" num6="135" url6="1e7OHD6ApRxMk3cy1NC94Trh6Ik0Zrw3t"
+          num1="130" name1="dragon-ball-z-dublado"
+          num2="131" name2="dragon-ball-z-dublado"
+          num3="132" name3="dragon-ball-z-dublado"
+          num4="133" name4="dragon-ball-z-dublado"
+          num5="134" name5="dragon-ball-z-dublado"
+          num6="135" name6="dragon-ball-z-dublado"
         />
-        <section className="episodeButtons">
-          <SectionLinks name={"Será este o fim dos guerreiros Z"} url={"1KvZqUazWqkE6J_R3B_o2r58TCk7nZu9D"} num={"136"} />
-          <SectionLinks name={"A decisão de Piccolo"} url={"18F_TRk1dAEIjovaplduEP1p4CHD2E4u-"} num={"137"}  />
-          <SectionLinks name={"Os androides destruidores"} url={"12YwOmJ1S3O9VFyOS89EwO4l8OZlgZeg5"} num={"138"}  />
-          <SectionLinks name={"Bulma descobre um grande mistério"} url={"1GcQRB9FDV0UszGgbkIJa9Zrkkn8usI9w"} num={"139"}  />
+        <section className="episodeButtons"> 
+          <SectionLinks num={"136"} name={"dragon-ball-z-dublado"}  /> 
+          <SectionLinks num={"137"} name={"dragon-ball-z-dublado"}  /> 
+          <SectionLinks num={"138"} name={"dragon-ball-z-dublado"}  /> 
+          <SectionLinks num={"139"} name={"dragon-ball-z-dublado"}  /> 
         </section>
       </section>
     </div>
--- Client/src/components/DragonBallZ/SeasonOne/index.tsx
+++ Client/src/components/DragonBallZ/SeasonOne/index.tsx
@@ -8,52 +8,52 @@ export default function SeasonOneBbz() {
       <h1 className="text-center text-4xl font-bold">Saga dos Sayajins</h1>
       <section className="mt-8 flex flex-col items-center">
         <SectionButtons 
-          num1="1" url1="1PwHTtguciWb7A0_e-OP-_vVK3Xbc8jae" name1="Surge um mini Goku! Seu nome é Gohan"
-          num2="2" url2="1PuUzaZQSZwd4f_lgT_jXetbGo3mWK9Yu" name2="Um guerreiro muito poderoso com antecedentes"
-          num3="3" url3="1Pud1YkdkqkQkHPSa_HE5oPojqzlkXwma" name3="A combinação mais forte deste mundo"
-          num4="4" url4="1PnGwK6s6Cb6mlsKDyOAxNyIORRaNrO7O" name4="Piccolo usa sua melhor estratégia! Gohan é um garoto"
-          num5="5" url5="1PoSJeNm-SdViDLp5LUmapoQ6BPYsQ8LX" name5="Goku morre! Só existe uma chance"
-          num6="6" url6="1PnFEAcMIwCxZVQFm4YztFDCZ_-9owfJX" name6="Enma Samá também se surpreende! Terá que lutar"
+          num1="1"  name1="dragon-ball-z-dublado"        
+          num2="2"  name2="dragon-ball-z-dublado"
+          num3="3"  name3="dragon-ball-z-dublado"
+          num4="4"  name4="dragon-ball-z-dublado"
+          num5="5"  name5="dragon-ball-z-dublado"
+          num6="6"  name6="dragon-ball-z-dublado"
         />
         <SectionButtons 
-          num1="7" url1="1QDJgaT_V_EkWT4ko36xkASke5qGp_l6A" name1="Terá que sobreviver aos dinossauros! O difícil"
-          num2="8" url2="1Q3GS4aPLQ5KcwyiCoXJrFjmK3CIKxmO-" name2="Uma transformação por causa da lua"
-          num3="9" url3="1Q4dMMIcVM8P3I56oqzsi2OIl4fQHg3E1" name3="Perdão Sr. Robô! Lágrimas que desaparecem no deserto"
-          num4="10" url4="1QSElWhGexDLTRiuXbyEvR7aXWkm7ga4R" name4="Não chore Gohan! É a primeira batalha"
-          num5="11" url5="1QKg9mrZiXc-nCd4-w81FaCP6pnbU_kim" name5="Os guerreiros mais fortes do universo"
-          num6="12" url6="1QMD_p-Pv21qVWEGYS68XUXSJq2ZcweGS" name6="Um pequeno descanso! Goku cai do caminho da serpente"
+          num1="7"  name1="dragon-ball-z-dublado"
+          num2="8"  name2="dragon-ball-z-dublado"
+          num3="9"  name3="dragon-ball-z-dublado"
+          num4="10" name4="dragon-ball-z-dublado"
+          num5="11" name5="dragon-ball-z-dublado"
+          num6="12" name6="dragon-ball-z-dublado"
         />
         <SectionButtons 
-          num1="13" url1="1R4kMnkdyAgCsMoNRj1ln9H1pw6UZHkmo" name1="Não toque nisso! A fruta secreta de Enma Sama"
-          num2="14" url2="1QmW4n50qH4qXFZw8mirlghCQ1d57irte" name2="Uma doce tentação! A hospitalidade da princesa serpente"
-          num3="15" url3="1Qy4PRE80aFDmWF_mKoPTUYr5HNtiQGbf" name3="Fugindo de Piccolo! Gohan enfrenta uma tempestade"
-          num4="16" url4="1QwG0fEUsmGsNLf345b7ZpoPLV8ue01r1" name4="Corre Gohan! Chi-Chi o espera na Montanha Paozu"
-          num5="17" url5="1QpghbsnrZ3jNQYiBa0bJxoSHalXllHGo" name5="Uma cidade sem futuro! Um longo caminho até a vitória"
-          num6="18" url6="1QiWmI8kjTweX3qA4olHxPEd1IoF9l6ZK" name6="O final do caminho da serpente! É o Sr. Kaio"
+          num1="13" name1="dragon-ball-z-dublado"
+          num2="14"  name2="dragon-ball-z-dublado"
+          num3="15"  name3="dragon-ball-z-dublado"
+          num4="16"  name4="dragon-ball-z-dublado"
+          num5="17"  name5="dragon-ball-z-dublado"
+          num6="18"  name6="dragon-ball-z-dublado"
         />
         <SectionButtons 
-          num1="19" url1="12_GuGnyAB1EM0o1pOqi6unWOic104RWz" name1="Domine a força da gravidade! Agarre Bubb"
-          num2="20" url2="1sbMS68U-BQpib6rGpS5_WeUifhWvaR6u" name2="A lenda revive! Os antepassados de Goku"
-          num3="21" url3="1jZmjv7ceRokclzdNW6uBnsNQwVG0IgiZ" name3="Sai daí Shen-Long! Os Sayajins chegaram à terra"
-          num4="22" url4="1eRdXH-gHVFAw2SLU2VYlrZrFu4H-y3Vi" name4="Impossível! Saibaimans nascidos da terra"
-          num5="23" url5="1VG9vBSnqF4sIxLj245Ok1FHkQ9YdXMzX" name5="Yamcha morre! Os temíveis Saibaimans"
-          num6="24" url6="1kY8ZgrMRYArpvS8iHZ0MVAUuFywb3N3e" name6="Adeus Ten-Shin! A arte do sacrifício de Chaos"
+          num1="19"  name1="dragon-ball-z-dublado"
+          num2="20" name2="dragon-ball-z-dublado"
+          num3="21"  name3="dragon-ball-z-dublado"
+          num4="22"  name4="dragon-ball-z-dublado"
+          num5="23"  name5="dragon-ball-z-dublado"
+          num6="24"  name6="dragon-ball-z-dublado"
         />
 
         <SectionButtons 
-          num1="25" url1="1w94RUJI1MP25VxSeXAFl3lBlCh_QSlLd" name1="O sacrifício de Tenshinhan! O último Kikiuhouda"
-          num2="26" url2="1wGqczccxCB2htPoV7nCWZvMCNKeEGhj0" name2="Só restam três horas de vida na terra! Apresse-se Goku"
-          num3="27" url3="1WXbFSpk7iLONP-gBhksyeECUIdidKyy2" name3="Explode a fúria de Gohan"
-          num4="28" url4="15O3JzfF2A6FRDvXO-WvLGJxtZMAQWxnt" name4="O ataque dos Sayajins! Kami-Sama e Piccolo morrem"
-          num5="29" url5="1VOaeqrciR3KuXWwAfGtvdlE1qehHEBpz" name5="Papai você é incrível! Uma técnica mortal chamada Kaioken"
-          num6="30" url6="16iGpZVM_AcF1HFX5exrRb36axSpzVD1B" name6="Uma luta além dos limites! Goku contra Vegeta"
+          num1="25"  name1="dragon-ball-z-dublado"
+          num2="26"  name2="dragon-ball-z-dublado"
+          num3="27"  name3="dragon-ball-z-dublado"
+          num4="28"  name4="dragon-ball-z-dublado"
+          num5="29"  name5="dragon-ball-z-dublado"
+          num6="30"  name6="dragon-ball-z-dublado"
         />
         <section className="episodeButtons">
-          <SectionLinks num="31" url="1UBriUxQBPUnIozM5kC-5PJvMAtJJIU8-" name="Arrisque a vida Goku! A última opção" />
-          <SectionLinks num="32" url="1shakrpru_HawgVL2yjJCeIm2z24jl2QD" name="Seu poder de luta aumentou dez vezes" />
-          <SectionLinks num="33" url="13baU7nOcr8a3MQopOR1KC-Zyv1LmSg4p" name="Não morra pai!! O poder oculto de Gohan" />
-          <SectionLinks num="34" url="12gdIveLOSY242i4k5KE85Of37EUD0CQC" name="Pegue-o Kuririn! Uma Genki-Dama cheia de esperança" />
-          <SectionLinks num="35" url="1y-peVZo8bBxS2MFv0NPBRwGEVrLw_Ikx" name="Ocorre um milagre! A transformação de Gohan" />
+          <SectionLinks num="31"  name="dragon-ball-z-dublado" />
+          <SectionLinks num="32"  name="dragon-ball-z-dublado" />
+          <SectionLinks num="33"  name="dragon-ball-z-dublado" />
+          <SectionLinks num="34"  name="dragon-ball-z-dublado" />
+          <SectionLinks num="35"  name="dragon-ball-z-dublado" />
         </section>
       </section>
     </div>
