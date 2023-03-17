70f9534f51eb5671e90f05e1f06ba641438e3de4
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-03-17 11:01:43 -0300

    :bricks: ci: Solving links problems for Dragon ball

--- Client/src/components/DragonBall/SeasonOne/index.tsx
+++ Client/src/components/DragonBall/SeasonOne/index.tsx
@@ -9,25 +9,25 @@ export default function SeasonOne() {
       <section className="mt-8 flex flex-col items-center">
         <section className="episodeButtons">
         <SectionButtons 
-          num1="1" url1={`1V-tvJDuEVrEc8ovXPKJMVgOVZBsVD_BG`} name1={`O segredo das Esferas do Dragão!`}
-          num2="2" url2={`1WK1NgTZzm0BxbyW2VcjTnm4oV1FvKYrJ`} name2={`A busca do imperador`}
-          num3="3" url3={`1YfOaAtXsogO5XI8NgToZUuewgSfCjQXr`} name3={`A nuvem voadora do Kame`}
-          num4="4" url4={`1Tfdqj_kYJ4MBlo285c6ngkfkbqxjsxkk`} name4={`Oolong, o sequestrador de garotas`}
-          num5="5" url5={`1CxOWNAmP_F0ZH0mBUqAlQ_q5st6OlUTs`} name5={`O forte e malvado Yamcha vive no dese`}
-          num6="6" url6={`1D5yb86_LXhrj1hCsxlOrp-8ViRyqAMfP`} name6={`Vigie as Esferas do Dragão`}
+          num1="1" name1={`dragon-ball-dublado`}
+          num2="2" name2={`dragon-ball-dublado`}
+          num3="3" name3={`dragon-ball-dublado`}
+          num4="4" name4={`dragon-ball-dublado`}
+          num5="5" name5={`dragon-ball-dublado`}
+          num6="6" name6={`dragon-ball-dublado`}
         />
         </section>
         <section className="episodeButtons">
         <SectionButtons 
-          num1="7" url1={`1DdCkkcdMkJjFknVvFP0rVClsCuGNnjTZ`} name1={`O rei Cutelo na montanha do fogo`}
-          num2="8" url2={`1Djhd5aP-hVHniS4eieVZ78qY72n8jweX`} name2={`Uma transformação por causa da lua`}
-          num3="9" url3={`1DXOes0Uc2sCOTEQtbKl5e0GRmDG1ZysH`} name3={`O truque especial do chefe Coelho`}
-          num4="10" url4={`1DbAGUnpP1YftYiq8_4jRPPhNY95urkAE`} name4={`As Esferas do Dragão são roubadas`}
-          num5="11" url5={`1DpZ3lYUS5iYdpSraix_oG3wTGkvuvuiI`} name5={`O Dragão é chamado`}
-          num6="12" url6={`1DpzyZC_B9orGD4fylblQWmZqg6b0V7CL`} name6={`Um pedido feito ao Deus Dragão`}
+          num1="7" name1={`dragon-ball-dublado`}
+          num2="8" name2={`dragon-ball-dublado`}
+          num3="9" name3={`dragon-ball-dublado`}
+          num4="10" name4={`dragon-ball-dublado`}
+          num5="11" name5={`dragon-ball-dublado`}
+          num6="12" name6={`dragon-ball-dublado`}
         />
         </section>
-        <SectionLinks num="13" url={`1Dm8-mRk-KQNzu622eIOOe4d8YXiAng5Q`} name={`A transformação de Goku`} />
+        <SectionLinks num="13" name={`dragon-ball-dublado`} />
       </section>
     </div>
   );
