4dab9eb34687c1778159e990ccb2a608edf48878
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-04-04 09:44:12 -0300

    :recycle: refactor: Refactor on Dragon Ball z Links

--- Client/src/components/DragonBallZ/SeasonFinal/index.tsx
+++ Client/src/components/DragonBallZ/SeasonFinal/index.tsx
@@ -1,150 +1,150 @@
 import "../styles.css";
-import SectionButtons from "../../SectionButtons";
-import SectionLinks from "../../SectionLinks";
+import NewSectionButtons from "../../NewSectionButtons";
+import NewSectionLinks from "../../NewSectionLinks";
 
 export default function SeasonFinalDbz() {
   return (
     <div className="containerEps">
       <h1 className="text-center text-4xl font-bold">Saga Buu</h1>
       <section className="flex flex-col items-center">
-        <SectionButtons 
-          num1="200" url1="" name1="Gohan vai para escola"
-          num2="201" url2="" name2="O guerreiro que luta pela justiça! O Grande Saiyaman"
-          num3="202" url3="" name3="O primeiro encontro de Gohan"
-          num4="203" url4="" name4="Gohan salva Videl"
-          num5="204" url5="" name5="O ladrão é o Grande Saiyaman"
-          num6="205" url6="" name6="Goku vai voltar"
+        <NewSectionButtons 
+          num1="200" name1="dragon-ball-z-dublado"
+          num2="201" name2="dragon-ball-z-dublado"
+          num3="202" name3="dragon-ball-z-dublado"
+          num4="203" name4="dragon-ball-z-dublado"
+          num5="204" name5="dragon-ball-z-dublado"
+          num6="205" name6="dragon-ball-z-dublado"
         />
 
-        <SectionButtons 
-          num1="206" url1="" name1="O poder surpreendente de Goten"
-          num2="207" url2="" name2="O treinamento para voar"
-          num3="208" url3="" name3="Os guerreiros Z se reúnem novamente"
-          num4="209" url4="" name4="Tenha cuidado Grande Saiyaman"
-          num5="210" url5="" name5="O pequeno Trunks"
-          num6="211" url6="" name6="Goten fica nervoso"
+        <NewSectionButtons 
+          num1="206" name1="dragon-ball-z-dublado"
+          num2="207" name2="dragon-ball-z-dublado"
+          num3="208" name3="dragon-ball-z-dublado"
+          num4="209" name4="dragon-ball-z-dublado"
+          num5="210" name5="dragon-ball-z-dublado"
+          num6="211" name6="dragon-ball-z-dublado"
         />
 
-        <SectionButtons 
-          num1="212" url1="" name1="O campeão do torneio infantil"
-          num2="213" url2="" name2="O que pretende fazer Mr. Satan"
-          num3="214" url3="" name3="As lutas das artes marciais"
-          num4="215" url4="" name4="O que está acontecendo Piccolo"
-          num5="216" url5="" name5="O mistério de Spopovitch"
-          num6="217" url6="" name6="Gohan se enfurece"
+        <NewSectionButtons 
+          num1="212" name1="dragon-ball-z-dublado"
+          num2="213" name2="dragon-ball-z-dublado"
+          num3="214" name3="dragon-ball-z-dublado"
+          num4="215" name4="dragon-ball-z-dublado"
+          num5="216" name5="dragon-ball-z-dublado"
+          num6="217" name6="dragon-ball-z-dublado"
         />
 
-        <SectionButtons 
-          num1="218" url1="" name1="Gohan é o Grande Saiyaman"
-          num2="219" url2="" name2="O poder de Gohan é roubado"
-          num3="220" url3="" name3="O perverso mago Babidi"
-          num4="221" url4="" name4="Um diabo do inferno"
-          num5="222" url5="" name5="Vegeta fica aborrecido"
-          num6="223" url6="" name6="O poder máximo de Goku"
+        <NewSectionButtons 
+          num1="218" name1="dragon-ball-z-dublado"
+          num2="219" name2="dragon-ball-z-dublado"
+          num3="220" name3="dragon-ball-z-dublado"
+          num4="221" name4="dragon-ball-z-dublado"
+          num5="222" name5="dragon-ball-z-dublado"
+          num6="223" name6="dragon-ball-z-dublado"
         />
 
-        <SectionButtons 
-          num1="224" url1="" name1="O Mr. Satan contra 3 super guerreiros"
-          num2="225" url2="" name2="Uma luta muito difícil contra a Nº 18"
-          num3="226" url3="" name3="É a sua vez Gohan"
-          num4="227" url4="" name4="A ideia de Dabura"
-          num5="228" url5="" name5="Vegeta o rei da destruição"
-          num6="229" url6="" name6="Goku contra Vegeta"
+        <NewSectionButtons 
+          num1="224" name1="dragon-ball-z-dublado"
+          num2="225" name2="dragon-ball-z-dublado"
+          num3="226" name3="dragon-ball-z-dublado"
+          num4="227" name4="dragon-ball-z-dublado"
+          num5="228" name5="dragon-ball-z-dublado"
+          num6="229" name6="dragon-ball-z-dublado"
         />
 
-        <SectionButtons 
-          num1="230" url1="" name1="Espere Babidi"
-          num2="231" url2="" name2="Majin Boo aparece"
-          num3="232" url3="" name3="Não deixaremos que Majin Boo desperte"
-          num4="233" url4="" name4="O lamento do Supremo Sr. Kaio"
-          num5="234" url5="" name5="Gohan corre perigo"
-          num6="235" url6="" name6="Vou te comer"
+        <NewSectionButtons 
+          num1="230" name1="dragon-ball-z-dublado"
+          num2="231" name2="dragon-ball-z-dublado"
+          num3="232" name3="dragon-ball-z-dublado"
+          num4="233" name4="dragon-ball-z-dublado"
+          num5="234" name5="dragon-ball-z-dublado"
+          num6="235" name6="dragon-ball-z-dublado"
         />
 
-        <SectionButtons 
-          num1="236" url1="" name1="Eu acabarei com Majin Boo"
-          num2="237" url2="" name2="Vegeta luta por seus entes queridos"
-          num3="238" url3="" name3="Majin Boo continua vivo"
-          num4="239" url4="" name4="Procurem as Esferas do Dragão"
-          num5="240" url5="" name5="A técnica especial para Goten e Trunks"
-          num6="241" url6="" name6="Goten e Trunks! A esperança de salvar o mundo"
+        <NewSectionButtons 
+          num1="236" name1="dragon-ball-z-dublado"
+          num2="237" name2="dragon-ball-z-dublado"
+          num3="238" name3="dragon-ball-z-dublado"
+          num4="239" name4="dragon-ball-z-dublado"
+          num5="240" name5="dragon-ball-z-dublado"
+          num6="241" name6="dragon-ball-z-dublado"
         />
 
-        <SectionButtons 
-          num1="242" url1="" name1="Gohan revive"
-          num2="243" url2="" name2="A lendária Espada Z"
-          num3="244" url3="" name3="Detenha-se Majin Boo"
-          num4="245" url4="" name4="O Super Saiyajin 3"
-          num5="246" url5="" name5="Até nunca Babidi"
-          num6="247" url6="" name6="A técnica da Fusão"
+        <NewSectionButtons 
+          num1="242" name1="dragon-ball-z-dublado"
+          num2="243" name2="dragon-ball-z-dublado"
+          num3="244" name3="dragon-ball-z-dublado"
+          num4="245" name4="dragon-ball-z-dublado"
+          num5="246" name5="dragon-ball-z-dublado"
+          num6="247" name6="dragon-ball-z-dublado"
         />
 
-        <SectionButtons 
-          num1="248" url1="" name1="Goku volta para o outro mundo"
-          num2="249" url2="" name2="Onde está Gohan"
-          num3="250" url3="" name3="A Espada Z se quebra"
-          num4="251" url4="" name4="Nasce um novo guerreiro! Seu nome é Gotenks"
-          num5="252" url5="" name5="Mr. Satan salva a terra"
-          num6="253" url6="" name6="Majin Boo deixa de matar pessoas"
+        <NewSectionButtons 
+          num1="248" name1="dragon-ball-z-dublado"
+          num2="249" name2="dragon-ball-z-dublado"
+          num3="250" name3="dragon-ball-z-dublado"
+          num4="251" name4="dragon-ball-z-dublado"
+          num5="252" name5="dragon-ball-z-dublado"
+          num6="253" name6="dragon-ball-z-dublado"
         />
 
-        <SectionButtons 
-          num1="254" url1="" name1="A fúria de Majin Boo"
-          num2="255" url2="" name2="Qual dos dois vencerá"
-          num3="256" url3="" name3="O fim da raça humana"
-          num4="257" url4="" name4="O treinamento é um perfeito sucesso"
-          num5="258" url5="" name5="O poder de Super Gotenks"
-          num6="259" url6="" name6="A técnica dos fantasmas"
+        <NewSectionButtons 
+          num1="254" name1="dragon-ball-z-dublado"
+          num2="255" name2="dragon-ball-z-dublado"
+          num3="256" name3="dragon-ball-z-dublado"
+          num4="257" name4="dragon-ball-z-dublado"
+          num5="258" name5="dragon-ball-z-dublado"
+          num6="259" name6="dragon-ball-z-dublado"
         />
 
-        <SectionButtons 
-          num1="260" url1="" name1="Super Gotenks 3"
-          num2="261" url2="" name2="O Volleyball de Majin Boo"
-          num3="262" url3="" name3="Gohan volta para a terra"
-          num4="263" url4="" name4="O poder milagroso de Gohan"
-          num5="264" url5="" name5="A grande explosão de Majin Boo"
-          num6="265" url6="" name6="Gotenks é absorvido"
+        <NewSectionButtons 
+          num1="260" name1="dragon-ball-z-dublado"
+          num2="261" name2="dragon-ball-z-dublado"
+          num3="262" name3="dragon-ball-z-dublado"
+          num4="263" name4="dragon-ball-z-dublado"
+          num5="264" name5="dragon-ball-z-dublado"
+          num6="265" name6="dragon-ball-z-dublado"
         />
 
-        <SectionButtons 
-          num1="266" url1="" name1="Goku volta a terra"
-          num2="267" url2="" name2="Acontece outro milagre"
-          num3="268" url3="" name3="O orgulho de Vegeta e a fúria de Goku"
-          num4="269" url4="" name4="O poder do grande Vegetto"
-          num5="270" url5="" name5="Majin Boo é perturbado"
-          num6="271" url6="" name6="Transforme-se em um doce"
+        <NewSectionButtons 
+          num1="266" name1="dragon-ball-z-dublado"
+          num2="267" name2="dragon-ball-z-dublado"
+          num3="268" name3="dragon-ball-z-dublado"
+          num4="269" name4="dragon-ball-z-dublado"
+          num5="270" name5="dragon-ball-z-dublado"
+          num6="271" name6="dragon-ball-z-dublado"
         />
 
-        <SectionButtons 
-          num1="272" url1="" name1="Vegetto é absorvido"
-          num2="273" url2="" name2="O que tem no estômago de Majin Boo"
-          num3="274" url3="" name3="A luta entre pais e filhos"
-          num4="275" url4="" name4="O segredo de Majin Boo"
-          num5="276" url5="" name5="Onde fica a saída"
-          num6="277" url6="" name6="A terra desaparece"
+        <NewSectionButtons 
+          num1="272" name1="dragon-ball-z-dublado"
+          num2="273" name2="dragon-ball-z-dublado"
+          num3="274" name3="dragon-ball-z-dublado"
+          num4="275" name4="dragon-ball-z-dublado"
+          num5="276" name5="dragon-ball-z-dublado"
+          num6="277" name6="dragon-ball-z-dublado"
         />
 
-        <SectionButtons 
-          num1="278" url1="" name1="O ataque de Majin Boo!"
-          num2="279" url2="" name2="Uma luta para o futuro"
-          num3="280" url3="" name3="Goku!! Você é o melhor"
-          num4="281" url4="" name4="Resista Vegeta"
-          num5="282" url5="" name5="Não machuque Satan"
-          num6="283" url6="" name6="A estratégia de Vegeta"
+        <NewSectionButtons 
+          num1="278" name1="dragon-ball-z-dublado"
+          num2="279" name2="dragon-ball-z-dublado"
+          num3="280" name3="dragon-ball-z-dublado"
+          num4="281" name4="dragon-ball-z-dublado"
+          num5="282" name5="dragon-ball-z-dublado"
+          num6="283" name6="dragon-ball-z-dublado"
         />
 
-        <SectionButtons 
-          num1="284" url1="" name1="A última esperança"
-          num2="285" url2="" name2="Uma Genki-Dama feita por todos"
-          num3="286" url3="" name3="Goku é o homem mais forte do universo"
-          num4="287" url4="" name4="Majin Boo luta a favor da justiça"
-          num5="288" url5="" name5="Goku! Você chegou tarde"
-          num6="289" url6="" name6="Meu nome é Pan"
+        <NewSectionButtons 
+          num1="284" name1="dragon-ball-z-dublado"
+          num2="285" name2="dragon-ball-z-dublado"
+          num3="286" name3="dragon-ball-z-dublado"
+          num4="287" name4="dragon-ball-z-dublado"
+          num5="288" name5="dragon-ball-z-dublado"
+          num6="289" name6="dragon-ball-z-dublado"
         />
 
         <section className="episodeButtons">
-          <SectionLinks num="290" url="" name="Eu sou Oob" />
-          <SectionLinks num="291" url="" name="O sonho de Goku" />
+          <NewSectionLinks num="290" name="dragon-ball-z-dublado" />
+          <NewSectionLinks num="291" name="dragon-ball-z-dublado" />
         </section>
       </section>
     </div>
--- Client/src/components/DragonBallZ/SeasonSix/index.tsx
+++ Client/src/components/DragonBallZ/SeasonSix/index.tsx
@@ -1,98 +1,98 @@
-import SectionButtons from "../../SectionButtons";
+import NewSectionButtons from "../../NewSectionButtons";
 
 export default function SeasonSixDbz(){
     return(
     <div className="containerEps">
         <h1 className="text-center text-4xl font-bold">Saga Cell</h1>
         <section className=" flex flex-col items-center">
-            <SectionButtons 
-            num1="140" url1="1gf8S0FxoX-TS4s6TqGb8WoYfTpcFQQ-W" name1="Um ovo misterioso encontrado na máquina do tempo"
-            num2="141" url2="1PlOnUiqQkGPnA5F69hcAfYHepFnFzzyR" name2="O nascimento de um novo guerreiro"
-            num3="142" url3="1Q4pDzeOQxcrwaNGtZq3or8pgyD0hobZR" name3="O monstro que tem o KI de Goku"
-            num4="143" url4="1Pwdt9Np-YflAWE5GJTeThldOcZOqGeRN" name4="O androide Cell"
-            num5="144" url5="1NVOP5FlsHPNAjxmOMGqrmVFX7rokSAKZ" name5="Cell escapa das mãos de Piccolo"
-            num6="145" url6="1odxaRLzkrKVunM-XJriicn0uDk1EVVof" name6="O esconderijo secreto do laboratório do Dr. Gero"
+            <NewSectionButtons 
+            num1="140" name1="dragon-ball-z-dublado"
+            num2="141" name2="dragon-ball-z-dublado"
+            num3="142" name3="dragon-ball-z-dublado"
+            num4="143" name4="dragon-ball-z-dublado"
+            num5="144" name5="dragon-ball-z-dublado"
+            num6="145" name6="dragon-ball-z-dublado"
             />
 
-            <SectionButtons 
-            num1="146" url1="1yUyjnA1ACWdkUH2fV1MNYrMBDiAVPJD6" name1="Goku se recupera"
-            num2="147" url2="13AzZCvKdrRoXCv70nZtb5WIf8MqM5WFO" name2="Vamos à sala do templo"
-            num3="148" url3="1vyP4aJl2IHARQgj9y_yoG161nOvJqyVR" name3="Piccolo contra o androide Nº 17"
-            num4="149" url4="1lOpSMue3UKipz6UM6iOkar1cCsTHIR1J" name4="Cell está perto de aperfeiçoar seu corpo"
-            num5="150" url5="1UUOy51BiOANuopXgd3Cy2kX-h1nJ0PBr" name5="Piccolo luta com todas as androide forças"
-            num6="151" url6="1tcGZz0P5gNmrplcMQJgkXQX6HNZX0n-A" name6="A última esperança! O androide Nº 16"
+            <NewSectionButtons 
+            num1="146" name1="dragon-ball-z-dublado"
+            num2="147" name2="dragon-ball-z-dublado"
+            num3="148" name3="dragon-ball-z-dublado"
+            num4="149" name4="dragon-ball-z-dublado"
+            num5="150" name5="dragon-ball-z-dublado"
+            num6="151" name6="dragon-ball-z-dublado"
             />
 
-            <SectionButtons 
-            num1="152" url1="1Jqf7P_7Es8Go8FEioIpet3EyqQ-JCnXA" name1="Cell absorve o androide Nº 17"
-            num2="153" url2="1Nk5av64hcvAzY0AwOFYeRimOH38LQjRC" name2="O novo desafio de Goku"
-            num3="154" url3="1DFC__euvvpLwZzRsLxrygwtTi5y0ig0m" name3="Apareça Nº 18"
-            num4="155" url4="1OYIIlubHpwA78LoZPB-T0pqG8yEzQLpX" name4="O super poder de Vegeta"
-            num5="156" url5="1Kgs3swdfS2UjalmoR045ISzbyv297496" name5="Eu sou o Super vegeta"
-            num6="157" url6="1ty6i9EQQuIyVYjYRss1hYRch7itkNade" name6="Um guerreiro orgulhoso"
+            <NewSectionButtons 
+            num1="152" name1="dragon-ball-z-dublado"
+            num2="153" name2="dragon-ball-z-dublado"
+            num3="154" name3="dragon-ball-z-dublado"
+            num4="155" name4="dragon-ball-z-dublado"
+            num5="156" name5="dragon-ball-z-dublado"
+            num6="157" name6="dragon-ball-z-dublado"
             />
 
-            <SectionButtons 
-            num1="158" url1="1j8vusRSzwvhyP_g3YWIVV9W51V251NLi" name1="A decisão de Kuririn"
-            num2="159" url2="1ZMDToLIyKIz3bUhJVmF-tHIfVAbDn3fS" name2="Cell quer aperfeiçoar o seu corpo"
-            num3="160" url3="15z_ps4aWNaegf66uaoAbgFyS_BbxZ63m" name3="Cell o deus da destruição"
-            num4="161" url4="1X5SzbwI0kki--A0t2pkWqVGj5wRiNKkZ" name4="Super Vegeta corre perigo"
-            num5="162" url5="1ddwuu6dQZfvWZaHZYYfSlhV9fVNapgse" name5="Trunks fica furioso"
-            num6="163" url6="1ITPi_I-4SaqjiPEYv_QswC7lWc7iCXoS" name6="Salvem meu pai"
+            <NewSectionButtons 
+            num1="158" name1="dragon-ball-z-dublado"
+            num2="159" name2="dragon-ball-z-dublado"
+            num3="160" name3="dragon-ball-z-dublado"
+            num4="161" name4="dragon-ball-z-dublado"
+            num5="162" name5="dragon-ball-z-dublado"
+            num6="163" name6="dragon-ball-z-dublado"
             />
 
-            <SectionButtons 
-            num1="164" url1="1tvma0ZcvdEhzmI00bYh7m4vShofb9gh3" name1="O desespero de viver num futuro infernal"
-            num2="165" url2="1F9dpFY0c1ZzbYwWgTeU4kgiFg1DtqV8f" name2="O ponto fraco de Super Trunks"
-            num3="166" url3="17xoGXLoxfE7-SP34yAgagRP9BlCYbKVi" name3="O mistério do novo torneio de artes marciais"
-            num4="167" url4="1TPe0x0-BeGjSOsrnUpGovIhReD5oysdT" name4="O anúncio do torneio de Cell"
-            num5="168" url5="1sGFfdLQQDJHyF0dqXgbLRjUk3NIFQR_K" name5="Os novos poderes de Goku e Gohan"
-            num6="169" url6="1nNGUyn0pu8D8uuN_b84d45HEiAKNQVdu" name6="A tranquilidade de Goku"
+            <NewSectionButtons 
+            num1="164" name1="dragon-ball-z-dublado"
+            num2="165" name2="dragon-ball-z-dublado"
+            num3="166" name3="dragon-ball-z-dublado"
+            num4="167" name4="dragon-ball-z-dublado"
+            num5="168" name5="dragon-ball-z-dublado"
+            num6="169" name6="dragon-ball-z-dublado"
             />
 
-            <SectionButtons 
-            num1="170" url1="" name1="O descanso dos guerreiros"
-            num2="171" url2="" name2="Quando Gohan era bebê"
-            num3="172" url3="" name3="Vamos procurar um novo Kami-Sama"
-            num4="173" url4="" name4="As Esferas do Dragão voltam a existir"
-            num5="174" url5="" name5="O sofrimento de Goku"
-            num6="175" url6="" name6="Começa o torneio de Cell"
+            <NewSectionButtons 
+            num1="170" name1="dragon-ball-z-dublado"
+            num2="171" name2="dragon-ball-z-dublado"
+            num3="172" name3="dragon-ball-z-dublado"
+            num4="173" name4="dragon-ball-z-dublado"
+            num5="174" name5="dragon-ball-z-dublado"
+            num6="175" name6="dragon-ball-z-dublado"
             />
 
-            <SectionButtons 
-            num1="176" url1="" name1="O exército do Sr. Satan"
-            num2="177" url2="" name2="Goku contra Cell"
-            num3="178" url3="" name3="O super Kamehameha de Cell"
-            num4="179" url4="" name4="Cell muda as regras do jogo"
-            num5="180" url5="" name5="Goku se rende"
-            num6="181" url6="" name6="Gohan… O jovem mais forte"
+            <NewSectionButtons 
+            num1="176" name1="dragon-ball-z-dublado"
+            num2="177" name2="dragon-ball-z-dublado"
+            num3="178" name3="dragon-ball-z-dublado"
+            num4="179" name4="dragon-ball-z-dublado"
+            num5="180" name5="dragon-ball-z-dublado"
+            num6="181" name6="dragon-ball-z-dublado"
             />
 
-            <SectionButtons 
-            num1="182" url1="" name1="Gohan libera todo o seu poder"
-            num2="183" url2="" name2="O ataque dos Cell juniores"
-            num3="184" url3="" name3="Gohan fica nervoso"
-            num4="185" url4="" name4="A destruição dos Cells juniores"
-            num5="186" url5="" name5="Cell é golpeado"
-            num6="187" url6="" name6="A transformação de Cell"
+            <NewSectionButtons 
+            num1="182" name1="dragon-ball-z-dublado"
+            num2="183" name2="dragon-ball-z-dublado"
+            num3="184" name3="dragon-ball-z-dublado"
+            num4="185" name4="dragon-ball-z-dublado"
+            num5="186" name5="dragon-ball-z-dublado"
+            num6="187" name6="dragon-ball-z-dublado"
             />
 
-            <SectionButtons 
-            num1="188" url1="" name1="Adeus a todos"
-            num2="189" url2="" name2="Um terrível pesadelo em pleno dia"
-            num3="190" url3="" name3="Uma mensagem para Gohan"
-            num4="191" url4="" name4="A luta terminou obrigado Goku"
-            num5="192" url5="" name5="Goku vai treinar no além"
-            num6="193" url6="" name6="Pai jamais vou me dar por vencido"
+            <NewSectionButtons 
+            num1="188" name1="dragon-ball-z-dublado"
+            num2="189" name2="dragon-ball-z-dublado"
+            num3="190" name3="dragon-ball-z-dublado"
+            num4="191" name4="dragon-ball-z-dublado"
+            num5="192" name5="dragon-ball-z-dublado"
+            num6="193" name6="dragon-ball-z-dublado"
             />
             
-            <SectionButtons 
-            num1="194" url1="" name1="Outro final feliz"
-            num2="195" url2="" name2="O lutador mais forte do outro mundo"
-            num3="196" url3="" name3="O encontro dos lutadores mais valentes"
-            num4="197" url4="" name4="O torneio fica emocionante"
-            num5="198" url5="" name5="Quem vai ganhar!? Goku ou Paikuhan"
-            num6="199" url6="" name6="Goku faz um Kamehameha com toda rapidez"
+            <NewSectionButtons 
+            num1="194" name1="dragon-ball-z-dublado"
+            num2="195" name2="dragon-ball-z-dublado"
+            num3="196" name3="dragon-ball-z-dublado"
+            num4="197" name4="dragon-ball-z-dublado"
+            num5="198" name5="dragon-ball-z-dublado"
+            num6="199" name6="dragon-ball-z-dublado"
             />
         </section>
     </div>
