e16c56bf52b790a025b0deee13c6e05332243832
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-04-04 09:43:45 -0300

    :lipstick: feat: Styles update on links

--- Client/src/components/NewVideoContent/index.tsx
+++ Client/src/components/NewVideoContent/index.tsx
@@ -1,4 +1,3 @@
-import { Link } from "react-router-dom";
 import Cover from "../../images/DB_case.jpg";
 
 interface InfoVideo {
@@ -9,8 +8,19 @@ interface InfoVideo {
 export default function NewVideoContent({ number, url }: InfoVideo) {
   return (
     <>
-      <h1 className="epName">Episodio: {url}</h1>
-      <video controls className="videoContent" poster={Cover}>
+      <h1 className="epName">
+        Episodio: <br /> {url}
+      </h1>
+      <video
+        id="my-video_html5_api"
+        controls
+        poster={Cover}
+        className="videoContent"
+        preload="auto"
+        data-setup="{}"
+        data-source="0"
+        data-video-src={`https://animefire.net/video/${number}/${url}?tempsubs=0&amp;1680610616`}
+      >
         <source
           src={`https://lightspeedst.net/s2/mp4/${number}/sd/${url}.mp4`}
           type="video/mp4"
@@ -19,7 +29,7 @@ export default function NewVideoContent({ number, url }: InfoVideo) {
       <div className="containerHelp">
         <h3 className="helpText">Algum problema com o vídeo?</h3>
         <a
-          href={`https://lightspeedst.net/s2/mp4/${number}/sd/${url}.mp4`}
+          href={`https://ns565646.ip-51-79-16.net/Uploads/Animes/D/${number}/${url}.MP4`}
           target="_blank"
           className="btnHelp"
         >
