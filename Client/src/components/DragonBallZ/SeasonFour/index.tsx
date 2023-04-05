624dd82344e65d122813766ef51bebefeb44af37
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-04-05 10:47:18 -0300

    :recycle: refactor: Finishing dbz season three and four

--- Client/src/components/DragonBallZ/SeasonFour/index.tsx
+++ Client/src/components/DragonBallZ/SeasonFour/index.tsx
@@ -8,18 +8,18 @@ export default function SeasonFourBbz() {
       <h1 className="text-center text-4xl font-bold">Galik Jr.</h1>
       <section className="mt-8 flex flex-col items-center">
         <NewSectionButtons 
-          num1="108" name1="dragon-ball-z-dublado"
-          num2="109" name2="dragon-ball-z-dublado"
-          num3="110" name3="dragon-ball-z-dublado"
-          num4="111" name4="dragon-ball-z-dublado"
-          num5="112" name5="dragon-ball-z-dublado"
-          num6="113" name6="dragon-ball-z-dublado"
+          num1="108" name1="df6c385ab0a46533"
+          num2="109" name2="d7f7eb5f25fe944e"
+          num3="110" name3="248be7304dc9ebff"
+          num4="111" name4="04ef3f5402caf42d"
+          num5="112" name5="421cb227869f13fa"
+          num6="113" name6="d785e6220b0289a6"
         />
         <section className="episodeButtons">
-          <NewSectionLinks num="114" name="dragon-ball-z-dublado" />
-          <NewSectionLinks num="115" name="dragon-ball-z-dublado" />
-          <NewSectionLinks num="116" name="dragon-ball-z-dublado" />
-          <NewSectionLinks num="117" name="dragon-ball-z-dublado" />
+          <NewSectionLinks num="114" name="c15ba0147ae8c4f0" />
+          <NewSectionLinks num="115" name="589f55fa62f7a33d" />
+          <NewSectionLinks num="116" name="0af13f9e059d6e45" />
+          <NewSectionLinks num="117" name="65469aed52042690" />
         </section>
       </section>
     </div>
--- Client/src/components/DragonBallZ/SeasonThree/index.tsx
+++ Client/src/components/DragonBallZ/SeasonThree/index.tsx
@@ -9,16 +9,16 @@ export default function SeasonThreeBbz() {
       <h1 className="text-center text-4xl font-bold">Eu sou o filho de Goku</h1>
       <section className="mt-8 flex flex-col items-center">
         <NewSectionButtons 
-          num1="100" name1="dragon-ball-z-dublado"
-          num2="101" name2="dragon-ball-z-dublado"
-          num3="102" name3="dragon-ball-z-dublado"
-          num4="103" name4="dragon-ball-z-dublado"
-          num5="104" name5="dragon-ball-z-dublado"
-          num6="105" name6="dragon-ball-z-dublado"
+          num1="100" name1="06be55e2d189460c"
+          num2="101" name2="bf318523fe90cb94"
+          num3="102" name3="30f3ea7c91643b38"
+          num4="103" name4="b28b5ddaed08dcc2"
+          num5="104" name5="2d5ff5b16fad89f4"
+          num6="105" name6="29c3b8d016eaa964"
         />
         <section className="episodeButtons">
-          <NewSectionLinks num="106" name="dragon-ball-z-dublado" />
-          <NewSectionLinks num="107" name="dragon-ball-z-dublado" />
+          <NewSectionLinks num="106" name="c48cd6288daba104" />
+          <NewSectionLinks num="107" name="b842df51dd154912" />
         </section>
       </section>
     </div>
