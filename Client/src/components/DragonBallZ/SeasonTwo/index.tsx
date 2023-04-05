8d3d539bfa9c5f42135b06785ce9b29442cdf87c
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-04-05 10:46:47 -0300

    :recycle: refactor: Finishing dbz season two

--- Client/src/components/DragonBallZ/SeasonTwo/index.tsx
+++ Client/src/components/DragonBallZ/SeasonTwo/index.tsx
@@ -8,98 +8,98 @@ export default function SeasonTwoBbz() {
       <h1 className="text-center text-4xl font-bold">Saga Freeza</h1>
       <section className="mt-8 flex flex-col items-center">
         <NewSectionButtons 
-          num1="36" name1="dragon-ball-z-dublado"
-          num2="37" name2="dragon-ball-z-dublado"
-          num3="38" name3="dragon-ball-z-dublado"
-          num4="39" name4="dragon-ball-z-dublado"
-          num5="40" name5="dragon-ball-z-dublado"
-          num6="41" name6="dragon-ball-z-dublado"
+          num1="36" name1="3dab48bf08097a14"
+          num2="37" name2="1f894011dcd852c4"
+          num3="38" name3="cd8b3a25f0086e2d"
+          num4="39" name4="13cd5cbaf761e3c0"
+          num5="40" name5="8c07270db0d614cb"
+          num6="41" name6="8b27e6b60ac325bf"
         />
         <NewSectionButtons 
-          num1="42" name1="dragon-ball-z-dublado"
-          num2="43" name2="dragon-ball-z-dublado"
-          num3="44" name3="dragon-ball-z-dublado"
-          num4="45" name4="dragon-ball-z-dublado"
-          num5="46" name5="dragon-ball-z-dublado"
-          num6="47" name6="dragon-ball-z-dublado"
+          num1="42" name1="c2ee006e03624735"
+          num2="43" name2="ce6ac03964a74991"
+          num3="44" name3="39cda1a46be6537d"
+          num4="45" name4="9f725e6d8b9b1d4d"
+          num5="46" name5="eb33b64542dbdb8b"
+          num6="47" name6="6458f22c841aca5c"
         />
         <NewSectionButtons 
-          num1="48" name1="dragon-ball-z-dublado"
-          num2="49" name2="dragon-ball-z-dublado"
-          num3="50" name3="dragon-ball-z-dublado"
-          num4="51" name4="dragon-ball-z-dublado"
-          num5="52" name5="dragon-ball-z-dublado"
-          num6="53" name6="dragon-ball-z-dublado"
-        />
-
-        <NewSectionButtons 
-          num1="54" name1="dragon-ball-z-dublado"
-          num2="55" name2="dragon-ball-z-dublado"
-          num3="56" name3="dragon-ball-z-dublado"
-          num4="57" name4="dragon-ball-z-dublado"
-          num5="58" name5="dragon-ball-z-dublado"
-          num6="59" name6="dragon-ball-z-dublado"
+          num1="48" name1="2585c958a8e842ca"
+          num2="49" name2="2a3db84471ee0f9f"
+          num3="50" name3="f3959d525111f99c"
+          num4="51" name4="611cd5b69baba2d9"
+          num5="52" name5="9265799d17053cc7"
+          num6="53" name6="7d358f5665cfc35f"
         />
 
         <NewSectionButtons 
-          num1="60" name1="dragon-ball-z-dublado"
-          num2="61" name2="dragon-ball-z-dublado"
-          num3="62" name3="dragon-ball-z-dublado"
-          num4="63" name4="dragon-ball-z-dublado"
-          num5="64" name5="dragon-ball-z-dublado"
-          num6="65" name6="dragon-ball-z-dublado"
+          num1="54" name1="e95f12c20019a5d5"
+          num2="55" name2="3a4b8af4133a6b9b"
+          num3="56" name3="53280813c1d8fd30"
+          num4="57" name4="e8d902874ec7f235"
+          num5="58" name5="99324dd28fff98bc"
+          num6="59" name6="1a69b7e42293ea71"
         />
 
         <NewSectionButtons 
-          num1="66" name1="dragon-ball-z-dublado"
-          num2="67" name2="dragon-ball-z-dublado"
-          num3="68" name3="dragon-ball-z-dublado"
-          num4="69" name4="dragon-ball-z-dublado"
-          num5="70" name5="dragon-ball-z-dublado"
-          num6="71" name6="dragon-ball-z-dublado"
+          num1="60" name1="8514706cfb716ef4"
+          num2="61" name2="e020db0556534969"
+          num3="62" name3="d6a8f646036eff4d"
+          num4="63" name4="758dfed60a56e11b"
+          num5="64" name5="175ea0fd6fcd4a84"
+          num6="65" name6="9641408199c323d9"
         />
 
         <NewSectionButtons 
-          num1="72" name1="dragon-ball-z-dublado"
-          num2="73" name2="dragon-ball-z-dublado"
-          num3="74" name3="dragon-ball-z-dublado"
-          num4="75" name4="dragon-ball-z-dublado"
-          num5="76" name5="dragon-ball-z-dublado"
-          num6="77" name6="dragon-ball-z-dublado"
+          num1="66" name1="2226576349ae920b"
+          num2="67" name2="ba73f90a88cae729"
+          num3="68" name3="0e3bc3b7a84ff9a4"
+          num4="69" name4="48017d8956e5171c"
+          num5="70" name5="0cf40571140baa09"
+          num6="71" name6="d3fb6ff24275c56e"
         />
 
         <NewSectionButtons 
-          num1="78" name1="dragon-ball-z-dublado"
-          num2="79" name2="dragon-ball-z-dublado"
-          num3="80" name3="dragon-ball-z-dublado"
-          num4="81" name4="dragon-ball-z-dublado"
-          num5="82" name5="dragon-ball-z-dublado"
-          num6="83" name6="dragon-ball-z-dublado"
+          num1="72" name1="5b5bd2567d0a6629"
+          num2="73" name2="4d35e9816683e410"
+          num3="74" name3="33ca3efd80f2b530"
+          num4="75" name4="252629e289b38988"
+          num5="76" name5="2c46e347ace07dee"
+          num6="77" name6="fb242459f4d2d489"
         />
 
         <NewSectionButtons 
-          num1="84" name1="dragon-ball-z-dublado"
-          num2="85" name2="dragon-ball-z-dublado"
-          num3="86" name3="dragon-ball-z-dublado"
-          num4="87" name4="dragon-ball-z-dublado"
-          num5="88" name5="dragon-ball-z-dublado"
-          num6="89" name6="dragon-ball-z-dublado"
+          num1="78" name1="b108e1cf80e095fc"
+          num2="79" name2="ef7d6e24cc644d99"
+          num3="80" name3="29888096635b43da"
+          num4="81" name4="cfd791a5acec2f6b"
+          num5="82" name5="3c2bb987f1b67667"
+          num6="83" name6="1b8dff198288656c"
         />
 
         <NewSectionButtons 
-          num1="90" name1="dragon-ball-z-dublado"
-          num2="91" name2="dragon-ball-z-dublado"
-          num3="92" name3="dragon-ball-z-dublado"
-          num4="93" name4="dragon-ball-z-dublado"
-          num5="94" name5="dragon-ball-z-dublado"
-          num6="95" name6="dragon-ball-z-dublado"
+          num1="84" name1="caee0e39adf34f20"
+          num2="85" name2="480126b74a8fecbf"
+          num3="86" name3="9f62d487cb8484e8"
+          num4="87" name4="1fe9f9a036ccef7d"
+          num5="88" name5="6400193cc1d174b4"
+          num6="89" name6="ba25841d1273e73a"
+        />
+
+        <NewSectionButtons 
+          num1="90" name1="5eb76aa925c28846"
+          num2="91" name2="bfd759fd3b7456a1"
+          num3="92" name3="980ec23c03a3ec60"
+          num4="93" name4="a0a41ef9b28ee188"
+          num5="94" name5="4f3f48ca6d8cadb7"
+          num6="95" name6="2da4e637b3a65036"
         />
 
         <section className="episodeButtons">
-          <NewSectionLinks num="96" name="dragon-ball-z-dublado" />
-          <NewSectionLinks num="97" name="dragon-ball-z-dublado" />
-          <NewSectionLinks num="98" name="dragon-ball-z-dublado" />
-          <NewSectionLinks num="99" name="dragon-ball-z-dublado" />
+          <NewSectionLinks num="96" name="2dc6597c2dd24fd8" />
+          <NewSectionLinks num="97" name="8618b601c6471b95" />
+          <NewSectionLinks num="98" name="e515779f1e0d9dde" />
+          <NewSectionLinks num="99" name="1206c6a687c75c58" />
         </section>
       </section>
     </div>
