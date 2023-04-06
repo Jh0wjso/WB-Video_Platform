89a4bfaa329417808ea8de821753fe4ac3948d7a
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-04-06 10:36:30 -0300

    :recycle: refactor: Update on dbz season final seasons

--- Client/src/components/DragonBallZ/SeasonFinal/index.tsx
+++ Client/src/components/DragonBallZ/SeasonFinal/index.tsx
@@ -8,143 +8,143 @@ export default function SeasonFinalDbz() {
       <h1 className="text-center text-4xl font-bold">Saga Buu</h1>
       <section className="flex flex-col items-center">
         <NewSectionButtons 
-          num1="200" name1="dragon-ball-z-dublado"
-          num2="201" name2="dragon-ball-z-dublado"
-          num3="202" name3="dragon-ball-z-dublado"
-          num4="203" name4="dragon-ball-z-dublado"
-          num5="204" name5="dragon-ball-z-dublado"
-          num6="205" name6="dragon-ball-z-dublado"
+          num1="200" name1=""
+          num2="201" name2=""
+          num3="202" name3=""
+          num4="203" name4=""
+          num5="204" name5=""
+          num6="205" name6=""
         />
 
         <NewSectionButtons 
-          num1="206" name1="dragon-ball-z-dublado"
-          num2="207" name2="dragon-ball-z-dublado"
-          num3="208" name3="dragon-ball-z-dublado"
-          num4="209" name4="dragon-ball-z-dublado"
-          num5="210" name5="dragon-ball-z-dublado"
-          num6="211" name6="dragon-ball-z-dublado"
+          num1="206" name1=""
+          num2="207" name2=""
+          num3="208" name3=""
+          num4="209" name4=""
+          num5="210" name5=""
+          num6="211" name6=""
         />
 
         <NewSectionButtons 
-          num1="212" name1="dragon-ball-z-dublado"
-          num2="213" name2="dragon-ball-z-dublado"
-          num3="214" name3="dragon-ball-z-dublado"
-          num4="215" name4="dragon-ball-z-dublado"
-          num5="216" name5="dragon-ball-z-dublado"
-          num6="217" name6="dragon-ball-z-dublado"
+          num1="212" name1=""
+          num2="213" name2=""
+          num3="214" name3=""
+          num4="215" name4=""
+          num5="216" name5=""
+          num6="217" name6=""
         />
 
         <NewSectionButtons 
-          num1="218" name1="dragon-ball-z-dublado"
-          num2="219" name2="dragon-ball-z-dublado"
-          num3="220" name3="dragon-ball-z-dublado"
-          num4="221" name4="dragon-ball-z-dublado"
-          num5="222" name5="dragon-ball-z-dublado"
-          num6="223" name6="dragon-ball-z-dublado"
+          num1="218" name1=""
+          num2="219" name2=""
+          num3="220" name3=""
+          num4="221" name4=""
+          num5="222" name5=""
+          num6="223" name6=""
         />
 
         <NewSectionButtons 
-          num1="224" name1="dragon-ball-z-dublado"
-          num2="225" name2="dragon-ball-z-dublado"
-          num3="226" name3="dragon-ball-z-dublado"
-          num4="227" name4="dragon-ball-z-dublado"
-          num5="228" name5="dragon-ball-z-dublado"
-          num6="229" name6="dragon-ball-z-dublado"
+          num1="224" name1=""
+          num2="225" name2=""
+          num3="226" name3=""
+          num4="227" name4=""
+          num5="228" name5=""
+          num6="229" name6=""
         />
 
         <NewSectionButtons 
-          num1="230" name1="dragon-ball-z-dublado"
-          num2="231" name2="dragon-ball-z-dublado"
-          num3="232" name3="dragon-ball-z-dublado"
-          num4="233" name4="dragon-ball-z-dublado"
-          num5="234" name5="dragon-ball-z-dublado"
-          num6="235" name6="dragon-ball-z-dublado"
+          num1="230" name1=""
+          num2="231" name2=""
+          num3="232" name3=""
+          num4="233" name4=""
+          num5="234" name5=""
+          num6="235" name6=""
         />
 
         <NewSectionButtons 
-          num1="236" name1="dragon-ball-z-dublado"
-          num2="237" name2="dragon-ball-z-dublado"
-          num3="238" name3="dragon-ball-z-dublado"
-          num4="239" name4="dragon-ball-z-dublado"
-          num5="240" name5="dragon-ball-z-dublado"
-          num6="241" name6="dragon-ball-z-dublado"
+          num1="236" name1=""
+          num2="237" name2=""
+          num3="238" name3=""
+          num4="239" name4=""
+          num5="240" name5=""
+          num6="241" name6=""
         />
 
         <NewSectionButtons 
-          num1="242" name1="dragon-ball-z-dublado"
-          num2="243" name2="dragon-ball-z-dublado"
-          num3="244" name3="dragon-ball-z-dublado"
-          num4="245" name4="dragon-ball-z-dublado"
-          num5="246" name5="dragon-ball-z-dublado"
-          num6="247" name6="dragon-ball-z-dublado"
+          num1="242" name1=""
+          num2="243" name2=""
+          num3="244" name3=""
+          num4="245" name4=""
+          num5="246" name5=""
+          num6="247" name6=""
         />
 
         <NewSectionButtons 
-          num1="248" name1="dragon-ball-z-dublado"
-          num2="249" name2="dragon-ball-z-dublado"
-          num3="250" name3="dragon-ball-z-dublado"
-          num4="251" name4="dragon-ball-z-dublado"
-          num5="252" name5="dragon-ball-z-dublado"
-          num6="253" name6="dragon-ball-z-dublado"
+          num1="248" name1=""
+          num2="249" name2=""
+          num3="250" name3=""
+          num4="251" name4=""
+          num5="252" name5=""
+          num6="253" name6=""
         />
 
         <NewSectionButtons 
-          num1="254" name1="dragon-ball-z-dublado"
-          num2="255" name2="dragon-ball-z-dublado"
-          num3="256" name3="dragon-ball-z-dublado"
-          num4="257" name4="dragon-ball-z-dublado"
-          num5="258" name5="dragon-ball-z-dublado"
-          num6="259" name6="dragon-ball-z-dublado"
+          num1="254" name1=""
+          num2="255" name2=""
+          num3="256" name3=""
+          num4="257" name4=""
+          num5="258" name5=""
+          num6="259" name6=""
         />
 
         <NewSectionButtons 
-          num1="260" name1="dragon-ball-z-dublado"
-          num2="261" name2="dragon-ball-z-dublado"
-          num3="262" name3="dragon-ball-z-dublado"
-          num4="263" name4="dragon-ball-z-dublado"
-          num5="264" name5="dragon-ball-z-dublado"
-          num6="265" name6="dragon-ball-z-dublado"
+          num1="260" name1=""
+          num2="261" name2=""
+          num3="262" name3=""
+          num4="263" name4=""
+          num5="264" name5=""
+          num6="265" name6=""
         />
 
         <NewSectionButtons 
-          num1="266" name1="dragon-ball-z-dublado"
-          num2="267" name2="dragon-ball-z-dublado"
-          num3="268" name3="dragon-ball-z-dublado"
-          num4="269" name4="dragon-ball-z-dublado"
-          num5="270" name5="dragon-ball-z-dublado"
-          num6="271" name6="dragon-ball-z-dublado"
+          num1="266" name1=""
+          num2="267" name2=""
+          num3="268" name3=""
+          num4="269" name4=""
+          num5="270" name5=""
+          num6="271" name6=""
         />
 
         <NewSectionButtons 
-          num1="272" name1="dragon-ball-z-dublado"
-          num2="273" name2="dragon-ball-z-dublado"
-          num3="274" name3="dragon-ball-z-dublado"
-          num4="275" name4="dragon-ball-z-dublado"
-          num5="276" name5="dragon-ball-z-dublado"
-          num6="277" name6="dragon-ball-z-dublado"
+          num1="272" name1=""
+          num2="273" name2=""
+          num3="274" name3=""
+          num4="275" name4=""
+          num5="276" name5=""
+          num6="277" name6=""
         />
 
         <NewSectionButtons 
-          num1="278" name1="dragon-ball-z-dublado"
-          num2="279" name2="dragon-ball-z-dublado"
-          num3="280" name3="dragon-ball-z-dublado"
-          num4="281" name4="dragon-ball-z-dublado"
-          num5="282" name5="dragon-ball-z-dublado"
-          num6="283" name6="dragon-ball-z-dublado"
+          num1="278" name1=""
+          num2="279" name2=""
+          num3="280" name3=""
+          num4="281" name4=""
+          num5="282" name5=""
+          num6="283" name6=""
         />
 
         <NewSectionButtons 
-          num1="284" name1="dragon-ball-z-dublado"
-          num2="285" name2="dragon-ball-z-dublado"
-          num3="286" name3="dragon-ball-z-dublado"
-          num4="287" name4="dragon-ball-z-dublado"
-          num5="288" name5="dragon-ball-z-dublado"
-          num6="289" name6="dragon-ball-z-dublado"
+          num1="284" name1=""
+          num2="285" name2=""
+          num3="286" name3=""
+          num4="287" name4=""
+          num5="288" name5=""
+          num6="289" name6=""
         />
 
         <section className="episodeButtons">
-          <NewSectionLinks num="290" name="dragon-ball-z-dublado" />
-          <NewSectionLinks num="291" name="dragon-ball-z-dublado" />
+          <NewSectionLinks num="290" name="" />
+          <NewSectionLinks num="291" name="" />
         </section>
       </section>
     </div>
--- Client/src/components/DragonBallZ/SeasonSix/index.tsx
+++ Client/src/components/DragonBallZ/SeasonSix/index.tsx
@@ -6,93 +6,93 @@ export default function SeasonSixDbz(){
         <h1 className="text-center text-4xl font-bold">Saga Cell</h1>
         <section className=" flex flex-col items-center">
             <NewSectionButtons 
-            num1="140" name1="dragon-ball-z-dublado"
-            num2="141" name2="dragon-ball-z-dublado"
-            num3="142" name3="dragon-ball-z-dublado"
-            num4="143" name4="dragon-ball-z-dublado"
-            num5="144" name5="dragon-ball-z-dublado"
-            num6="145" name6="dragon-ball-z-dublado"
+            num1="140" name1=""
+            num2="141" name2=""
+            num3="142" name3=""
+            num4="143" name4=""
+            num5="144" name5=""
+            num6="145" name6=""
             />
 
             <NewSectionButtons 
-            num1="146" name1="dragon-ball-z-dublado"
-            num2="147" name2="dragon-ball-z-dublado"
-            num3="148" name3="dragon-ball-z-dublado"
-            num4="149" name4="dragon-ball-z-dublado"
-            num5="150" name5="dragon-ball-z-dublado"
-            num6="151" name6="dragon-ball-z-dublado"
+            num1="146" name1=""
+            num2="147" name2=""
+            num3="148" name3=""
+            num4="149" name4=""
+            num5="150" name5=""
+            num6="151" name6=""
             />
 
             <NewSectionButtons 
-            num1="152" name1="dragon-ball-z-dublado"
-            num2="153" name2="dragon-ball-z-dublado"
-            num3="154" name3="dragon-ball-z-dublado"
-            num4="155" name4="dragon-ball-z-dublado"
-            num5="156" name5="dragon-ball-z-dublado"
-            num6="157" name6="dragon-ball-z-dublado"
+            num1="152" name1=""
+            num2="153" name2=""
+            num3="154" name3=""
+            num4="155" name4=""
+            num5="156" name5=""
+            num6="157" name6=""
             />
 
             <NewSectionButtons 
-            num1="158" name1="dragon-ball-z-dublado"
-            num2="159" name2="dragon-ball-z-dublado"
-            num3="160" name3="dragon-ball-z-dublado"
-            num4="161" name4="dragon-ball-z-dublado"
-            num5="162" name5="dragon-ball-z-dublado"
-            num6="163" name6="dragon-ball-z-dublado"
+            num1="158" name1=""
+            num2="159" name2=""
+            num3="160" name3=""
+            num4="161" name4=""
+            num5="162" name5=""
+            num6="163" name6=""
             />
 
             <NewSectionButtons 
-            num1="164" name1="dragon-ball-z-dublado"
-            num2="165" name2="dragon-ball-z-dublado"
-            num3="166" name3="dragon-ball-z-dublado"
-            num4="167" name4="dragon-ball-z-dublado"
-            num5="168" name5="dragon-ball-z-dublado"
-            num6="169" name6="dragon-ball-z-dublado"
+            num1="164" name1=""
+            num2="165" name2=""
+            num3="166" name3=""
+            num4="167" name4=""
+            num5="168" name5=""
+            num6="169" name6=""
             />
 
             <NewSectionButtons 
-            num1="170" name1="dragon-ball-z-dublado"
-            num2="171" name2="dragon-ball-z-dublado"
-            num3="172" name3="dragon-ball-z-dublado"
-            num4="173" name4="dragon-ball-z-dublado"
-            num5="174" name5="dragon-ball-z-dublado"
-            num6="175" name6="dragon-ball-z-dublado"
+            num1="170" name1=""
+            num2="171" name2=""
+            num3="172" name3=""
+            num4="173" name4=""
+            num5="174" name5=""
+            num6="175" name6=""
             />
 
             <NewSectionButtons 
-            num1="176" name1="dragon-ball-z-dublado"
-            num2="177" name2="dragon-ball-z-dublado"
-            num3="178" name3="dragon-ball-z-dublado"
-            num4="179" name4="dragon-ball-z-dublado"
-            num5="180" name5="dragon-ball-z-dublado"
-            num6="181" name6="dragon-ball-z-dublado"
+            num1="176" name1=""
+            num2="177" name2=""
+            num3="178" name3=""
+            num4="179" name4=""
+            num5="180" name5=""
+            num6="181" name6=""
             />
 
             <NewSectionButtons 
-            num1="182" name1="dragon-ball-z-dublado"
-            num2="183" name2="dragon-ball-z-dublado"
-            num3="184" name3="dragon-ball-z-dublado"
-            num4="185" name4="dragon-ball-z-dublado"
-            num5="186" name5="dragon-ball-z-dublado"
-            num6="187" name6="dragon-ball-z-dublado"
+            num1="182" name1=""
+            num2="183" name2=""
+            num3="184" name3=""
+            num4="185" name4=""
+            num5="186" name5=""
+            num6="187" name6=""
             />
 
             <NewSectionButtons 
-            num1="188" name1="dragon-ball-z-dublado"
-            num2="189" name2="dragon-ball-z-dublado"
-            num3="190" name3="dragon-ball-z-dublado"
-            num4="191" name4="dragon-ball-z-dublado"
-            num5="192" name5="dragon-ball-z-dublado"
-            num6="193" name6="dragon-ball-z-dublado"
+            num1="188" name1=""
+            num2="189" name2=""
+            num3="190" name3=""
+            num4="191" name4=""
+            num5="192" name5=""
+            num6="193" name6=""
             />
             
             <NewSectionButtons 
-            num1="194" name1="dragon-ball-z-dublado"
-            num2="195" name2="dragon-ball-z-dublado"
-            num3="196" name3="dragon-ball-z-dublado"
-            num4="197" name4="dragon-ball-z-dublado"
-            num5="198" name5="dragon-ball-z-dublado"
-            num6="199" name6="dragon-ball-z-dublado"
+            num1="194" name1=""
+            num2="195" name2=""
+            num3="196" name3=""
+            num4="197" name4=""
+            num5="198" name5=""
+            num6="199" name6=""
             />
         </section>
     </div>
