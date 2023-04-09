3a016de6330f72b1f5f3b7da3c9c087cdbf2b9a1
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-04-08 23:40:29 -0300

    :recycle: refactor: Refactor on DBZ season final

--- Client/src/components/DragonBallZ/SeasonFinal/index.tsx
+++ Client/src/components/DragonBallZ/SeasonFinal/index.tsx
@@ -8,143 +8,143 @@ export default function SeasonFinalDbz() {
       <h1 className="text-center text-4xl font-bold">Saga Buu</h1>
       <section className="flex flex-col items-center">
         <NewSectionButtons 
-          num1="200" name1=""
-          num2="201" name2=""
-          num3="202" name3=""
-          num4="203" name4=""
-          num5="204" name5=""
-          num6="205" name6=""
+          num1="200" name1="a994a082908157f1"
+          num2="201" name2="78532929cb4a71ab"
+          num3="202" name3="85de6b51002ed4d1"
+          num4="203" name4="89b61bcb58a39843"
+          num5="204" name5="293ac361707487a6"
+          num6="205" name6="cdedb9acb0181c40"
         />
 
         <NewSectionButtons 
-          num1="206" name1=""
-          num2="207" name2=""
-          num3="208" name3=""
-          num4="209" name4=""
-          num5="210" name5=""
-          num6="211" name6=""
+          num1="206" name1="c914df096e72d173"
+          num2="207" name2="7977e4aebf671a7c"
+          num3="208" name3="d7afc2f95b1f589a"
+          num4="209" name4="bcaa9dd2c01b3485"
+          num5="210" name5="4a5a847d7608f957"
+          num6="211" name6="93250a433f234958"
         />
 
         <NewSectionButtons 
-          num1="212" name1=""
-          num2="213" name2=""
-          num3="214" name3=""
-          num4="215" name4=""
-          num5="216" name5=""
-          num6="217" name6=""
+          num1="212" name1="882fc5d1be38f730"
+          num2="213" name2="08c7701121c5ddcb"
+          num3="214" name3="f677db8f620e6cac"
+          num4="215" name4="8ed0809b8f12c7c9"
+          num5="216" name5="c10581248899b93c"
+          num6="217" name6="58c9b9d31424ec89"
         />
 
         <NewSectionButtons 
-          num1="218" name1=""
-          num2="219" name2=""
-          num3="220" name3=""
-          num4="221" name4=""
-          num5="222" name5=""
-          num6="223" name6=""
+          num1="218" name1="1d2a9a2e58a7505e"
+          num2="219" name2="eb3160fa6e3b980c"
+          num3="220" name3="1b3fbfa8fdc23bbc"
+          num4="221" name4="c6f1946a9881463b"
+          num5="222" name5="d97fd7bd2c47ea91"
+          num6="223" name6="4ff771ec109d86d5"
         />
 
         <NewSectionButtons 
-          num1="224" name1=""
-          num2="225" name2=""
-          num3="226" name3=""
-          num4="227" name4=""
-          num5="228" name5=""
-          num6="229" name6=""
+          num1="224" name1="3b0a89b87309aa5a"
+          num2="225" name2="27295cb07c10cc14"
+          num3="226" name3="6edca0a51e5d0594"
+          num4="227" name4="4ebd657fb739687e"
+          num5="228" name5="ce1cda947c504037"
+          num6="229" name6="8fe0afb99d40cf6f"
         />
 
         <NewSectionButtons 
-          num1="230" name1=""
-          num2="231" name2=""
-          num3="232" name3=""
-          num4="233" name4=""
-          num5="234" name5=""
-          num6="235" name6=""
+          num1="230" name1="d556adcc77dc0fbd"
+          num2="231" name2="e8c0342e5c48b809"
+          num3="232" name3="ab42d436a9d839b2"
+          num4="233" name4="f3f313c635379b4a"
+          num5="234" name5="b4cd6bec6bc0e7a0"
+          num6="235" name6="aeb6fe8ff3d55f05"
         />
 
         <NewSectionButtons 
-          num1="236" name1=""
-          num2="237" name2=""
-          num3="238" name3=""
-          num4="239" name4=""
-          num5="240" name5=""
-          num6="241" name6=""
+          num1="236" name1="046a1257f0d5fea7"
+          num2="237" name2="0c6c35373048cff2"
+          num3="238" name3="5bd54ea251c05860"
+          num4="239" name4="65306704253b2622"
+          num5="240" name5="ca5667950d9cc857"
+          num6="241" name6="47c19b2ecdd0f469"
         />
 
         <NewSectionButtons 
-          num1="242" name1=""
-          num2="243" name2=""
-          num3="244" name3=""
-          num4="245" name4=""
-          num5="246" name5=""
-          num6="247" name6=""
+          num1="242" name1="2bed7764200dcd37"
+          num2="243" name2="e02e4eb4a4f3962f"
+          num3="244" name3="d06d8c11819d148c"
+          num4="245" name4="77499725db188712"
+          num5="246" name5="590cf171a0df5fdd"
+          num6="247" name6="0c5910bfea24b94e"
         />
 
         <NewSectionButtons 
-          num1="248" name1=""
-          num2="249" name2=""
-          num3="250" name3=""
-          num4="251" name4=""
-          num5="252" name5=""
-          num6="253" name6=""
+          num1="248" name1="8d22cf941848510c"
+          num2="249" name2="579d96fef4426533"
+          num3="250" name3="5f7d13ff10241be4"
+          num4="251" name4="1392628e5f04492e"
+          num5="252" name5="5312665c80a2fec2"
+          num6="253" name6="44d32c58df3e74ab"
         />
 
         <NewSectionButtons 
-          num1="254" name1=""
-          num2="255" name2=""
-          num3="256" name3=""
-          num4="257" name4=""
-          num5="258" name5=""
-          num6="259" name6=""
+          num1="254" name1="5296807de9b5f26a"
+          num2="255" name2="4077ffd44e48f950"
+          num3="256" name3="48fc768bd86b6aee"
+          num4="257" name4="62c0a0ea2416ed8d"
+          num5="258" name5="2f84e84c1dbbf9cf"
+          num6="259" name6="719ea7193aac24ba"
         />
 
         <NewSectionButtons 
-          num1="260" name1=""
-          num2="261" name2=""
-          num3="262" name3=""
-          num4="263" name4=""
-          num5="264" name5=""
-          num6="265" name6=""
+          num1="260" name1="49dde6275a50c61a"
+          num2="261" name2="77e92a0cd9cc2774"
+          num3="262" name3="172f0142972b572f"
+          num4="263" name4="172f0142972b572f"
+          num5="264" name5="76881486fae1510c"
+          num6="265" name6="b2ee93d9cb0e4d55"
         />
 
         <NewSectionButtons 
-          num1="266" name1=""
-          num2="267" name2=""
-          num3="268" name3=""
-          num4="269" name4=""
-          num5="270" name5=""
-          num6="271" name6=""
+          num1="266" name1="758b995ac384ee4b"
+          num2="267" name2="32f67f22da2373e2"
+          num3="268" name3="66626ef1d2e304d9"
+          num4="269" name4="66e42876d8a13a80"
+          num5="270" name5="ca25a481c77764f0"
+          num6="271" name6="671a95e69b6db176"
         />
 
         <NewSectionButtons 
-          num1="272" name1=""
-          num2="273" name2=""
-          num3="274" name3=""
-          num4="275" name4=""
-          num5="276" name5=""
-          num6="277" name6=""
+          num1="272" name1="a5c1fddf7a2e2cce"
+          num2="273" name2="6026900e559e2bc0"
+          num3="274" name3="bdef8d8f22c79b9a"
+          num4="275" name4="c202ac7291cb4768"
+          num5="276" name5="b1e71f6959109cb2"
+          num6="277" name6="7ffd2a1649bc79ff"
         />
 
         <NewSectionButtons 
-          num1="278" name1=""
-          num2="279" name2=""
-          num3="280" name3=""
-          num4="281" name4=""
-          num5="282" name5=""
-          num6="283" name6=""
+          num1="278" name1="deed72260eb5e9c6"
+          num2="279" name2="c6fbd9a8c02bf69d"
+          num3="280" name3="988ae29fc94b4ffb"
+          num4="281" name4="f44cfbdcba2de4a0"
+          num5="282" name5="5dac1f9ba18d67df"
+          num6="283" name6="2f4c12ac647fac4c"
         />
 
         <NewSectionButtons 
-          num1="284" name1=""
-          num2="285" name2=""
-          num3="286" name3=""
-          num4="287" name4=""
-          num5="288" name5=""
-          num6="289" name6=""
+          num1="284" name1="96b08cd34208a973"
+          num2="285" name2="ba19ed709eb7697b"
+          num3="286" name3="538397208ceed96b"
+          num4="287" name4="8088c759dd06392a"
+          num5="288" name5="7f48374793e77aba"
+          num6="289" name6="420fbc7e97d82252"
         />
 
         <section className="episodeButtons">
-          <NewSectionLinks num="290" name="" />
-          <NewSectionLinks num="291" name="" />
+          <NewSectionLinks num="290" name="bbf8caa9b7be842a" />
+          <NewSectionLinks num="291" name="bcf9255bbd604b1a" />
         </section>
       </section>
     </div>
