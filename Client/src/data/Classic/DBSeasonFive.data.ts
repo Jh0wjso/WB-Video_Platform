98ff0be871ce7011a689ace1c18d2ec2c86d96e6
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-04-17 09:43:50 -0300

    :sparkles: feat: Finishing DB Classic

---
+++ Client/src/data/Classic/DBSeasonFive.data.ts
@@ -0,0 +1,212 @@
+import { EpInfo }  from '../EpContent'
+
+export const DBSeasonFiveData : EpInfo[] = [
+    {
+        num: "102",
+        name: "1aTOfesePLij1Oe1WC2WnmLbVinsFK5OZ",
+    },
+    {
+        num: "103",
+        name: "1aeQybhOihciHpRbIkYmXYnUBdSF67pXd",
+    },
+    {
+        num: "104",
+        name: "1aYSPS4Ae7T3blboMs_8dxFzfYPw39VRU",
+    },
+    {
+        num: "105",
+        name: "1aYPCnSk3MTBgBkVzKhM1_5LPUiLp7IK9",
+    },
+    {
+        num: "106",
+        name: "1aTKigDpF_pBSKoVvmRbWRseuPxfp5Dht",
+    },
+    {
+        num: "107",
+        name: "1aSCkNg664o1iHx7Gr6Bu5vDIJ0KbuSPp",
+    },
+    {
+        num: "108",
+        name: "1aoC4Jd0y1oC2K1Vh5PJ6ANm2CsTC9oBY",
+    },
+    {
+        num: "109",
+        name: "1afAHNDg1Vl15wdwq8cKNHCbH23XGPCe4",
+    },
+    {
+        num: "110",
+        name: "1b2Pfr4es7ANhRRCVThBBpp_-g0Sm2y9z",
+    },
+    {
+        num: "111",
+        name: "1b0XgYdGrVvsfkPdJWSHnYX_LkNB3V1rd",
+    },
+    {
+        num: "112",
+        name: "1awE8eye6v_1tR9V50evYw5_Eo-ob11gp",
+    },
+    {
+        num: "113",
+        name: "1avI4huhVhM9qhdFd1DQ4_gN7pYnjlpSd",
+    },
+    {
+        num: "114",
+        name: "1bGq66SnuGlViiGVISOOkqOHUwCVdj5MT",
+    },
+    {
+        num: "115",
+        name: "1bB9bulu7xNyiSpo7eK18VrAErWZKSXX_",
+    },
+    {
+        num: "116",
+        name: "1bLncUnTMZ77iaShOiR8M8sbtj8fHFkVk",
+    },
+    {
+        num: "117",
+        name: "1b9w5lPtphYJzJ7nsSswiACu8qAm3T9cC",
+    },
+    {
+        num: "118",
+        name: "1b6gpzll4ZE_S1diYuDFENeXzGnJHVRKf",
+    },
+    {
+        num: "119",
+        name: "1b34Lqj7DIla4QCMX5pBJxp_L871JmsLT",
+    },
+    {
+        num: "120",
+        name: "1c3sYddaQ-EndaBznYGZA4nr4JnyfOao-",
+    },
+    {
+        num: "121",
+        name: "1c2i2WxxLChBVZu6olun9TtJt2l6xAmSV",
+    },
+    {
+        num: "122",
+        name: "1bvgs1VHSzDcGbOLuqLREnB2BoCvFT6Ca",
+    },
+    {
+        num: "123",
+        name: "1bePSLOy_xkElRg9HpJvST141xBXPykhY",
+    },
+    {
+        num: "124",
+        name: "1cKCuYDGdnwGxCGp1j71mHQAP3dJns7Ho",
+    },
+    {
+        num: "125",
+        name: "1cLLfUDySqgAwDd6k9GPPoiKKX6KpZkhy",
+    },
+    {
+        num: "126",
+        name: "1czI9BlnHxKSYA2gZhcujR_7IMPTzxifJ",
+    },
+    {
+        num: "127",
+        name: "1cvIiCOPculLr4IL2lyUNgif8fOjnkPQm",
+    },
+    {
+        num: "128",
+        name: "1czykMAkFCFOU6_DyI1qhwJriIxxeY216",
+    },
+    {
+        num: "129",
+        name: "1cThnt3uqYw1Ul7lVeT4zAL8SnOmk_0C4",
+    },
+    {
+        num: "130",
+        name: "1cMoYn3PIY0fa-8b0bNEq1DrmuAI1QpeY",
+    },
+    {
+        num: "131",
+        name: "1cdm7utI2KwOO1CNut9ps3kP872acZjP9",
+    },
+    {
+        num: "132",
+        name: "1dbeSrwbZGHBLrbFb0D30jcBV-Pg4lvDV",
+    },
+    {
+        num: "133",
+        name: "1ddeaKG068N4vWUFvjGoIyWCRsqfm3e3z",
+    },
+    {
+        num: "134",
+        name: "1df6ZFixfx3QGY0xSNcNla1392qd01pVX",
+    },
+    {
+        num: "135",
+        name: "1dfAN3xX_7NzZSAwEVXx5ncF3UVh-x2KO",
+    },
+    {
+        num: "136",
+        name: "1daw3e18RDAHBFbc6DlsmmXRk_PzihFwo",
+    },
+    {
+        num: "137",
+        name: "1rDsmhHgPXP1WyIeGHh9uzEoXVFhIRJQW",
+    },
+    {
+        num: "138",
+        name: "1eJO9CEy8crt-VL2Bnf1ljBSrzWvjlqro",
+    },
+    {
+        num: "139",
+        name: "1eLcQnBT2OOqHwtVpQFNeF_c7xyVFyJBK",
+    },
+    {
+        num: "140",
+        name: "1eQZ-FgI-DS9WS-DjSu7h5NakwSoivFxc",
+    },
+    {
+        num: "141",
+        name: "1ecywS8NaorsGUpxgOIDFyHn0D1O0DOlR",
+    },
+    {
+        num: "142",
+        name: "1ejxBHLfgONJ8643GhFhfVdc9lI7EjkuP",
+    },
+    {
+        num: "143",
+        name: "1es7PbRiVLUO__HfJIvZwovE6j6KfFo2X",
+    },
+    {
+        num: "144",
+        name: "1eWacw_qeae0BIzGv36mNzt6GX528pyVF",
+    },
+    {
+        num: "145",
+        name: "1e_JI56GngSbXNC5l0PfgftJkus4nGQx5",
+    },
+    {
+        num: "146",
+        name: "1e_bE7cGTzzfaXe4Lc0YMLK_rrWjOqdLL",
+    },
+    {
+        num: "147",
+        name: "1eaVa31I4Lzg7wC0_Z38DVIGRqw1vfejb",
+    },
+    {
+        num: "148",
+        name: "1eGgfAnGxuojdTUMrpAY10m2GCWxia0wZ",
+    },
+    {
+        num: "149",
+        name: "1eJSpD-JY8gnoCnwIuLVi68Gdl6UQtgOl",
+    },
+    {
+        num: "150",
+        name: "1eO409eqkNYwT1dwxezbGSR9WBqAgMRMX",
+    },
+    {
+        num: "151",
+        name: "1eTrG-xqTtbezlABFDe7R2PuKLVH99z-x",
+    },
+    {
+        num: "152",
+        name: "1efALikK1CUrXaxONxsAy-RbZpxChRBOq",
+    },
+    {
+        num: "153",
+        name: "1eqa3ArHRxxePR-Mh5RiLwuLgdFQzXMrt",
+    },
+];
---
+++ Client/src/data/Classic/DBSeasonFour.data.ts
@@ -0,0 +1,96 @@
+import { EpInfo }  from '../EpContent'
+
+export const DBSeasonFourData : EpInfo[] = [
+    {
+        num: "79",
+        name: "1TcwasgkfVP8sjCZMJljU1jsJboqTeNDh",
+    },
+    {
+        num: "80",
+        name: "1TckSBueOUMsA-O80NGoVVH4_FE5aq0yw",
+    },
+    {
+        num: "81",
+        name: "1TaM9-A9BEoUwv_ekYtwA8tLENZxOcQTy",
+    },
+    {
+        num: "82",
+        name: "1TXnweCkg9drzoCgD2Fh6ck2eElEQ5ZCI",
+    },
+    {
+        num: "83",
+        name: "1TUe4eo3KD_Qjs8vqoccMdLOlDINpYrWX",
+    },
+    {
+        num: "84",
+        name: "1TUU8WxYq9LzrY7axkLql4QYwiZzTu53j",
+    },
+    {
+        num: "85",
+        name: "1ZKyoRankTrrYtVdK2FxC7L2cSHBzgmbp",
+    },
+    {
+        num: "86",
+        name: "1ZY9ai6r7LcA721mqBLYL_FcPfzamwOum",
+    },
+    {
+        num: "87",
+        name: "1ZTYvLddicD6h9SmftIBZxaDj4FzOsjhu",
+    },
+    {
+        num: "88",
+        name: "1ZRaR_zdVGFfrLJB6vR0AwZLJWsJS-RMN",
+    },
+    {
+        num: "89",
+        name: "1ZRZlfMbiMe1-59ZbNwLm4eclPOqbx3P7",
+    },
+    {
+        num: "90",
+        name: "1Zch7OkKcWTAxvaR3X3dlenH1ghYAx-nd",
+    },
+    {
+        num: "91",
+        name: "1_3p4TSMNJov1FAkteRm8YyCQ102rApa7",
+    },
+    {
+        num: "92",
+        name: "1_3nSCTdlN3GFxmpSwIhY_hNG2FTRb7CM",
+    },
+    {
+        num: "93",
+        name: "1_7XdN1MdKWEn3xkx4GFoJCKT3LeDPPiS",
+    },
+    {
+        num: "94",
+        name: "1ZyrMEtWeuFUVDrW0vDoqx9NUOm2S8dTL",
+    },
+    {
+        num: "95",
+        name: "1ZlmsqPfMyiAs0uYuzVL-EvF2RJfujNN6",
+    },
+    {
+        num: "96",
+        name: "1_3LZ8O8EH7cdy9R2Q-zXNKZ-mT5f7K7e",
+    },
+    {
+        num: "97",
+        name: "1_PFg0k1klstQTRJz4MYo5jjKTx-p9bf9",
+    },
+    {
+        num: "98",
+        name: "1_NxYNES11TYVrV7SoQhU6JIiYeRLz7w_",
+    },
+    {
+        num: "99",
+        name: "1_kizGUkJW7Kqp_wCKEQa3Ynsu1eZbrpL",
+    },
+    {
+        num: "100",
+        name: "1_kiMaqUOQqSWVOBGNNDSBOKI4tP3MT1V",
+    },
+    {
+        num: "101",
+        name: "1_QlUeqZTfINdsZCALkJj6y-vRHkMSfbE",
+    },
+];
