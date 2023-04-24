2b626629e63bfc101762742fd31435056515c712
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-04-24 10:52:23 -0300

    :lipstick: feat: Sidebar Style

--- Client/src/data/Sidebar/Sidebar.data.ts
+++ Client/src/data/Sidebar/Sidebar.data.ts
@@ -5,15 +5,15 @@ type SideBarInfo = {
 
 export const SideBarData : SideBarInfo[] = [
     {
-        name: "Dragon Ball Heroes: Ep 46",
+        name: "DB Heroes: Ep 46",
         url: "1-DhO14a8INhEIwti05IgjpeH90SRAKM3",
     },
     {
-        name: "Dragon Ball Heroes: Ep 45",
+        name: "DB Heroes: Ep 45",
         url: "1-9qY8jyia8r_pE4znXiH4uNB8FwO7-0R",
     },
     {
-        name: "Dragon Ball Heroes: Ep 44",
+        name: "DB Heroes: Ep 44",
         url: "1-BHedmMLqObIc8WVE95vPUTFEJbdTYzN",
     },
     {
@@ -21,31 +21,31 @@ export const SideBarData : SideBarInfo[] = [
         url: "1n4byzVSVcTeWcbzC9soMx4XfrGKm1YuG",
     },
     {
-        name: "Dragon Ball Heroes: Ep 43",
+        name: "DB Heroes: Ep 43",
         url: "1-5oPWzBhTuJR43oBfy6eURyFeB8vQZkr",
     },
     {
-        name: "Dragon Ball Heroes: Ep 42",
+        name: "DB Heroes: Ep 42",
         url: "1-7JJ5tUojK98bco-ldpPY6pF_dWZmEhS",
     },
     {
-        name: "Dragon Ball Heroes: Ep 41",
+        name: "DB Heroes: Ep 41",
         url: "1-LoIjbSORoTxxwNKpYqNbVpagvVxvLAq",
     },
     {
-        name: "Dragon Ball Heroes: Ep 40",
+        name: "DB Heroes: Ep 40",
         url: "1liykwjBh92PI4gRUCap9MbGjJw9Cw1AP",
     },
     {
-        name: "Dragon Ball Heroes: Ep 39",
+        name: "DB Heroes: Ep 39",
         url: "1lS5HvESEyzok_W-LXfyRj3laHZGbrMy-",
     },
     {
-        name: "Dragon Ball Heroes: Ep 38",
+        name: "DB Heroes: Ep 38",
         url: "1lSLINDZNWhVNNK3tahAWXgEVz91DRhNd",
     },
     {
-        name: "Dragon Ball Heroes: Ep 37",
+        name: "DB Heroes: Ep 37",
         url: "1lSrZJKHOiiyT7JqIChxoOm_jUnOH1dQ-",
     },
 ];
