a3d6d074d5cea024519fe13ce4f6c449e72be193
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-05-25 23:03:48 -0300

    :lipstick: feat: Sidebar location update

--- Client/src/screens/Inicio/index.tsx
+++ Client/src/screens/Inicio/index.tsx
@@ -6,10 +6,10 @@ import Sidebar from "../../components/SideBar";
 export default function Inicio() {
   return (
     <div className="flex w-full justify-between flex-1 min-h-screen">
+      <Sidebar />
       <div className="homeContent">
         <Home />
       </div>
-      <Sidebar />
     </div>
   );
 }
