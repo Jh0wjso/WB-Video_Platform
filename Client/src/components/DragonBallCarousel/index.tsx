46a0f3b4d2cbabdbb88959c841646ea396583359
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-05-26 21:14:48 -0300

    :lipstick: feat: Dbs Epsode Caousel Update

--- Client/src/components/DragonBallCarousel/index.tsx
+++ Client/src/components/DragonBallCarousel/index.tsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 import Carousel from "better-react-carousel";
 
-import DbS from "../../images/super.jpeg";
+import DbS from "../../images/capaDBS.jpeg";
 import { DBZCarouselData } from "../../data/Carousel/DBZCarousel.data";
 
 export default function DragonBallCarousel() {
   return (
     <div className="container flex flex-col items-center mt-9 w-screen">
-      <div className="container bg-gray-700 flex p-2 flex-row justify-between items-center w-screen border-gray-700 rounded-t-lg">
+      <div className="container flex p-2 flex-row justify-between items-center w-screen rounded-t-lg">
         <h1 className="text-2xl text-white md:text-4xl ml-4">Super</h1>
         <Link
           className="mr-4 md:mr-10 text-white chrome bg-orange-600 rounded md:py-1 px-1 md:px-2 transition-colors hover:bg-orange-400 font-bold"
@@ -16,7 +16,7 @@ export default function DragonBallCarousel() {
           Ver Mais
         </Link>
       </div>
-      <div className="carouselDb p-5 text-center font-bold mb-5 container text-gray-700 border-2 w-full rounded-b-lg bg-gray-300 md:border-gray-700">
+      <div className="carouselDb p-5 text-center font-bold mb-5 container text-gray-700 w-full rounded-b-lg">
         <Carousel cols={5} rows={1} gap={10} loop>
           {DBZCarouselData.map((item) => (
             <Carousel.Item>
@@ -25,7 +25,7 @@ export default function DragonBallCarousel() {
                 className="imageCarousel"
               >
                 <img className="coverImage" src={DbS} />
-                <p>{`${item.name}`}</p>
+                <p className="text-white mt-5">{`${item.name}`}</p>
               </Link>
             </Carousel.Item>
           ))}
