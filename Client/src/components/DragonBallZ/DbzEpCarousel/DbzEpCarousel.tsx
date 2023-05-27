17e16c53de18272f85fd02e03576cb0804e858cd
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-05-27 14:30:26 -0300

    :sparkles: feat: Dbz Eps carousel

---
+++ Client/src/components/DragonBallZ/DbzEpCarousel/DbzEpCarousel.tsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import Carousel from "better-react-carousel";
+import DbS from "../../../images/capaDBZ.jpeg";
+import { DbzEpData } from "../../../data/Carousel/DragonballZEps.data";
+
+export default function DragonBallZEpCarousel() {
+  return (
+    <div className="container flex flex-col items-center mt-9 w-screen">
+      <div className="container flex p-2 flex-row justify-between items-center w-screen rounded-t-lg">
+        <h1 className="text-2xl text-white md:text-4xl ml-4">DBZ</h1>
+        <Link
+          className="mr-4 md:mr-10 text-white chrome bg-orange-600 rounded md:py-1 px-1 md:px-2 transition-colors hover:bg-orange-400 font-bold"
+          to="/dbz"
+        >
+          Ver Mais
+        </Link>
+      </div>
+      <div className="carouselDb p-5 text-center font-bold mb-5 container text-gray-700 w-full rounded-b-lg">
+        <Carousel cols={5} rows={1} gap={10} loop>
+          {DbzEpData.map((item) => (
+            <Carousel.Item>
+              <Link
+                to={`/video2/${item.url}/${item.ep}`}
+                className="imageCarousel"
+              >
+                <img className="coverImage" src={DbS} />
+                <p className="text-white mt-5">{`${item.name}`}</p>
+              </Link>
+            </Carousel.Item>
+          ))}
+        </Carousel>
+      </div>
+    </div>
+  );
+}
--- Client/src/components/Home/index.tsx
+++ Client/src/components/Home/index.tsx
@@ -3,11 +3,13 @@ import DragonBallCarousel from "../DragonBallCarousel";
 
 import "./styles.css";
 import MovieContentZ from "../DragonBallZ/MovieContentZ";
+import DragonBallZEpCarousel from "../DragonBallZ/DbzEpCarousel/DbzEpCarousel";
 
 export default function Home() {
   return (
     <section className="homeContent flex flex-col items-center justify-center">
       <div className="flex flex-col">
+        <DragonBallZEpCarousel />
         <HeroesCarousel />
         <DragonBallCarousel />
         <MovieContentZ />
