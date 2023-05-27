d30def913b0b8a138490ac8bbfd597f44d228724
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-05-27 14:29:57 -0300

    :sparkles: feat: Dbz Data eps for carousel

---
+++ Client/src/data/Carousel/DragonballZEps.data.ts
@@ -0,0 +1,54 @@
+import { CarouselInfo } from "./Carousel";
+
+export const DbzEpData: CarouselInfo[] = [
+  {
+    ep: "1",
+    url: "850598be5727e69f",
+    name: "Surge um mini Goku! Seu nome é ...",
+  },
+  {
+    ep: "2",
+    url: "fee9188d76a91589",
+    name: "Um guerreiro muito poderoso com ...",
+  },
+  {
+    ep: "3",
+    url: "78b47c3e7fe8d4f1",
+    name: "A combinação mais ...",
+  },
+  {
+    ep: "4",
+    url: "9064b9d45c8508bc",
+    name: "Piccolo usa sua melhor ...",
+  },
+  {
+    ep: "5",
+    url: "d0e18cb36c85a3a8",
+    name: "Goku morre! Só existe ...",
+  },
+  {
+    ep: "6",
+    url: "3c47745a250b4f5f",
+    name: "Enma Samá também ...",
+  },
+  {
+    ep: "7",
+    url: "07943d610003edaf",
+    name: "Terá que sobreviver aos ...",
+  },
+  {
+    ep: "8",
+    url: "20ce0c2ebfe76e02",
+    name: "Uma transformação por causa da ...",
+  },
+  {
+    ep: "9",
+    url: "066225268c8e8d10",
+    name: "Perdão Sr. Robô! Lágrimas ...",
+  },
+  {
+    ep: "10",
+    url: "eb5a4d62cc6542c0",
+    name: "Não chore Gohan! É a primeira ...",
+  },
+];
