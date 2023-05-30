bcda419b54f5aca4e1ea8c98c0879f5bb8f288a9
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-05-29 21:53:03 -0300

    :lipstick: feat: Page not found update

--- Client/src/screens/PageNotfound/index.tsx
+++ Client/src/screens/PageNotfound/index.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './styles.css'
+import { Link } from "react-router-dom";
+import "./styles.css";
 import Icon from "../../images/undraw_drink_coffee_0asa.svg";
 
 const PageNotFound = () => {
   return (
-    <div className='containerContent'>
-    <img src={Icon} width={300} alt="Dragon Ball cover" className="imgIcon"/>
-      <h1 className='tittleErrorMessage'>404 - Página não encontrada</h1>
-      <p className='subTittleErrorMessage'>Kamisama ainda está construindo essa area.</p>
-      <Link to="/" className='btnBack'>
+    <div className="containerContent">
+      <img src={Icon} width={300} alt="Dragon Ball cover" className="imgIcon" />
+      <h1 className="tittleErrorMessage">404 - Página não encontrada</h1>
+      <p className="subTittleErrorMessage">
+        Kamisama ainda está construindo essa area.
+      </p>
+      <Link to="/" className="btnBack">
         Voltar para o site
       </Link>
     </div>
