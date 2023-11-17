954aaba6466ff099d118fa74a741d705b2b0da25
Jhonatan Silvério <Jhonatan.Silverio@csgi.com>
2023-11-16 22:23:46 -0300

    :sparkles: feat: Movie row one data setup

--- Client/src/data/Movies/Movies.data.ts
+++ Client/src/data/Movies/Movies.data.ts
@@ -6,8 +6,23 @@ type MovieInfo = {
 
 export const MovieData : MovieInfo [] = [
     {
-        tittle: "",
-        path: "",
-        poster: ""
+        tittle: "A Lenda de Shenlong",
+        path: "/assistir/A Lenda de Shenlong/1C624UGEH3nAfd6W7XxqbsBFEPPCkWD9-",
+        poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/9CxRXbiutg7vn2rNTPFN3wGVyRe.jpg"
+    },
+    {
+        tittle: "O Castelo do Diabo",
+        path: "/assistir/O Castelo do Diabo/11pX0JMBmNVvMS8xvF01QA-s78X3UTH7w",
+        poster: "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/scvwYMmVLI5aEBX4efEG9ByjeDu.jpg"
+    },
+    {
+        tittle: "Uma Aventura Mística",
+        path: "/assistir/Uma Aventura Mistica/1aYH-8R8TIkBiIJhz0UfUp6A8XfQEbaoC",
+        poster: "https://images.justwatch.com/poster/104110837/s718/dragon-ball-uma-aventura-mistica.jpg"
+    },
+    {
+        tittle: "Devolva-me Gohan",
+        path: "/assistir/Devolva-me Gohan/1-DXvCLJKSeaS4QB2BttDRh1hgQ5RgOKI",
+        poster: "https://d2d7ho1ae66ldi.cloudfront.net/ArquivoTitulos/07596091-a263-11ea-aa6c-d62279b277aa/800_Dragon+Ball+Z%3A+Devolva-me+Gohan!!_poster.jpg"
     },
 ]
