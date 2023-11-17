13266fa66e97f5dc28e01c591c3c1e601c83b386
Jhonatan Silvério <Jhonatan.Silverio@csgi.com>
2023-11-16 22:24:01 -0300

    :sparkles: feat: Movie row one code update

--- Client/src/components/MoviesRowComponents/MovieRowOne/index.tsx
+++ Client/src/components/MoviesRowComponents/MovieRowOne/index.tsx
@@ -1,29 +1,18 @@
 import { Link } from "react-router-dom";
-
-import Um from "../../../images/MoviesImages/1.jpg";
-import Dois from "../../../images/MoviesImages/2.jpg";
-import Tres from "../../../images/MoviesImages/3.jpg";
-import Quatro from "../../../images/MoviesImages/4.jpg";
+import { MovieData } from "../../../data/Movies/Movies.data";
 
 export default function MovieRowOne() {
   return (
     <div className="cardsContainer">
-      <Link className="cardAnime" to="/assistir/A Lenda de Shenlong/1C624UGEH3nAfd6W7XxqbsBFEPPCkWD9-">
-        <img src={Um} width={250} className="coverImage" />
-        <p className="textCard">A Lenda de Shenlong</p>
-      </Link>
-      <Link to={"/assistir/O Castelo do Diabo/11pX0JMBmNVvMS8xvF01QA-s78X3UTH7w"} className="cardAnime">
-        <img src={Dois} width={250} className="coverImage" />
-        <p className="textCard">O Castelo do Diabo</p>
-      </Link>
-      <Link to={"/assistir/Uma Aventura Mistica/1aYH-8R8TIkBiIJhz0UfUp6A8XfQEbaoC"} className="cardAnime">
-        <img src={Tres} width={250} className="coverImage" />
-        <p className="textCard">Uma Aventura Mística</p>
-      </Link>
-      <Link to={"/assistir/Devolva-me Gohan/1-DXvCLJKSeaS4QB2BttDRh1hgQ5RgOKI"} className="cardAnime">
-        <img src={Quatro} width={250} className="coverImage" />
-        <p className="textCard">Devolva-me Gohan</p>
-      </Link>
+      {MovieData.map((item) => (
+        <Link
+          className="cardAnime"
+          to={`${item.path}`}
+        >
+          <img src={`${item.poster}`} width={250} className="coverImage" />
+          <p className="textCard">{`${item.tittle}`}</p>
+        </Link>
+      ))}
     </div>
   );
 }
