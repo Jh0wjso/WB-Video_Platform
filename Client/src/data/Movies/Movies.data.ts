0b7e788dc787e72d5072e0d6aa3af8f0226de856
Jhonatan Silvério <Jhonatan.Silverio@csgi.com>
2023-11-16 22:07:28 -0300

    :sparkles: feat: Movie Data setup

---
+++ Client/src/data/Movies/Movies.data.ts
@@ -0,0 +1,13 @@
+type MovieInfo = {
+    tittle: string
+    path: string
+    poster: string
+}
+
+export const MovieData : MovieInfo [] = [
+    {
+        tittle: "",
+        path: "",
+        poster: ""
+    },
+]
