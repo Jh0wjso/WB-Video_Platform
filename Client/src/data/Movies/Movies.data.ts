45017b754905888c9cccf0a9477aa3505c2abf00
Jhonatan Silvério <Jhonatan.Silverio@csgi.com>
2023-11-17 22:44:02 -0300

    :sparkles: feat: Movie row to setup on Movies data

--- Client/src/data/Movies/Movies.data.ts
+++ Client/src/data/Movies/Movies.data.ts
@@ -25,4 +25,24 @@ export const MovieData : MovieInfo [] = [
         path: "/assistir/Devolva-me Gohan/1-DXvCLJKSeaS4QB2BttDRh1hgQ5RgOKI",
         poster: "https://d2d7ho1ae66ldi.cloudfront.net/ArquivoTitulos/07596091-a263-11ea-aa6c-d62279b277aa/800_Dragon+Ball+Z%3A+Devolva-me+Gohan!!_poster.jpg"
     },
+    {
+        tittle: "O Homem Mais Forte do Mundo",
+        path: "/assistir/O Homem Mais Forte do Mundo/1TG-0p34ILl5OmxRcMJ-tscz0keqxYT6N",
+        poster: "https://images.justwatch.com/poster/104108253/s592/dragon-ball-z-o-homem-mais-forte-do-mundo"
+    },
+    {
+        tittle: "A Árvore do Poder",
+        path: "/assistir/A Arvore do Poder/1y-GYI3pinbls3CraedGWneolO-IFoioE",
+        poster: "https://images.justwatch.com/poster/104126227/s592/dragon-ball-z-a-arvore-do-poder"
+    },
+    {
+        tittle: "Goku o Super Saiyajin",
+        path: "/assistir/Goku o Super Saiyajin/1TiAM1jZDNScj9Q0MmzWe3XDIzlldQdfX",
+        poster: "https://images.justwatch.com/poster/104111183/s718/dragon-ball-z-goku-o-super-sayajin.jpg"
+    },
+    {
+        tittle: "Uma vingança para Freeza",
+        path: "/assistir/Uma vingança para Freeza/1TqoPudcj99YoVkiR6fZOU4Odqu3Pijqu",
+        poster: "https://images.justwatch.com/poster/104126377/s332/dragon-ball-z-uma-vinganca-para-freeza"
+    },
 ]
