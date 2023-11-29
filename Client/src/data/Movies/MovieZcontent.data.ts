5a003e127262dc7471da876e2b922b3b1b4daed3
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-11-28 22:43:21 -0300

    :sparkles: feat: Movie Z Data Setup

--- Client/src/data/Movies/MovieZcontent.data.ts
+++ Client/src/data/Movies/MovieZcontent.data.ts
@@ -2,8 +2,78 @@ import { MovieInfo } from "./MovieInfo";
 
 export const MovieZData : MovieInfo [] = [
     {
-        path: "",
-        poster: "",
-        tittle: ""
+        path: "/assistir/A Lenda de Shenlong/1C624UGEH3nAfd6W7XxqbsBFEPPCkWD9-",
+        poster: "https://images.justwatch.com/poster/104111860/s718/dragon-ball-a-lenda-de-shenlong.jpg",
+        tittle: "A Lenda de Shenlong"
+    },
+    {
+        path: "/assistir/O Homem Mais Forte do Mundo/1TG-0p34ILl5OmxRcMJ-tscz0keqxYT6N",
+        poster: "https://images.justwatch.com/poster/104108253/s718/dragon-ball-z-o-super-heroi.jpg",
+        tittle: "O Homem Mais Forte do Mundo"
+    },
+    {
+        tittle: "A Árvore do Poder",
+        path: "/assistir/A Arvore do Poder/1y-GYI3pinbls3CraedGWneolO-IFoioE",
+        poster: "https://images.justwatch.com/poster/104126227/s592/dragon-ball-z-a-arvore-do-poder"
+    },
+    {
+        tittle: "Goku o Super Saiyajin",
+        path: "/assistir/Goku o Super Saiyajin/1TiAM1jZDNScj9Q0MmzWe3XDIzlldQdfX",
+        poster: "https://images.justwatch.com/poster/104111183/s718/dragon-ball-z-goku-o-super-sayajin.jpg"
+    },
+    {
+        tittle: "Uma vingança para Freeza",
+        path: "/assistir/Uma vingança para Freeza/1TqoPudcj99YoVkiR6fZOU4Odqu3Pijqu",
+        poster: "https://images.justwatch.com/poster/104126377/s332/dragon-ball-z-uma-vinganca-para-freeza"
+    },
+    {
+        tittle: "O retorno de Cooler",
+        path: "/assistir/O retorno de Cooler/1XmsxNt8rmwCXB3gzJATwY50pGOeDBQsD",
+        poster: "https://images.justwatch.com/poster/104126187/s332/dragon-ball-z-o-retorno-de-coola"
+    },
+    {
+        tittle: "O Retorno dos Androides",
+        path: "/assistir/O Retorno dos Androides/1YD09OLCgsie8euaSCmAMB79mj8Z_gjn2",
+        poster: "https://images.justwatch.com/poster/104110907/s592/dragon-ball-z-o-retorno-dos-androides"
+    },
+    {
+        tittle: "O lendário super Saiyajin",
+        path: "/assistir/O lendário super Saiyajin/1Xfe79rEjTt-iyxtmn905_jJ9BtnrkWIC",
+        poster: "https://images.justwatch.com/poster/105666773/s718/dragon-ball-z-broly-the-legendary-super-saiyan.jpg"
+    },
+    {
+        tittle: "Batalha nos Dois Mundos",
+        path: "/assistir/Batalha nos Dois Mundos/18God08mQPM6MK1bnvrLipFnf2wnjDp3X",
+        poster: "https://images.justwatch.com/poster/104108874/s592/dragon-ball-z-a-batalha-nos-dois-mundos"
+    },
+    {
+        tittle: "O Retorno de Broly",
+        path: "/assistir/O Retorno de Broly/1gleAXntyC7pK-lxBbh6SXpWaXvQsRaj",
+        poster: "https://images.justwatch.com/poster/104126130/s718/dragon-ball-z-o-retorno-do-guerreiro-lendario.jpg"
+    },
+    {
+        tittle: "Renascimento Da Fusão",
+        path: "/assistir/Renascimento Da Fusão/1glo0fH3a3M5JMZBCmbPbspzFucMYjCdF",
+        poster: "https://images.justwatch.com/poster/124366259/s718/dragon-ball-z-a-fusao.jpg"
+    },
+    {
+        tittle: "Combate Final Bio-Broly",
+        path: "/assistir/Combate Final Bio-Broly/1gjHJU5loy6iqNRN6SqHzsTuNGZ8yxafy",
+        poster: "https://images.justwatch.com/poster/104108513/s718/dragon-ball-z-o-combate-final-bio-broly.jpg"
+    },
+    {
+        tittle: "O Ataque do Dragão",
+        path: "/assistir/O Ataque do Dragão/1gr5LNLLjpzkA4zPnv4bA984k-nIZDtJu",
+        poster: "https://images.justwatch.com/poster/104111148/s718/dragon-ball-z-o-ataque-do-dragao.jpg"
+    },
+    {
+        tittle: "Batalha dos Deuses",
+        path: "/assistir/Batalha dos Deuses/1k3__SaqihW9lFiBQeEFJ3rLFblp4sLbL",
+        poster: "https://images.justwatch.com/poster/256827026/s592/dragon-ball-z-battle-of-gods-theatrical-version"
+    },
+    {
+        tittle: "O Renascimento de Freeza",
+        path: "/assistir/O Renascimento de Freeza/1kMkntsimdLRlSsAUOa0_FL1jGi-Uvyf6",
+        poster: "https://images.justwatch.com/poster/86425722/s718/dragon-ball-z-o-renascimento-de-freeza.jpg"
     }
 ];
