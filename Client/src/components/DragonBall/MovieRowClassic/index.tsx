e3dd37bfdb1b79756a56c0a654bd035164aca57c
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-11-29 22:55:25 -0300

    :sparkles: feat: Movie row classic update

--- Client/src/components/DragonBall/MovieRowClassic/index.tsx
+++ Client/src/components/DragonBall/MovieRowClassic/index.tsx
@@ -1,31 +1,19 @@
 import { Link } from "react-router-dom";
-
-import Um from "../../../images/MoviesImages/1.jpg";
-import Dois from "../../../images/MoviesImages/2.jpg";
-import Tres from "../../../images/MoviesImages/3.jpg";
-import Quatro from "../../../images/MoviesImages/17.jpg";
+import { MovieClassicData } from "../../../data/Movies/MovieClassic.data";
 
 export default function MovieRowClassic() {
   return (
     <>
       <p className="font-bold text-5xl mt-6">Filmes da Série</p>
       <div className="cardsContainer">
-        <Link to={"/assistir/A Lenda de Shenlong/1C624UGEH3nAfd6W7XxqbsBFEPPCkWD9-"} className="cardAnime">
-          <img src={Um} width={250} className="coverImage" />
-          <p className="textCard">A Lenda de Shenlong</p>
+        {
+          MovieClassicData.map((item) => (
+            <Link to={`${item.path}`} className="cardAnime">
+          <img src={`${item.poster}`} width={250} className="coverImage" />
+          <p className="textCard">{item.tittle}</p>
         </Link>
-        <Link to={"/assistir/O Castelo do Diabo/11pX0JMBmNVvMS8xvF01QA-s78X3UTH7w"} className="cardAnime">
-          <img src={Dois} width={250} className="coverImage" />
-          <p className="textCard">O Castelo do Diabo</p>
-        </Link>
-        <Link to={"/assistir/Uma Aventura Mistica/1aYH-8R8TIkBiIJhz0UfUp6A8XfQEbaoC"} className="cardAnime">
-          <img src={Tres} width={250} className="coverImage" />
-          <p className="textCard">Uma Aventura Mística</p>
-        </Link>
-        <Link to={"/assistir/Em busca do poder/1-DXvCLJKSeaS4QB2BttDRh1hgQ5RgOKI"} className="cardAnime">
-          <img src={Quatro} width={250} className="coverImage" />
-          <p className="textCard">Em busca do poder</p>
-        </Link>
+          ))
+        }
       </div>
     </>
   );
---
+++ Client/src/data/Movies/MovieClassic.data.ts
@@ -0,0 +1,24 @@
+import { MovieInfo } from "./MovieInfo";
+
+export const MovieClassicData : MovieInfo [] = [
+    {
+        path: "/assistir/A Lenda de Shenlong/1C624UGEH3nAfd6W7XxqbsBFEPPCkWD9-",
+        poster: "https://images.justwatch.com/poster/104111860/s718/dragon-ball-a-lenda-de-shenlong.jpg",
+        tittle: "A Lenda de Shenlong"
+    },
+    {
+        path: "/assistir/O Castelo do Diabo/11pX0JMBmNVvMS8xvF01QA-s78X3UTH7w",
+        poster: "https://images.justwatch.com/poster/104110874/s592/dragon-ball-o-castelo-fantastico",
+        tittle: "O Castelo do Diabo"
+    },
+    {
+        path: "/assistir/Uma Aventura Mistica/1aYH-8R8TIkBiIJhz0UfUp6A8XfQEbaoC",
+        poster: "https://images.justwatch.com/poster/104110837/s718/dragon-ball-aventura-mistica.jpg",
+        tittle: "Uma Aventura Mistica"
+    },
+    {
+        path: "/assistir/Em busca do poder/1-DXvCLJKSeaS4QB2BttDRh1hgQ5RgOKI",
+        poster: "https://pm1.aminoapps.com/6924/35748487486759e4576211db10570d25c0002e9br1-342-481v2_uhq.jpg",
+        tittle: "Em busca do poder"
+    },
+];
