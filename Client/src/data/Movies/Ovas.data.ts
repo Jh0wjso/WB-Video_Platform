7a80b178dcf1b62ee60cd6c5d86c02173b030d85
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-12-01 23:29:39 -0300

    :sparkles: feat: Ova data setup

--- Client/src/data/Movies/Ovas.data.ts
+++ Client/src/data/Movies/Ovas.data.ts
@@ -2,23 +2,23 @@ import { MovieInfo } from "./MovieInfo";
 
 export const OvasData : MovieInfo [] = [
     {
-        path: "",
-        poster: "",
-        tittle: ""
+        path: "/video/Segurança no trânsito/1WKQ4j_18wcNOJjrqU8L7l9eSTb9NGeJo",
+        poster: "https://media.fstatic.com/RY71QEtJumyPCsUNpOt9Je_r16Q=/322x478/smart/filters:format(webp)/media/movies/covers/2014/11/dragon-ball-a-seguranca-de-transito-de-goku_t35112.jpg",
+        tittle: "Segurança no trânsito"
     },
     {
-        path: "",
-        poster: "",
-        tittle: ""
+        path: "/video/O Corpo de Bombeiros de Goku/1cUGipvHu9vGjorpenSdIDO0XcnzB3anB",
+        poster: "https://media.fstatic.com/qEHQAV940BtyINwJj6LZ-tZ_DI8=/322x478/smart/filters:format(webp)/media/movies/covers/2014/11/dragon-ball-o-corpo-de-bombeiros-de-goku_t35110.jpg",
+        tittle: "O Corpo de Bombeiros de Goku"
     },
     {
-        path: "",
-        poster: "",
-        tittle: ""
+        path: "/video/Junte-se a nós! O Mundo de Goku/1IxGJwpk8bmEiloVsXSkReqtkpBYZwFri",
+        poster: "https://351f7c20ce.cbaul-cdnwnd.com/da10ec0b067a65f626bdcfd8f87cb29f/system_preview_detail_200000014-ee2fcef2b1/tere.jpg",
+        tittle: "Junte-se a nós! O Mundo de Goku"
     },
     {
-        path: "",
-        poster: "",
-        tittle: ""
+        path: "/video/O Plano Para Erradicar os Saiyajins/1BCWfFWHzdTJlVZKj9nyOhOw0W_y62Mwx",
+        poster: "https://static.wikia.nocookie.net/dragonball/images/f/fa/Dbzgaidenova_.jpg",
+        tittle: "O Plano Para Erradicar os Saiyajins"
     },
 ];
