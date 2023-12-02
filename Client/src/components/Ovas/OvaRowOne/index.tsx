647a715fec944ea061c7af19d04333bcce9b1a07
Jh0wjso <jhonatansilverio1258@gmail.com>
2023-12-01 23:32:20 -0300

    :sparkles: feat: Script ova update

--- Client/src/components/Ovas/OvaRowOne/index.tsx
+++ Client/src/components/Ovas/OvaRowOne/index.tsx
@@ -1,29 +1,18 @@
 import { Link } from "react-router-dom";
-
-import Um from "../../../images/Ovas/1.jpeg";
-import Dois from "../../../images/Ovas/2.jpeg";
-import Tres from "../../../images/Ovas/3.jpeg";
-import Quatro from "../../../images/Ovas/4.jpeg";
+import { OvasData } from "../../../data/Movies/Ovas.data";
 
 export default function OvaRowOne() {
   return (
     <div className="cardsContainer">
-      <Link to={"/video/Segurança no trânsito/1WKQ4j_18wcNOJjrqU8L7l9eSTb9NGeJo"} className="cardAnime">
-        <img src={Um} width={250} className="coverImage" />
-        <p className="textCard">Segurança no trânsito</p>
-      </Link>
-      <Link to={"/video/O Corpo de Bombeiros de Goku/1cUGipvHu9vGjorpenSdIDO0XcnzB3anB"} className="cardAnime">
-        <img src={Dois} width={250} className="coverImage" />
-        <p className="textCard">O Corpo de Bombeiros de Goku</p>
-      </Link>
-      <Link to={"/video/Junte-se a nós! O Mundo de Goku/1IxGJwpk8bmEiloVsXSkReqtkpBYZwFri"} className="cardAnime">
-        <img src={Tres} width={250} className="coverImage" />
-        <p className="textCard">Junte-se a nós! O Mundo de Goku</p>
-      </Link>
-      <Link to={"/video/O Plano Para Erradicar os Saiyajins/1BCWfFWHzdTJlVZKj9nyOhOw0W_y62Mwx"} className="cardAnime">
-        <img src={Quatro} width={250} className="coverImage" />
-        <p className="textCard">O Plano Para Erradicar os Saiyajins</p>
-      </Link>
+      {OvasData.map((item) => (
+        <Link
+          className="cardAnime"
+          to={`${item.path}`}
+        >
+          <img src={`${item.poster}`} width={250} className="coverImage" />
+          <p className="textCard">{`${item.tittle}`}</p>
+        </Link>
+      ))}
     </div>
   );
 }
