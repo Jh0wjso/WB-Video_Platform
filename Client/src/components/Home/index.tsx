40c1e64694d75960d9555d7e0cb94b954b354d60
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-01-16 22:02:26 -0300

    git commit -m ":sparkles: feat: Setup home ep on home"

--- Client/src/components/Home/index.tsx
+++ Client/src/components/Home/index.tsx
@@ -4,11 +4,13 @@ import DragonBallCarousel from "../DragonBallCarousel";
 import "./styles.css";
 import MovieContentZ from "../DragonBallZ/MovieContentZ";
 import DragonBallZEpCarousel from "../DragonBallZ/DbzEpCarousel/DbzEpCarousel";
+import HomeEp from "../HomeEp/HomeEp";
 
 export default function Home() {
   return (
     <section className="homeContent flex flex-col items-center justify-center">
       <div className="flex flex-col">
+        <HomeEp />
         <DragonBallZEpCarousel />
         <HeroesCarousel />
         <DragonBallCarousel />
