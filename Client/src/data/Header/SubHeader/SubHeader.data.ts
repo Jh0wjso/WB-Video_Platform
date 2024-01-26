b735f15cb4b44fb3fde5620ff8b7d877472d5686
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-01-26 11:57:10 -0300

    :sparkles: feat: Setup subHeader

---
+++ Client/src/data/Header/SubHeader/SubHeader.data.ts
@@ -0,0 +1,27 @@
+type SubHeaderType = {
+  link: string;
+  name: string;
+};
+
+export const SubHeaderContent: SubHeaderType[] = [
+  {
+    link: "classic",
+    name: "DB",
+  },
+  {
+    link: "dbz",
+    name: "DBZ",
+  },
+  {
+    link: "dbkai",
+    name: "DBK",
+  },
+  {
+    link: "dbsuper",
+    name: "DBS",
+  },
+  {
+    link: "dbheroes",
+    name: "DBH",
+  },
+];
