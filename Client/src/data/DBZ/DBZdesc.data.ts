7f4a661b847aeb08f48023a17fb52328d15ddc7b
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-06-26 19:50:58 -0300

    :sparkles: feat: Setup Anime desc

---
+++ Client/src/data/DBZ/DBZdesc.data.ts
@@ -0,0 +1,16 @@
+import { AnimeDescription } from "../AnimeDesc";
+
+export const DBZDesc: AnimeDescription = {
+  name: "Dagon Ball Z",
+  categories: [
+    "Action",
+    "Adventure",
+    "Comedy",
+    "Fantasy",
+    "Martial Arts",
+    "Shounen",
+  ],
+  description:
+    "Dragon Ball is a Japanese media franchise created by Akira Toriyama in 1984. The initial manga, written and illustrated by Toriyama, was serialized in Weekly Shōnen Jump from 1984 to 1995, with the 519 individual chapters collected into 42 tankōbon volumes by its publisher Shueisha. Dragon Ball was initially inspired by the classical Chinese novel Journey to the West, as well as Hong Kong martial arts films. The series follows the adventures of the protagonist, Son Goku, from his childhood through adulthood as he trains in martial arts. He spends his childhood far from civilization until he meets a teen.",
+  link: "dbz",
+};
