174138909ebaeadbecb259f6643e9457cd2d87ef
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-06-27 13:48:05 -0300

    :sparkles: feat: Home page update

--- Client/src/components/DragonBallZ/DbzEpCarousel/DbzEpCarousel.tsx
+++ Client/src/components/DragonBallZ/DbzEpCarousel/DbzEpCarousel.tsx
@@ -1,31 +1,37 @@
 import { Link } from "react-router-dom";
-import Carousel from "better-react-carousel";
+import { IoIosPlayCircle } from "react-icons/io";
 import { DbzEpData } from "../../../data/Carousel/DragonballZEps.data";
 
 export default function DragonBallZEpCarousel() {
   return (
     <div className="containerHomeEp">
-      <div className="container flex p-2 flex-row justify-between items-center border-b rounded">
-        <h1 className="carouselTittle">Dragon Ball Z</h1>
-        <Link
-          className="mr-4 md:mr-10 text-white chrome bg-orange-600 rounded md:py-1 px-1 md:px-2 transition-colors hover:bg-orange-400 font-bold"
-          to="/dbz"
-        >
-          Ver Mais
-        </Link>
+      <div className="container flex items-center justify-center py-4">
+        <p className="mx-6 text-xl text-gray-400">All</p>
+        <p className="mx-6 text-xl">Anime</p>
+        <p className="mx-6 text-xl">Movies</p>
+        <p className="mx-6 text-xl">Mangas</p>
       </div>
       <div className="flex flex-wrap justify-center">
-      {DbzEpData.map((item) => (
-        <Link
-          to={`/video2/${item.url}/${item.ep}`}
-          className="hover:underline m-3"
-        >
-          <img className="coverImage" src={`${item.image}`} width={350} />
-          <div className="flex flex-row items-center">
-            <p className="text-white">{`${item.name}`}</p>
-          </div>
-        </Link>
-      ))}
+        {DbzEpData.map((item) => (
+          <Link
+            to={`/${item.url}`}
+            className="relative m-3 flex flex-col items-center transition-all duration-300 ease-in-out"
+            style={{ width: 200 }}
+          >
+            <img
+              className="coverImage opacity-60"
+              src={item.image}
+              style={{ width: 200, height: 400 }}
+              alt={item.name}
+            />
+            <p className="text-white flex flex-col absolute bottom-0 bg-black bg-opacity-50 w-full text-start p-2">
+              <p className="font-bold">{item.name}</p>
+              <p className="flex items-center text-xs">
+                <IoIosPlayCircle className="mr-2" size={20} /> {item.ep}
+              </p>
+            </p>
+          </Link>
+        ))}
       </div>
     </div>
   );
