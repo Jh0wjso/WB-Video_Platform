fbc0b2a0c7167fde1582a96b8379748292264b21
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-06-28 08:10:28 -0300

    :lipstick: feat: Update Footer

--- Client/src/components/Footer/index.tsx
+++ Client/src/components/Footer/index.tsx
@@ -1,6 +1,4 @@
 import { Link } from "react-router-dom";
-
-import Ball from "../../images/Dragonball.png";
 import "./styles.css";
 import { HeaderContent } from "../../data/Header/HeaderContent.data";
 
@@ -8,11 +6,8 @@ export default function Footer() {
   return (
     <footer className="p-4 flex flex-col justify-center items-center bg-gray-700 shadow md:px-6 md:py-8">
       <div className="footerContent container md:flex items-center md:flex-row md:items-center justify-center md:justify-between">
-        <Link to="/" className="flex items-center mx-3 mb-2 sm:mb-0">
-          <img src={Ball} width={35} className="mr-2" />
-          <span className="self-center text-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            WatchBall
-          </span>
+        <Link to="/" className="flex flex-row items-center">
+          <h1 className="pageTittle">WATCHBALL</h1>
         </Link>
         <ul className="navbarLinksContainer">
           {HeaderContent.map((e) => (
@@ -21,15 +16,6 @@ export default function Footer() {
             </li>
           ))}
         </ul>
-      </div>
-      <div className="container">
-        <hr className="my-6 sm:mx-auto lg:my-8" />
-        <span className="block text-sm text-center dark:text-gray-400">
-          © 2024{" "}
-          <Link to="/" className="hover:underline hover:text-gray-500">
-            WatchBall
-          </Link>
-        </span>
       </div>
     </footer>
   );
