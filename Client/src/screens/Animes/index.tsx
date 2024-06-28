c3b0f5ad11c3f36ce935ed044d6e3875510b8374
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-06-28 07:28:03 -0300

    :lipstick: feat: Setup Header

--- Client/src/screens/Animes/index.tsx
+++ Client/src/screens/Animes/index.tsx
@@ -10,50 +10,52 @@ import Dbova from "../../images/capaDBO.jpg";
 import Dbes from "../../images/capaDBEs.jpeg";
 
 import "./styles.css";
+import Header from "../../components/Header";
 
 export default function Animes() {
   return (
-    <div className="containerContent">
-      <h1 className="secondContainer">
-        Assista a todos os animes
-      </h1>
-      <div className="cardsContainer">
-        <Link to={"/classic"} className="cardAnime">
-          <img src={Db} width={250} className="coverImage" />
-          <p className="textCard">Dragon Ball</p>
-        </Link>
-        <Link to={"/dbz"} className="cardAnime">
-          <img src={Dbz} width={250} className="coverImage" />
-          <p className="textCard">Dragon Ball Z</p>
-        </Link>
+    <>
+      <Header />
+      <div className="containerContent">
+        <h1 className="secondContainer">Assista a todos os animes</h1>
+        <div className="cardsContainer">
+          <Link to={"/classic"} className="cardAnime">
+            <img src={Db} width={250} className="coverImage" />
+            <p className="textCard">Dragon Ball</p>
+          </Link>
+          <Link to={"/dbz"} className="cardAnime">
+            <img src={Dbz} width={250} className="coverImage" />
+            <p className="textCard">Dragon Ball Z</p>
+          </Link>
 
-        <Link to={"/dbkai"} className="cardAnime">
-          <img src={Dbkai} width={250} className="coverImage" />
-          <p className="textCard">Dragon Ball Z Kai</p>
-        </Link>
-        <Link to={"/dbsuper"} className="cardAnime">
-          <img src={Dbs} width={250} className="coverImage" />
-          <p className="textCard">Dragon Ball Super</p>
-        </Link>
+          <Link to={"/dbkai"} className="cardAnime">
+            <img src={Dbkai} width={250} className="coverImage" />
+            <p className="textCard">Dragon Ball Z Kai</p>
+          </Link>
+          <Link to={"/dbsuper"} className="cardAnime">
+            <img src={Dbs} width={250} className="coverImage" />
+            <p className="textCard">Dragon Ball Super</p>
+          </Link>
+        </div>
+        <div className="cardsContainer">
+          <Link to={"/dbgt"} className="cardAnime">
+            <img src={Dbgt} width={250} className="coverImage" />
+            <p className="textCard">Dragon Ball GT</p>
+          </Link>
+          <Link to={"/dbheroes"} className="cardAnime">
+            <img src={Dbh} width={250} className="coverImage" />
+            <p className="textCard">Heroes</p>
+          </Link>
+          <Link to={"/ova"} className="cardAnime">
+            <img src={Dbova} width={250} className="coverImage" />
+            <p className="textCard">Dragon Ball OVAs</p>
+          </Link>
+          <Link to={"/special"} className="cardAnime">
+            <img src={Dbes} width={250} className="coverImage" />
+            <p className="textCard">Especiais</p>
+          </Link>
+        </div>
       </div>
-      <div className="cardsContainer">
-        <Link to={"/dbgt"} className="cardAnime">
-          <img src={Dbgt} width={250} className="coverImage" />
-          <p className="textCard">Dragon Ball GT</p>
-        </Link>
-        <Link to={"/dbheroes"} className="cardAnime">
-          <img src={Dbh} width={250} className="coverImage" />
-          <p className="textCard">Heroes</p>
-        </Link>
-        <Link to={"/ova"} className="cardAnime">
-          <img src={Dbova} width={250} className="coverImage" />
-          <p className="textCard">Dragon Ball OVAs</p>
-        </Link>
-        <Link to={"/special"} className="cardAnime">
-          <img src={Dbes} width={250} className="coverImage" />
-          <p className="textCard">Especiais</p>
-        </Link>
-      </div>
-    </div>
+    </>
   );
 }
--- Client/src/screens/Movies/index.tsx
+++ Client/src/screens/Movies/index.tsx
@@ -1,3 +1,4 @@
+import Header from "../../components/Header";
 import MovieRow from "../../components/MoviesRowComponents/MovieRow";
 import "./styles.css";
 
@@ -5,6 +6,7 @@ import "./styles.css";
 export default function Movies() {
   return (
     <div className="containerContent">
+      <Header />
       <h1 className="secondContainer">
         Todos os filmes disponíveis
       </h1>
