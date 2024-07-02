7a010329b7a48233f478a8700be68a8318e89a97
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-07-02 15:22:19 -0300

    :lipstick: feat: Animes Screen Finished

--- Client/src/screens/Animes/index.tsx
+++ Client/src/screens/Animes/index.tsx
@@ -1,59 +1,35 @@
 import { Link } from "react-router-dom";
 
-import Db from "../../images/capaDB.jpeg";
-import Dbz from "../../images/capaDBZ.jpeg";
-import Dbkai from "../../images/capaKai.jpeg";
-import Dbs from "../../images/capaDBS.jpeg";
-import Dbgt from "../../images/capaDBGT.jpeg";
-import Dbh from "../../images/capaDBH.jpg";
-import Dbova from "../../images/capaDBO.jpg";
-import Dbes from "../../images/capaDBEs.jpeg";
-
 import "./styles.css";
 import Header from "../../components/Header";
+import { AnimeSData } from "../../data/AnimesData/AnimeSType.data";
+import { IoIosPlayCircle } from "react-icons/io";
 
 export default function Animes() {
   return (
     <>
       <Header />
       <div className="containerContent">
-        <h1 className="secondContainer">Assista a todos os animes</h1>
-        <div className="cardsContainer">
-          <Link to={"/classic"} className="cardAnime">
-            <img src={Db} width={250} className="coverImage" />
-            <p className="textCard">Dragon Ball</p>
-          </Link>
-          <Link to={"/dbz"} className="cardAnime">
-            <img src={Dbz} width={250} className="coverImage" />
-            <p className="textCard">Dragon Ball Z</p>
-          </Link>
-
-          <Link to={"/dbkai"} className="cardAnime">
-            <img src={Dbkai} width={250} className="coverImage" />
-            <p className="textCard">Dragon Ball Z Kai</p>
-          </Link>
-          <Link to={"/dbsuper"} className="cardAnime">
-            <img src={Dbs} width={250} className="coverImage" />
-            <p className="textCard">Dragon Ball Super</p>
-          </Link>
-        </div>
-        <div className="cardsContainer">
-          <Link to={"/dbgt"} className="cardAnime">
-            <img src={Dbgt} width={250} className="coverImage" />
-            <p className="textCard">Dragon Ball GT</p>
-          </Link>
-          <Link to={"/dbheroes"} className="cardAnime">
-            <img src={Dbh} width={250} className="coverImage" />
-            <p className="textCard">Heroes</p>
-          </Link>
-          <Link to={"/ova"} className="cardAnime">
-            <img src={Dbova} width={250} className="coverImage" />
-            <p className="textCard">Dragon Ball OVAs</p>
-          </Link>
-          <Link to={"/special"} className="cardAnime">
-            <img src={Dbes} width={250} className="coverImage" />
-            <p className="textCard">Especiais</p>
-          </Link>
+        <div className="cardsContainer space-x-4 space-y-2">
+          {AnimeSData.map((item) => (
+            <Link
+              to={`/${item.url}`}
+              className="contentContainer"
+            >
+              <img
+                className="coverImage opacity-60"
+                src={item.image}
+                style={{ width: 300, height: 500 }}
+                alt={item.name}
+              />
+              <p className="nameBox">
+                <p className="font-bold">{item.name}</p>
+                <p className="flex items-center text-xs">
+                  <IoIosPlayCircle className="mr-2" size={20} /> {item.ep}
+                </p>
+              </p>
+            </Link>
+          ))}
         </div>
       </div>
     </>
