710fa0cdc3221c1d2a0286b4a1028d88aeb022e7
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-07-02 20:27:33 -0300

    :lipstick: feat: Back to home

--- Client/src/screens/Animes/index.tsx
+++ Client/src/screens/Animes/index.tsx
@@ -4,12 +4,18 @@ import "./styles.css";
 import Header from "../../components/Header";
 import { AnimeSData } from "../../data/AnimesData/AnimeSType.data";
 import { IoIosPlayCircle } from "react-icons/io";
+import { IoArrowBack } from "react-icons/io5";
 
 export default function Animes() {
   return (
     <>
       <Header />
       <div className="containerContent">
+      <div className="container hover:opacity-75">
+        <Link className="flex items-center" to={"/"}>
+          <IoArrowBack size={25} />{" "}Inicio
+        </Link>
+      </div>
         <div className="cardsContainer space-x-4 space-y-2">
           {AnimeSData.map((item) => (
             <Link
--- Client/src/screens/Movies/index.tsx
+++ Client/src/screens/Movies/index.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+import { IoArrowBack } from "react-icons/io5";
 import Header from "../../components/Header";
 import MovieRow from "../../components/MoviesRowComponents/MovieRow";
 import "./styles.css";
@@ -7,6 +9,11 @@ export default function Movies() {
   return (
     <div className="containerContent">
       <Header />
+      <div className="container hover:opacity-75">
+        <Link className="flex items-center" to={"/"}>
+          <IoArrowBack size={25} />{" "}Inicio
+        </Link>
+      </div>
       <MovieRow />
     </div>
   );
