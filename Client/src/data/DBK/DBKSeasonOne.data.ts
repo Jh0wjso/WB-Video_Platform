7e8eb73263e7b5cdb774ed58c79e25acecbabdf5
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-07-03 20:58:56 -0300

    :sparkles: feat: DB KAi Data Update

--- Client/src/data/DBK/DBKSeasonOne.data.ts
+++ Client/src/data/DBK/DBKSeasonOne.data.ts
@@ -1,76 +1,672 @@
-import { EpInfo }  from '../EpContent'
+import { EpInfo } from "../EpContent";
 
-export const DBKSeasonOneData : EpInfo[] = [
-    {
-        num: "01",
-        name: "dragon-ball-kai-dublado",
-    },
-    {
-        num: "02",
-        name: "dragon-ball-kai-dublado",
-    },
-    {
-        num: "03",
-        name: "dragon-ball-kai-dublado",
-    },
-    {
-        num: "04",
-        name: "dragon-ball-kai-dublado",
-    },
-    {
-        num: "05",
-        name: "dragon-ball-kai-dublado",
-    },
-    {
-        num: "06",
-        name: "dragon-ball-kai-dublado",
-    },
-    {
-        num: "07",
-        name: "dragon-ball-kai-dublado",
-    },
-    {
-        num: "08",
-        name: "dragon-ball-kai-dublado",
-    },
-    {
-        num: "09",
-        name: "dragon-ball-kai-dublado",
-    },
-    {
-        num: "10",
-        name: "dragon-ball-kai-dublado",
-    },
-    {
-        num: "11",
-        name: "dragon-ball-kai-dublado",
-    },
-    {
-        num: "12",
-        name: "dragon-ball-kai-dublado",
-    },
-    {
-        num: "13",
-        name: "dragon-ball-kai-dublado",
-    },
-    {
-        num: "14",
-        name: "dragon-ball-kai-dublado",
-    },
-    {
-        num: "15",
-        name: "dragon-ball-kai-dublado",
-    },
-    {
-        num: "16",
-        name: "dragon-ball-kai-dublado",
-    },
-    {
-        num: "17",
-        name: "dragon-ball-kai-dublado",
-    },
-    {
-        num: "18",
-        name: "dragon-ball-kai-dublado",
-    },
+export const DBKSeasonOneData: EpInfo[] = [
+  {
+    num: "01",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "02",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "03",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "04",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "05",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "06",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "07",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "08",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "09",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "10",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "11",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "12",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "13",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "14",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "15",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "16",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "17",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "18",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "19",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "20",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "21",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "22",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "23",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "24",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "25",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "26",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "27",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "28",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "29",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "30",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "31",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "32",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "33",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "34",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "35",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "36",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "37",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "38",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "39",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "40",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "41",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "42",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "43",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "44",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "45",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "46",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "47",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "48",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "49",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "50",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "51",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "52",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "53",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "54",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "55",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "56",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "57",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "58",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "59",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "60",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "61",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "62",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "63",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "64",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "65",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "66",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "67",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "68",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "69",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "70",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "71",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "72",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "73",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "74",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "75",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "76",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "77",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "78",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "79",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "80",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "81",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "82",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "83",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "84",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "85",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "86",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "87",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "88",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "89",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "90",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "91",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "92",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "93",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "94",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "95",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "96",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "97",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "98",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "99",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "100",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "101",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "102",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "103",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "104",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "105",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "106",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "107",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "108",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "109",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "110",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "111",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "112",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "113",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "114",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "115",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "116",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "117",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "118",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "119",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "120",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "121",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "122",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "123",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "124",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "125",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "126",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "127",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "128",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "129",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "130",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "131",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "132",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "133",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "134",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "135",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "136",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "137",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "138",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "139",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "140",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "141",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "142",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "143",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "144",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "145",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "146",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "147",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "148",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "149",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "150",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "151",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "152",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "153",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "154",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "155",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "156",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "157",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "158",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "159",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "160",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "161",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "162",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "163",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "164",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "165",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "166",
+    name: "dragon-ball-kai-dublado",
+  },
+  {
+    num: "167",
+    name: "dragon-ball-kai-dublado",
+  },
 ];
---
+++ Client/src/data/DBK/DBKaidesc.data.ts
@@ -0,0 +1,17 @@
+import { AnimeDescription } from "../AnimeDesc";
+
+export const DBKaiDesc: AnimeDescription = {
+  name: "Dagon Ball KAI",
+  categories: [
+    "Ação",
+    "Aventura",
+    "Comedia",
+    "Fantasia",
+    "Shounen",
+    "Remake",
+    "Reboot",
+  ],
+  description:
+    "Dragon Ball Kai é uma versão remasterizada de Dragon Ball Z produzida em comemoração ao aniversário de 20 anos da série. A nova versão foi feita para comemorar o aniversário de 20 anos da série e foi exibida entre 2009 e 2011. A série foi editada para seguir mais fielmente o mangá de Akira Toriyama, removendo cenas de preenchimento e mantendo a qualidade da animação.",
+  link: "dbz",
+};
