1c35e0edf6230e33209b470d89071799988987ac
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-07-03 21:16:49 -0300

    :lipstick: feat: DB Super Screen update

--- Client/src/components/DragonBallSuper/SeasonOne/index.tsx
+++ Client/src/components/DragonBallSuper/SeasonOne/index.tsx
@@ -5,7 +5,6 @@ import { DBSSeasonOneData } from "../../../data/DBS/DBSSeasonOne.data";
 export default function SeasonOneSuper() {
   return (
     <div className="containerEps">
-      <h1 className="text-center text-4xl font-bold">Deus da Destruição Bills</h1>
       <section className="episodeButtonsZ">
         {DBSSeasonOneData.map((item) => (
           <SectionLinks num={`${item.num}`} url={`${item.name}`} />
--- Client/src/screens/DBSuperScreen/index.tsx
+++ Client/src/screens/DBSuperScreen/index.tsx
@@ -1,28 +1,19 @@
-import DBSuperAbstract from "../../components/DragonBallSuper/DBSuperAbstract";
-import MovieRowSuper from "../../components/DragonBallSuper/MovieRow";
-import SeasonFinalSuper from "../../components/DragonBallSuper/SeasonFinal";
-import SeasonFourSuper from "../../components/DragonBallSuper/SeasonFour";
+import DBSuperSerie from "../../components/DragonBallSuper/MainSerie";
 import SeasonOneSuper from "../../components/DragonBallSuper/SeasonOne";
-import SeasonThreeSuper from "../../components/DragonBallSuper/SeasonThree";
-import SeasonTwoSuper from "../../components/DragonBallSuper/SeasonTwo";
-import './styles.css';
+import "./styles.css";
 
 export default function DBSuperScreen() {
   return (
-    <div className="classicScreenBoxS">
-      <div className="bgBox min-h-screen flex flex-col items-center">
-        <h1 className="text-center text-5xl py-8 font-bold">
-          Assista a Todos os Episódios Aqui:
+    <div className="min-h-screen min-w-full flex flex-col items-center">
+      <DBSuperSerie />
+      <div className="bgBox pl-5 flex flex-col justify-center items-start">
+        <h1 className="text-start text-3xl pt-8 font-bold">
+          Episódios | Dragon Ball KAI
         </h1>
-        <DBSuperAbstract />
-        <section className="flex flex-col w-[80%]">
-          <SeasonOneSuper />
-          <SeasonTwoSuper />
-          <SeasonThreeSuper />
-          <SeasonFourSuper />
-          <SeasonFinalSuper />
-        </section>
-        <MovieRowSuper />
+        <h1 className="text-start text-xl text-gray-400">
+          Ano de lançamento: 2009
+        </h1>
+        <SeasonOneSuper />
       </div>
     </div>
   );
