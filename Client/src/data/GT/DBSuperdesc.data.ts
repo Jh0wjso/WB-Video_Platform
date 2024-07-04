19e05e1765943e8029c605814794d730807e6a89
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-07-04 08:23:04 -0300

    :sparkles: feat: DB GT Data Update

---
+++ Client/src/data/GT/DBSuperdesc.data.ts
@@ -0,0 +1,18 @@
+import { AnimeDescription } from "../AnimeDesc";
+
+export const DBgtDesc: AnimeDescription = {
+  name: "Dagon Ball GT",
+  categories: [
+    "Ação",
+    "Aventura",
+    "Fantasia",
+    "Super Poderes",
+    "Artes Marciais",
+    "Sci-Fi",
+    "Shounen",
+    "Super Poderes",
+  ],
+  description:
+    "Dragon Ball GT criada pelo filho de Akira Toriyama, Toyble, é uma série de anime que é uma sequência direta de Dragon Ball Z. A série foi exibida entre 1996 e 1997, com um total de 64 episódios. A série foi transmitida no Brasil pelo Cartoon Network e pelo serviço de streaming Crunchyroll.",
+  link: "dbgt",
+};
--- Client/src/data/GT/GTSeasonOne.data.ts
+++ Client/src/data/GT/GTSeasonOne.data.ts
@@ -89,4 +89,172 @@ export const GTSeasonOneData : EpInfo[] = [
         num: "22",
         name: "dragon-ball-gt-dublado-3-temporada-dublado",
     },
+    {
+        num: "23",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "24",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "25",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "26",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "27",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "28",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "29",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "30",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "31",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "32",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "33",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "34",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "35",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "36",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "37",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "38",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "39",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "40",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "41",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "42",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "43",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "44",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "45",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "46",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "47",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "48",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "49",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "50",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "51",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "52",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "53",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "54",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "55",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "56",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "57",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "58",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "59",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "60",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "61",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "62",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "63",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
+    {
+        num: "64",
+        name: "dragon-ball-gt-dublado-3-temporada-dublado",
+    },
 ];
