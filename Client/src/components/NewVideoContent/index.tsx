2df6663e5c206ebd6e0889824fbc0fcd6b60c178
Jh0wjso <jhonatansilverio1258@gmail.com>
2024-07-05 12:44:46 -0300

    :sparkles: feat: Video Player Update

--- Client/src/components/NewVideoContent/index.tsx
+++ Client/src/components/NewVideoContent/index.tsx
@@ -1,5 +1,3 @@
-import Cover from "../../images/DB_case.jpg";
-
 interface InfoVideo {
   number: string;
   url: string;
@@ -8,16 +6,17 @@ interface InfoVideo {
 export default function NewVideoContent({ number, url }: InfoVideo) {
   return (
     <>
-      <h1 className="epName">
-        Episodio: <br /> {url}
-      </h1>
-      <iframe
+      <h1 className="epName">Episodio: {" " + url}</h1>
+      <video
         src={`https://mangas.cloud/Animes/Letra-D/${number}/${url}.mp4`}
         height="100%"
         width="100%"
-        allowFullScreen
-        className="videoContent"
-      ></iframe>
+        className="videoPlayer"
+        controls={true}
+        poster={
+          "https://i.pinimg.com/736x/76/7b/61/767b61bd84f46d3c458d94ad15a6796d.jpg"
+        }
+      ></video>
       <div className="containerHelp">
         <h3 className="helpText">Algum problema com o vídeo?</h3>
         <a
